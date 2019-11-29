0e968a1c560b0bb0ebe371dfeb34d5ebb353b1fb
kuldeepkeshwar <kuldeepkeshwar@gmail.com>
2019-11-29 14:15:07 +0530

    setup size-plugin-bot

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import commonjs from 'rollup-plugin-commonjs'
 import external from 'rollup-plugin-peer-deps-external'
 import resolve from 'rollup-plugin-node-resolve'
 import { sizeSnapshot } from 'rollup-plugin-size-snapshot'
-
+import size from 'rollup-plugin-size';
 import pkg from './package.json'
 
 export default [
@@ -14,7 +14,9 @@ export default [
       format: 'cjs',
       sourcemap: true,
     },
-    plugins: [external(), babel(), resolve(), commonjs(), sizeSnapshot()],
+    plugins: [external(), babel(), resolve(), commonjs(),
+      size({publish:true,exclude:pkg.module,filename:'sizes-cjs.json'}),
+      sizeSnapshot()],
   },
   {
     input: 'src/index.js',
@@ -23,6 +25,8 @@ export default [
       format: 'es',
       sourcemap: true,
     },
-    plugins: [external(), babel(), sizeSnapshot()],
+    plugins: [external(), babel(),
+      size({publish:true,exclude:pkg.main,filename:'sizes-es.json'}),
+      sizeSnapshot()],
   },
 ]
