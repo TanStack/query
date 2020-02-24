29d8b1aec01c842d2ad8ff07fff2cd8b5a00fcb5
Andrew Cherniavskii <andrew.cherniavskii@gmail.com>
2020-02-24 22:43:50 +0100

    add test for dependent queries

--- src/__tests__/userQuery.test.js
+++ src/__tests__/userQuery.test.js
@@ -35,4 +35,37 @@ describe('useQuery', () => {
     await waitForElement(() => getByTestId('title'))
     expect(getByTestId('title').textContent).toBe('default')
   })
+
+  // See https://github.com/tannerlinsley/react-query/issues/137
+  it('should not override initial data in dependent queries', async () => {
+    function Page() {
+      const { data: first } = useQuery(
+        false && 'first',
+        () => {},
+        { initialData: 'first' }
+      );
+
+      const { data: second } = useQuery(
+        false && 'second',
+        () => {},
+        { initialData: 'second' }
+      );
+
+      return (
+        <div>
+          <h2 data-testid="first">{first}</h2>
+          <h2 data-testid="second">{second}</h2>
+        </div>
+      )
+    }
+
+    const { getByTestId } = render(<Page />)
+
+    await waitForElement(() => [
+      getByTestId('first'),
+      getByTestId('second'),
+    ])
+    expect(getByTestId('first').textContent).toBe('first')
+    expect(getByTestId('second').textContent).toBe('second')
+  })
 })
