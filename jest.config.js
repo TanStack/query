80bdba04250dcbb002b48624bc527e76a66de5de
Ben Monro <benjamin.monro@walmartlabs.com>
2020-03-01 11:38:07 -0800

    added jest config

---
+++ jest.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    // "coverageThreshold": {
+    //     "global": {
+    //         "branches": 80,
+    //         "functions": 80,
+    //         "lines": 80,
+    //         "statements": -10
+    //     }
+    // }
+    coverageReporters =["json", "lcov", "text", "clover", "text-summary"]
+}
