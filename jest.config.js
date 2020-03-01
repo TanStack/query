8c890b708967f97c7f9ecc5b8a4bd0f5af45bb1c
Ben Monro <ben.monro@gmail.com>
2020-03-01 12:34:27 -0800

    chore: add tests.yml build action to run tests on PR / master. (#177)
    * Create test.yml
    
    * Update test.yml
    
    * added jest config
    
    * fixed typo
    
    * covg threshold
    
    * consolidating package.json
    
    * frozen lockfile
    
    * rename tests for badge

---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+    "coverageThreshold": {
+        "global": {
+            "branches": 65.38,
+            "functions": 67.65,
+            "lines": 72.75,
+            "statements": 71.54
+        }
+    },
+    collectCoverage: true,
+    coverageReporters: ["json", "lcov", "text", "clover", "text-summary"]
+}
