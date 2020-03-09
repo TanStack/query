f561605b6dfb649587f9a6109e31d31a9b8f2ea7
Andrew Cherniavskii <andrew.cherniavskii@gmail.com>
2020-03-09 19:41:20 +0100

    fix queryCache not notifying listeners, when query with initialData is added (#213)
    * add passing test
    
    * add failing test
    
    * format code
    
    * fix failing test

--- src/tests/queryCache.test.js
+++ src/tests/queryCache.test.js
@@ -1,6 +1,10 @@
 import { queryCache } from '../index'
 
 describe('queryCache', () => {
+  afterEach(() => {
+    queryCache.clear();
+  })
+
   test('setQueryData does not crash if query could not be found', () => {
     expect(() =>
       queryCache.setQueryData(['USER', { userId: 1 }], prevUser => ({
@@ -16,5 +20,25 @@ describe('queryCache', () => {
     const second = await queryCache.prefetchQuery('key', fetchFn)
 
     expect(second).toBe(first)
+  })
+
+  test('should notify listeners when new query is added', () => {
+    const callback = jest.fn()
+
+    queryCache.subscribe(callback)
+
+    queryCache.prefetchQuery('test', () => 'data')
+
+    expect(callback).toHaveBeenCalled()
+  })
+
+  test('should notify subsribers when new query with initialData is added', () => {
+    const callback = jest.fn()
+
+    queryCache.subscribe(callback)
+
+    queryCache.prefetchQuery('test', () => {}, { initialData: 'initial' })
+
+    expect(callback).toHaveBeenCalled()
   })
 })
