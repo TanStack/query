fefd7afcca6ad7ae411bc8a86e3cf820b6c10233
Tim Wachsener <tim@wachsener.de>
2020-03-19 23:44:13 +0100

    Add useInfiniteQuery with initialData tests

--- src/tests/useInfiniteQuery.test.js
+++ src/tests/useInfiniteQuery.test.js
@@ -11,6 +11,14 @@ import { sleep } from './utils'
 
 const pageSize = 10
 
+const initialItems = (page) => {
+  return {
+    items: [...new Array(10)].fill(null).map((_, d) => page * pageSize + d),
+    nextId: page + 1,
+    ts: 0,
+  }
+}
+
 const fetchItems = async (page, ts) => {
   await sleep(10)
   return {
@@ -118,6 +126,95 @@ describe('useInfiniteQuery', () => {
       rendered.getByText('Item: 19'),
       rendered.getByText('Page 0: 2'),
       rendered.getByText('Page 1: 3'),
+    ])
+  })
+
+  it('should compute canFetchMore correctly using initialData', async () => {
+    function Page() {
+      const fetchCountRef = React.useRef(0)
+      const {
+        status,
+        data,
+        error,
+        isFetching,
+        isFetchingMore,
+        fetchMore,
+        canFetchMore,
+        refetch,
+      } = useInfiniteQuery(
+        'items',
+        (key, nextId = 0) => fetchItems(nextId, fetchCountRef.current++),
+        {
+          initialData: [initialItems(0), initialItems(1)],
+          getFetchMore: (lastGroup, allGroups) => lastGroup.nextId,
+        }
+      )
+
+      expect(canFetchMore).not.toBeUndefined()
+
+      return (
+        <div>
+          <h1>Pagination</h1>
+          {status === 'loading' ? (
+            'Loading...'
+          ) : status === 'error' ? (
+            <span>Error: {error.message}</span>
+          ) : (
+            <>
+              <div>Data:</div>
+              {data.map((page, i) => (
+                <div key={i}>
+                  <div>
+                    Page {i}: {page.ts}
+                  </div>
+                  <div key={i}>
+                    {page.items.map(item => (
+                      <p key={item}>Item: {item}</p>
+                    ))}
+                  </div>
+                </div>
+              ))}
+              <div>
+                <button
+                  onClick={() => fetchMore()}
+                  disabled={!canFetchMore || isFetchingMore}
+                >
+                  {isFetchingMore
+                    ? 'Loading more...'
+                    : canFetchMore
+                    ? 'Load More'
+                    : 'Nothing more to load'}
+                </button>
+                <button onClick={() => refetch()}>Refetch</button>
+              </div>
+              <div>
+                {isFetching && !isFetchingMore
+                  ? 'Background Updating...'
+                  : null}
+              </div>
+            </>
+          )}
+        </div>
+      )
+    }
+
+    const rendered = render(<Page />)
+
+    rendered.getByText('Load More')
+
+    await waitForElement(() => [
+      rendered.getByText('Item: 19'),
+      rendered.getByText('Page 0: 0'),
+      rendered.getByText('Page 1: 0'),
+    ])
+
+    fireEvent.click(rendered.getByText('Load More'))
+
+    await waitForElement(() => rendered.getByText('Loading more...'))
+
+    await waitForElement(() => [
+      rendered.getByText('Item: 29'),
+      rendered.getByText('Page 2: 1'),
     ])
   })
 })
