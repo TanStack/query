00fb29fac676ee3ee9c2d992aa973f2188a9cd09
Tanner Linsley <tannerlinsley@gmail.com>
2020-03-26 00:29:33 -0600

    Simpler custom hooks example

---
+++ examples/custom-hooks/src/index.js
@@ -0,0 +1,90 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import ReactDOM from "react-dom";
+
+import usePosts from ".hooks/usePosts";
+import usePost from ".hooks/usePost";
+
+function App() {
+  const [postId, setPostId] = React.useState(-1);
+
+  return (
+    <>
+      <p>
+        As you visit the posts below, you will notice them in a loading state
+        the first time you load them. However, after you return to this list and
+        click on any posts you have already visited again, you will see them
+        load instantly and background refresh right before your eyes!{" "}
+        <strong>
+          (You may need to throttle your network speed to simulate longer
+          loading sequences)
+        </strong>
+      </p>
+      {postId > -1 ? (
+        <Post postId={postId} setPostId={setPostId} />
+      ) : (
+        <Posts setPostId={setPostId} />
+      )}
+    </>
+  );
+}
+
+function Posts({ setPostId }) {
+  const { status, data, error, isFetching } = usePosts();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        {status === "loading" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <div>
+              {data.map(post => (
+                <p key={post.id}>
+                  <a onClick={() => setPostId(post.id)} href="#">
+                    {post.title}
+                  </a>
+                </p>
+              ))}
+            </div>
+            <div>{isFetching ? "Background Updating..." : " "}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function Post({ postId, setPostId }) {
+  const { status, data, error, isFetching } = usePost();
+
+  return (
+    <div>
+      <div>
+        <a onClick={() => setPostId(-1)} href="#">
+          Back
+        </a>
+      </div>
+      {!postId || status === "loading" ? (
+        "Loading..."
+      ) : status === "error" ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <>
+          <h1>{data.title}</h1>
+          <div>
+            <p>{data.body}</p>
+          </div>
+          <div>{isFetching ? "Background Updating..." : " "}</div>
+        </>
+      )}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
