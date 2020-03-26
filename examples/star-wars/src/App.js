9b26181b0afbbb0f99ec6ab3b37c0d6edbd6c6e1
Tanner Linsley <tannerlinsley@gmail.com>
2020-03-26 00:53:21 -0600

    Add Star Wars example

---
+++ examples/star-wars/src/App.js
@@ -0,0 +1,40 @@
+import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import { ThemeProvider } from "@material-ui/core";
+import { createMuiTheme } from "@material-ui/core/styles";
+
+import "./styles.css";
+import Layout from "./Layout";
+
+export default function App() {
+  return (
+    <Router>
+      <ThemeProvider theme={theme}>
+        <Layout />
+      </ThemeProvider>
+    </Router>
+  );
+}
+
+const theme = createMuiTheme({
+  typography: {
+    h1: {
+      fontFamily: "Roboto Mono, monospace"
+    },
+    h2: {
+      fontFamily: "Roboto Mono, monospace"
+    },
+    h3: {
+      fontFamily: "Roboto Mono, monospace"
+    },
+    h4: {
+      fontFamily: "Roboto Mono, monospace"
+    },
+    h5: {
+      fontFamily: "Roboto Mono, monospace"
+    },
+    h6: {
+      fontFamily: "Roboto Mono, monospace"
+    }
+  }
+});
