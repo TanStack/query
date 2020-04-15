fdf11e190f97deb49c3227d054c2a795189d8cd3
Juan Pablo Ramirez <juan.ramirez.q@gmail.com>
2020-04-14 19:36:31 -0500

    add rick and morty example
    this is a very similar example to the star wars, since the star wars api
    is going to be deprecated and is currently down, my proposal is to use
    the rick and morty example to replace the star wars example that is not
    working right now.

---
+++ examples/rick-morty/src/index.js
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./App";
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
