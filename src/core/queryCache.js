db5a51e087c7984d98da51ee6831780a5b3a64aa
Bishwendu Kundu <bishwenduk029@gmail.com>
2020-06-27 11:26:00 +0530

    fix(query-cache): Prevent re-run of query for refetch interval if update is disabled (#640)
    * fix(query-cache): Prevent re-run of query based on refetch interval if update is disabled
    
    * Update src/core/queryCache.js
    
    Co-authored-by: Bishwendu Kundu <bishwendu.kundu@broadcom.com>
    Co-authored-by: Tanner Linsley <tannerlinsley@gmail.com>
    
    Fixes #635

--- src/core/queryCache.js
+++ src/core/queryCache.js
@@ -420,6 +420,7 @@ export function makeQueryCache({ frozen = isServer, defaultConfig } = {}) {
           ) {
             instance.refetchIntervalId = setInterval(() => {
               if (
+                query.instances.some(instance => instance.config.enabled) &&
                 isDocumentVisible() ||
                 query.instances.some(
                   instance => instance.config.refetchIntervalInBackground
