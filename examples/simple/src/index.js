13e99245e0f75dbc4d31fc532228008c98c4c097
Tanner Linsley <tannerlinsley@gmail.com>
2020-07-07 23:01:37 -0600

    Readme + media

---
+++ examples/simple/src/index.js
@@ -0,0 +1,29 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import ReactDOM from "react-dom";
+import { useQuery } from "react-query";
+
+export default function App() {
+  const { isLoading, error, data } = useQuery("repoData", () =>
+    fetch(
+      "https://api.github.com/repos/tannerlinsley/react-query"
+    ).then((res) => res.json())
+  );
+
+  if (isLoading) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
+  return (
+    <div>
+      <h1>{data.name}</h1>
+      <p>{data.description}</p>
+      <strong>👀 {data.subscribers_count}</strong>{" "}
+      <strong>✨ {data.stargazers_count}</strong>{" "}
+      <strong>🍴 {data.forks_count}</strong>
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
