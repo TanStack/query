9677b30944e14da8bb55a6d773692becb42bf8c6
Tanner Linsley <tannerlinsley@gmail.com>
2020-07-07 23:41:08 -0600

    docs: fix build

--- docs/next.config.js
+++ docs/next.config.js
@@ -1,49 +1,7 @@
-const fs = require('fs')
 const path = require('path')
 const dotenvLoad = require('dotenv-load')
 const optimizedImages = require('next-optimized-images')
 dotenvLoad()
-
-const {
-  NOTION_TOKEN,
-  BLOG_INDEX_ID,
-} = require('./src/lib/notion/server-constants')
-
-try {
-  fs.unlinkSync(path.resolve('.blog_index_data'))
-} catch (_) {
-  /* non fatal */
-}
-try {
-  fs.unlinkSync(path.resolve('.blog_index_data_previews'))
-} catch (_) {
-  /* non fatal */
-}
-
-const warnOrError =
-  process.env.NODE_ENV !== 'production'
-    ? console.warn
-    : msg => {
-        throw new Error(msg)
-      }
-
-if (!NOTION_TOKEN) {
-  // We aren't able to build or serve images from Notion without the
-  // NOTION_TOKEN being populated
-  warnOrError(
-    `\nNOTION_TOKEN is missing from env, this will result in an error\n` +
-      `Make sure to provide one before starting Next.js`
-  )
-}
-
-if (!BLOG_INDEX_ID) {
-  // We aren't able to build or serve images from Notion without the
-  // NOTION_TOKEN being populated
-  warnOrError(
-    `\nBLOG_INDEX_ID is missing from env, this will result in an error\n` +
-      `Make sure to provide one before starting Next.js`
-  )
-}
 
 const remarkPlugins = [
   require('remark-slug'),
--- docs/src/lib/docs/utils.js
+++ docs/src/lib/docs/utils.js
@@ -1,27 +1,25 @@
-export function getSlug({
-  slug
-}) {
+export function getSlug({ slug }) {
   if (!slug) {
     return {
-      slug: '/docs/overview'
-    };
+      slug: '/docs/overview',
+    }
   }
 
   if (slug[0] === 'tag') {
     return {
       tag: slug[1],
-      slug: `/docs/${slug.slice(2).join('/')}`
-    };
+      slug: `/docs/${slug.slice(2).join('/')}`,
+    }
   }
 
   return {
-    slug: `/docs/${slug && slug.join('/')}`
-  };
+    slug: `/docs/${slug && slug.join('/')}`,
+  }
 }
 export function removeFromLast(path, key) {
-  const i = path.lastIndexOf(key);
-  return i === -1 ? path : path.substring(0, i);
+  const i = path.lastIndexOf(key)
+  return i === -1 ? path : path.substring(0, i)
 }
 export function addTagToSlug(slug, tag) {
-  return tag ? slug.replace('/docs', `/docs/tag/${tag}`) : slug;
+  return tag ? slug.replace('/docs', `/docs/tag/${tag}`) : slug
 }
