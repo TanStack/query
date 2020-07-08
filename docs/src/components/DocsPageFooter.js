82b5e31dbc8d0e94a65597f7c7f1fd0561138ddb
Tanner Linsley <tannerlinsley@gmail.com>
2020-07-08 09:20:17 -0600

    docs: better footer

--- docs/src/components/DocsPageFooter.js
+++ docs/src/components/DocsPageFooter.js
@@ -14,7 +14,38 @@ export const DocsPageFooter = React.memo(({ route, prevRoute, nextRoute }) => {
 
   return (
     <>
-      <div className="my-8">
+      <div className="py-8">
+        <div className="flex space-between items-center">
+          {prevRoute && prevRoute.path ? (
+            <NextLink href={removeFromLast(prevRoute.path, '.')}>
+              <a className="flex-grow  block">
+                <span className="text-sm block text-gray-500 mb-1 font-semibold">
+                  ← Prev
+                </span>
+                <span className="text-xl block text-blue-600 font-semibold">
+                  {prevRoute.title}
+                </span>
+              </a>
+            </NextLink>
+          ) : (
+            <div />
+          )}
+          {nextRoute && nextRoute.path && (
+            <NextLink href={removeFromLast(nextRoute.path, '.')}>
+              <a className="flex-grow text-right block">
+                <span className="text-sm block text-gray-500 mb-1 font-semibold">
+                  Next →
+                </span>
+                <span className="text-xl block text-blue-600 font-semibold">
+                  {nextRoute.title}
+                </span>
+              </a>
+            </NextLink>
+          )}
+        </div>
+      </div>
+
+      <div className="mb-8">
         <a
           href={editUrl}
           target="_blank"
@@ -32,37 +63,6 @@ export const DocsPageFooter = React.memo(({ route, prevRoute, nextRoute }) => {
         <div className="grid grid-cols-2 gap-3 w-auto max-w-xs mx-auto md:mx-2">
           <TWButton icon={<FiThumbsUp />}>Yes</TWButton>
           <TWButton icon={<FiThumbsDown />}>No</TWButton>
-        </div>
-      </div>
-
-      <div className="py-12">
-        <div className="flex space-between items-center">
-          {prevRoute && prevRoute.path ? (
-            <NextLink href={removeFromLast(prevRoute.path, '.')}>
-              <a className="flex-grow  block">
-                <span className="text-sm block text-gray-500 mb-1 font-semibold">
-                  Prev
-                </span>
-                <span className="text-xl block text-blue-600 font-semibold">
-                  {prevRoute.title}
-                </span>
-              </a>
-            </NextLink>
-          ) : (
-            <div />
-          )}
-          {nextRoute && nextRoute.path && (
-            <NextLink href={removeFromLast(nextRoute.path, '.')}>
-              <a className="flex-grow text-right block">
-                <span className="text-sm block text-gray-500 mb-1 font-semibold">
-                  Next
-                </span>
-                <span className="text-xl block text-blue-600 font-semibold">
-                  {nextRoute.title}
-                </span>
-              </a>
-            </NextLink>
-          )}
         </div>
       </div>
     </>
