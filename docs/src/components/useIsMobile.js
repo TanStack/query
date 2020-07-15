ba48a923fac28857875fb81b9194deeed82be4aa
Tanner Linsley <tannerlinsley@gmail.com>
2020-07-15 15:48:36 -0600

    docs: fix documentation sidebar breakpoint

--- docs/src/components/useIsMobile.js
+++ docs/src/components/useIsMobile.js
@@ -1,29 +1,29 @@
-import { useState, useCallback, useEffect } from 'react';
+import { useState, useCallback, useEffect } from 'react'
 
 const useMediaQuery = width => {
-  const [targetReached, setTargetReached] = useState(false);
+  const [targetReached, setTargetReached] = useState(false)
   const updateTarget = useCallback(e => {
     if (e.matches) {
-      setTargetReached(true);
+      setTargetReached(true)
     } else {
-      setTargetReached(false);
+      setTargetReached(false)
     }
-  }, []);
+  }, [])
   useEffect(() => {
-    const media = window.matchMedia(`(max-width: ${width}px)`);
-    media.addListener(updateTarget); // Check on mount (callback is not called until a change occurs)
+    const media = window.matchMedia(`(max-width: ${width}px)`)
+    media.addListener(updateTarget) // Check on mount (callback is not called until a change occurs)
 
     if (media.matches) {
-      setTargetReached(true);
+      setTargetReached(true)
     }
 
-    return () => media.removeListener(updateTarget);
-  }, []);
-  return targetReached;
-};
+    return () => media.removeListener(updateTarget)
+  }, [])
+  return targetReached
+}
 
 const useIsMobile = () => {
-  return useMediaQuery(640);
-};
+  return useMediaQuery(1024)
+}
 
-export { useMediaQuery, useIsMobile };
+export { useMediaQuery, useIsMobile }
