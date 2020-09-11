887b45b8c55c12923d953c943cb01dc5a20fa53e
Niek <just.niek@gmail.com>
2020-09-11 08:24:40 +0200

    feat: add support for tree shaking

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   collectCoverage: true,
   coverageReporters: ['json', 'lcov', 'text', 'clover', 'text-summary'],
+  testMatch: ['<rootDir>/src/**/*.test.tsx'],
   testPathIgnorePatterns: ['<rootDir>/types/'],
   moduleNameMapper: {
     'react-query': '<rootDir>/src/react/index.ts',
