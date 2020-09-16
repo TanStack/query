c977982c59bc4148db1655096dc9350017828ce7
Tanner Linsley <tannerlinsley@gmail.com>
2020-09-16 15:05:45 -0600

    docs: add graphql-request example

---
+++ examples/basic-graphql-request/src/index.js
@@ -0,0 +1,160 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import React from "react";
+import ReactDOM from "react-dom";
+import {
+  useQuery,
+  useQueryCache,
+  QueryCache,
+  ReactQueryCacheProvider,
+} from "react-query";
+import { ReactQueryDevtools } from "react-query-devtools";
+import { request, gql } from "graphql-request";
+
+const endpoint = "https://graphqlzero.almansi.me/api";
+
+const queryCache = new QueryCache();
+
+function App() {
+  const [postId, setPostId] = React.useState(-1);
+
+  return (
+    <ReactQueryCacheProvider queryCache={queryCache}>
+      <p>
+        As you visit the posts below, you will notice them in a loading state
+        the first time you load them. However, after you return to this list and
+        click on any posts you have already visited again, you will see them
+        load instantly and background refresh right before your eyes!{" "}
+        <strong>
+          (You may need to throttle your network speed to simulate longer
+          loading sequences)
+        </strong>
+      </p>
+      {postId > -1 ? (
+        <Post postId={postId} setPostId={setPostId} />
+      ) : (
+        <Posts setPostId={setPostId} />
+      )}
+      <ReactQueryDevtools initialIsOpen />
+    </ReactQueryCacheProvider>
+  );
+}
+
+function usePosts() {
+  return useQuery("posts", async () => {
+    const {
+      posts: { data },
+    } = await request(
+      endpoint,
+      gql`
+        query {
+          posts {
+            data {
+              id
+              title
+            }
+          }
+        }
+      `
+    );
+    return data;
+  });
+}
+
+function Posts({ setPostId }) {
+  const cache = useQueryCache();
+  const { status, data, error, isFetching } = usePosts();
+
+  return (
+    <div>
+      <h1>Posts</h1>
+      <div>
+        {status === "loading" ? (
+          "Loading..."
+        ) : status === "error" ? (
+          <span>Error: {error.message}</span>
+        ) : (
+          <>
+            <div>
+              {data.map((post) => (
+                <p key={post.id}>
+                  <a
+                    onClick={() => setPostId(post.id)}
+                    href="#"
+                    style={
+                      // We can use the queryCache here to show bold links for
+                      // ones that are cached
+                      cache.getQueryData(["post", post.id])
+                        ? {
+                            fontWeight: "bold",
+                            color: "green",
+                          }
+                        : {}
+                    }
+                  >
+                    {post.title}
+                  </a>
+                </p>
+              ))}
+            </div>
+            <div>{isFetching ? "Background Updating..." : " "}</div>
+          </>
+        )}
+      </div>
+    </div>
+  );
+}
+
+function usePost(postId) {
+  return useQuery(
+    ["post", postId],
+    async () => {
+      const { post } = await request(
+        endpoint,
+        gql`
+        query {
+          post(id: ${postId}) {
+            id
+            title
+            body
+          }
+        }
+        `
+      );
+
+      return post;
+    },
+    {
+      enabled: postId,
+    }
+  );
+}
+
+function Post({ postId, setPostId }) {
+  const { status, data, error, isFetching } = usePost(postId);
+
+  return (
+    <div>
+      <div>
+        <a onClick={() => setPostId(-1)} href="#">
+          Back
+        </a>
+      </div>
+      {!postId || status === "loading" ? (
+        "Loading..."
+      ) : status === "error" ? (
+        <span>Error: {error.message}</span>
+      ) : (
+        <>
+          <h1>{data.title}</h1>
+          <div>
+            <p>{data.body}</p>
+          </div>
+          <div>{isFetching ? "Background Updating..." : " "}</div>
+        </>
+      )}
+    </div>
+  );
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
