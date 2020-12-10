50ac608964d23cf0554434ebd63f136947a58623
Tanner Linsley <tannerlinsley@gmail.com>
2020-12-10 10:36:39 -0700

    chore: revert codesandbox fix

--- devtools/index.js
+++
@@ -1,5 +0,0 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('../dist/react-query-devtools.development.min.js')
-} else {
-  module.exports = require('../dist/react-query-devtools.development.js')
-}
