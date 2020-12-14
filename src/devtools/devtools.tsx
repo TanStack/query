b030e0e2587541447db36ad7af0d472a704cfcf6
Tanner Linsley <tannerlinsley@gmail.com>
2020-12-14 16:46:39 -0700

    types(devtools): better types for devtools
    Fixes #1406

--- src/devtools/devtools.tsx
+++ src/devtools/devtools.tsx
@@ -22,6 +22,56 @@ import { getQueryStatusLabel, getQueryStatusColor } from './utils'
 import Explorer from './Explorer'
 import Logo from './Logo'
 
+interface DevtoolsOptions {
+  /**
+   * Set this true if you want the dev tools to default to being open
+   */
+  initialIsOpen?: boolean
+  /**
+   * Use this to add props to the panel. For example, you can add className, style (merge and override default style), etc.
+   */
+  panelProps?: React.DetailedHTMLProps<
+    React.HTMLAttributes<HTMLDivElement>,
+    HTMLDivElement
+  >
+  /**
+   * Use this to add props to the close button. For example, you can add className, style (merge and override default style), onClick (extend default handler), etc.
+   */
+  closeButtonProps?: React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+  /**
+   * Use this to add props to the toggle button. For example, you can add className, style (merge and override default style), onClick (extend default handler), etc.
+   */
+  toggleButtonProps?: React.DetailedHTMLProps<
+    React.ButtonHTMLAttributes<HTMLButtonElement>,
+    HTMLButtonElement
+  >
+  /**
+   * The position of the React Query logo to open and close the devtools panel.
+   * Defaults to 'bottom-left'.
+   */
+  position?: 'top-left' | 'top-right' | 'bottom-left' | 'bottom-right'
+  /**
+   * Use this to render the devtools inside a different type of container element for a11y purposes.
+   * Any string which corresponds to a valid intrinsic JSX element is allowed.
+   * Defaults to 'footer'.
+   */
+  containerElement?: string | any
+}
+
+interface DevtoolsPanelOptions {
+  /**
+   * The standard React style object used to style a component with inline styles
+   */
+  style?: React.CSSProperties
+  /**
+   * The standard React className property used to style a component with classes
+   */
+  className?: string
+}
+
 const isServer = typeof window === 'undefined'
 
 const theme = {
@@ -45,7 +95,7 @@ export function ReactQueryDevtools({
   toggleButtonProps = {},
   position = 'bottom-left',
   containerElement: Container = 'footer',
-}) {
+}: DevtoolsOptions): React.ReactElement {
   const rootRef = React.useRef()
   const panelRef = React.useRef()
   const [isOpen, setIsOpen] = useLocalStorage(
@@ -210,7 +260,10 @@ const sortFns = {
 }
 
 export const ReactQueryDevtoolsPanel = React.forwardRef(
-  function ReactQueryDevtoolsPanel(props, ref) {
+  function ReactQueryDevtoolsPanel(
+    props: DevtoolsPanelOptions,
+    ref
+  ): React.ReactElement {
     const { setIsOpen, ...panelProps } = props
 
     const queryClient = useQueryClient()
