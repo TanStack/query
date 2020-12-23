e69b391efcbb09015dc1ecdac288cde17cfdb703
Abdullah Mzaien <s201540830@kfupm.edu.sa>
2020-12-23 05:14:05 +0300

    fix(devtools): change the new invalidate button color to have more contrast (#1507)
    * Changing the new invalidate button color to have more contrast
    
    * consistant colors all over dev tools

--- src/devtools/devtools.tsx
+++ src/devtools/devtools.tsx
@@ -658,6 +658,7 @@ export const ReactQueryDevtoolsPanel = React.forwardRef(
                   onClick={() => queryClient.invalidateQueries(activeQuery)}
                   style={{
                     background: theme.warning,
+                    color: theme.inputTextColor,
                   }}
                 >
                   Invalidate
