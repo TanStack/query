eb3d700dc522b86f6e9cc42d08e3b6197766c53b
Tanner Linsley <tannerlinsley@gmail.com>
2021-01-17 09:11:30 -0700

    fix(hydration): useMemo for hydrating again

--- src/hydration/react.tsx
+++ src/hydration/react.tsx
@@ -11,8 +11,9 @@ export function useHydrate(state: unknown, options?: HydrateOptions) {
 
   // Running hydrate again with the same queries is safe,
   // it wont overwrite or initialize existing queries,
-  // relying on useEffect here is only a performance optimization
-  React.useEffect(() => {
+  // relying on useMemo here is only a performance optimization.
+  // hydrate can and should be run *during* render here for SSR to work properly
+  React.useMemo(() => {
     if (state) {
       hydrate(queryClient, state, optionsRef.current)
     }
