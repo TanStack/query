a9faf986bde9619cfc6ff8f50296059260c9cb81
Ashwin Gokhale <ashwin.gokhale98@gmail.com>
2021-01-17 00:11:25 -0800

    feat(types): Add optional cancelRefetch field to ResultOptions interface

--- src/core/types.ts
+++ src/core/types.ts
@@ -215,7 +215,9 @@ export interface ResultOptions {
   throwOnError?: boolean
 }
 
-export interface RefetchOptions extends ResultOptions {}
+export interface RefetchOptions extends ResultOptions {
+  cancelRefetch?: boolean;
+}
 
 export interface InvalidateQueryFilters extends QueryFilters {
   refetchActive?: boolean
