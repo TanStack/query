4dfcf8989f6092ac8c0d555f5f3feb2c5b913abf
Tanner Linsley <tannerlinsley@gmail.com>
2021-03-26 16:17:30 -0600

    docs: fix carbonads

--- docs/src/components/CarbonAds.js
+++ docs/src/components/CarbonAds.js
@@ -1,17 +1,45 @@
 import React from 'react'
 
+function buildScript(src, attrs = {}) {
+  if (typeof document !== 'undefined') {
+    const script = document.createElement('script')
+    script.async = true
+    script.defer = true
+    Object.keys(attrs).forEach(attr => script.setAttribute(attr, attrs[attr]))
+    script.src = src
+
+    return script
+  }
+}
+
 export default function CarbonAds() {
+  const ref = React.useRef()
+
   React.useEffect(() => {
-    if (window._carbonads) {
-      window._carbonads.refresh()
-    }
+    const script = buildScript(
+      '//cdn.carbonads.com/carbon.js?serve=CESDV23N&placement=react-querytanstackcom',
+      {
+        type: 'text/javascript',
+        id: '_carbonads_js',
+      }
+    )
+
+    ref.current.appendChild(script)
   }, [])
 
-  return (
-    <div
-      dangerouslySetInnerHTML={{
-        __html: `<script async type="text/javascript" src="//cdn.carbonads.com/carbon.js?serve=CESDV23N&placement=react-querytanstackcom" id="_carbonads_js"></script>`,
-      }}
-    ></div>
-  )
+  React.useEffect(() => {
+    const interval = setInterval(() => {
+      ;[...ref.current.children].forEach(child => {
+        if (child.id.startsWith('carbonads_')) {
+          ref.current.removeChild(child)
+        }
+      })
+    }, 100)
+
+    return () => {
+      clearInterval(interval)
+    }
+  })
+
+  return <div ref={ref} />
 }
