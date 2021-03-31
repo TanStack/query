1314db8c0f3f2e607cd072dbaed1b203bf28c18d
Dominik Dorfmeister <office@dorfmeister.cc>
2021-03-31 20:32:04 +0200

    chore: codesandbox auto builds
    no-use-before-define

--- examples/basic-typescript/src/index.tsx
+++ examples/basic-typescript/src/index.tsx
@@ -12,31 +12,6 @@ import { ReactQueryDevtools } from "react-query/devtools";
 
 const queryClient = new QueryClient();
 
-function App() {
-  const [postId, setPostId] = React.useState(-1);
-
-  return (
-    <QueryClientProvider client={queryClient}>
-      <p>
-        As you visit the posts below, you will notice them in a loading state
-        the first time you load them. However, after you return to this list and
-        click on any posts you have already visited again, you will see them
-        load instantly and background refresh right before your eyes!{" "}
-        <strong>
-          (You may need to throttle your network speed to simulate longer
-          loading sequences)
-        </strong>
-      </p>
-      {postId > -1 ? (
-        <Post postId={postId} setPostId={setPostId} />
-      ) : (
-        <Posts setPostId={setPostId} />
-      )}
-      <ReactQueryDevtools initialIsOpen />
-    </QueryClientProvider>
-  );
-}
-
 type Post = {
   id: number;
   title: string;
@@ -146,6 +121,31 @@ function Post({
         </>
       )}
     </div>
+  );
+}
+
+function App() {
+  const [postId, setPostId] = React.useState(-1);
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <p>
+        As you visit the posts below, you will notice them in a loading state
+        the first time you load them. However, after you return to this list and
+        click on any posts you have already visited again, you will see them
+        load instantly and background refresh right before your eyes!{" "}
+        <strong>
+          (You may need to throttle your network speed to simulate longer
+          loading sequences)
+        </strong>
+      </p>
+      {postId > -1 ? (
+        <Post postId={postId} setPostId={setPostId} />
+      ) : (
+        <Posts setPostId={setPostId} />
+      )}
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
   );
 }
 
