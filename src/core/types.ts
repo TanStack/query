a88d80f6f7b2d96ab605ac08f4fbaed9a3bcd4b7
Hyuntae EOM <hyunt0413@gmail.com>
2021-04-10 22:58:09 +0900

    fix(types): Add `undefined` return type in onMutate  (#2089)
    * onMutate callback add void in return type
    
    * Add core type
    
    * return type void to undefined
    
    * onMutate return type change 'Promise<TContext> | Promise<undefined> | TContext | undefined'

--- src/core/types.ts
+++ src/core/types.ts
@@ -474,7 +474,7 @@ export interface MutationOptions<
   mutationFn?: MutationFunction<TData, TVariables>
   mutationKey?: MutationKey
   variables?: TVariables
-  onMutate?: (variables: TVariables) => Promise<TContext> | TContext
+  onMutate?: (variables: TVariables) => Promise<TContext> | Promise<undefined> | TContext | undefined
   onSuccess?: (
     data: TData,
     variables: TVariables,
