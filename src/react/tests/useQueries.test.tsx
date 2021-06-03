40b3b01dce1f9a36e5bc469dfd8bfc8f061507c6
Dominik Dorfmeister <office@dorfmeister.cc>
2021-06-03 15:45:15 +0200

    fix(useQueries): make sure keepPreviousData is respected

--- src/react/tests/useQueries.test.tsx
+++ src/react/tests/useQueries.test.tsx
@@ -1,6 +1,7 @@
+import { waitFor } from '@testing-library/react'
 import React from 'react'
 
-import { queryKey, renderWithClient, sleep } from './utils'
+import { queryKey, renderWithClient, setActTimeout, sleep } from './utils'
 import { useQueries, QueryClient, UseQueryResult, QueryCache } from '../..'
 
 describe('useQueries', () => {
@@ -41,5 +42,90 @@ describe('useQueries', () => {
     expect(results[0]).toMatchObject([{ data: undefined }, { data: undefined }])
     expect(results[1]).toMatchObject([{ data: 1 }, { data: undefined }])
     expect(results[2]).toMatchObject([{ data: 1 }, { data: 2 }])
+  })
+
+  it('should keep previous data if amount of queries is the same', async () => {
+    const key1 = queryKey()
+    const key2 = queryKey()
+    const results: UseQueryResult[][] = []
+
+    function Page() {
+      const [count, setCount] = React.useState(1)
+      const result = useQueries([
+        {
+          queryKey: [key1, count],
+          keepPreviousData: true,
+          queryFn: async () => {
+            await sleep(5)
+            return count * 2
+          },
+        },
+        {
+          queryKey: [key2, count],
+          keepPreviousData: true,
+          queryFn: async () => {
+            await sleep(10)
+            return count * 5
+          },
+        },
+      ])
+      results.push(result)
+
+      React.useEffect(() => {
+        setActTimeout(() => {
+          setCount(prev => prev + 1)
+        }, 20)
+      }, [])
+
+      return null
+    }
+
+    renderWithClient(queryClient, <Page />)
+
+    await waitFor(() => expect(results.length).toBe(7))
+
+    expect(results[0]).toMatchObject([
+      {
+        status: 'loading',
+        data: undefined,
+        isPreviousData: false,
+        isFetching: true,
+      },
+      {
+        status: 'loading',
+        data: undefined,
+        isPreviousData: false,
+        isFetching: true,
+      },
+    ])
+    expect(results[1]).toMatchObject([
+      { status: 'success', data: 2, isPreviousData: false, isFetching: false },
+      {
+        status: 'loading',
+        data: undefined,
+        isPreviousData: false,
+        isFetching: true,
+      },
+    ])
+    expect(results[2]).toMatchObject([
+      { status: 'success', data: 2, isPreviousData: false, isFetching: false },
+      { status: 'success', data: 5, isPreviousData: false, isFetching: false },
+    ])
+    expect(results[3]).toMatchObject([
+      { status: 'success', data: 2, isPreviousData: true, isFetching: true },
+      { status: 'success', data: 5, isPreviousData: true, isFetching: true },
+    ])
+    expect(results[4]).toMatchObject([
+      { status: 'success', data: 2, isPreviousData: true, isFetching: true },
+      { status: 'success', data: 5, isPreviousData: true, isFetching: true },
+    ])
+    expect(results[5]).toMatchObject([
+      { status: 'success', data: 4, isPreviousData: false, isFetching: false },
+      { status: 'success', data: 5, isPreviousData: true, isFetching: true },
+    ])
+    expect(results[6]).toMatchObject([
+      { status: 'success', data: 4, isPreviousData: false, isFetching: false },
+      { status: 'success', data: 10, isPreviousData: false, isFetching: false },
+    ])
   })
 })
