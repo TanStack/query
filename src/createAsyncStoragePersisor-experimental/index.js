6b96263c92b469ca001ab34fa71c976356162b29
aung <thein.aungmyint@gmail.com>
2021-06-09 18:14:08 +0630

    added a new file for asyncStoragePersistor

---
+++ src/createAsyncStoragePersisor-experimental/index.js
@@ -0,0 +1,45 @@
+import AsyncStorage from '@react-native-community/async-storage';
+
+function throttle(func, wait) {
+  if (wait === 0) {
+    wait = 100;
+  }
+
+  var timer = null;
+  return function () {
+    for (
+      var _len = arguments.length, args = new Array(_len), _key = 0;
+      _key < _len;
+      _key++
+    ) {
+      args[_key] = arguments[_key];
+    }
+
+    if (timer === null) {
+      timer = setTimeout(function () {
+        func.apply(0, args);
+        timer = null;
+      }, wait);
+    }
+  };
+}
+
+export const asyncStoragePersistor = ({asyncStorageKey, throttleTime}) => {
+  return {
+    persistClient: throttle(function (persistedClient) {
+      AsyncStorage.setItem(asyncStorageKey, JSON.stringify(persistedClient));
+    }, throttleTime),
+    restoreClient: async function restoreClient() {
+      const cacheString = await AsyncStorage.getItem(asyncStorageKey);
+
+      if (!cacheString) {
+        return;
+      }
+
+      return JSON.parse(cacheString);
+    },
+    removeClient: function removeClient() {
+      AsyncStorage.removeItem(asyncStorageKey);
+    },
+  };
+};
