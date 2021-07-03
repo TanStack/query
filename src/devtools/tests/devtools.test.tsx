de637f372d214e69874ec6ba468bd4311beef9a5
Prateek Surana <prateeksurana3255@gmail.com>
2021-07-03 17:52:47 +0530

    (test): ✅   Add tests for query states

--- src/devtools/tests/devtools.test.tsx
+++ src/devtools/tests/devtools.test.tsx
@@ -1,11 +1,27 @@
 import React from 'react'
-import { fireEvent, screen, waitForElementToBeRemoved } from '@testing-library/react'
+import {
+  fireEvent,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react'
 import { QueryClient, QueryCache, useQuery } from '../..'
-import { renderWithClient, sleep } from './utils'
+import { getByTextContent, renderWithClient, sleep } from './utils'
 
 describe('ReactQueryDevtools', () => {
   const queryCache = new QueryCache()
-  const queryClient = new QueryClient({ queryCache })
+  const queryClient = new QueryClient({
+    queryCache,
+    defaultOptions: {
+      queries: {
+        staleTime: 0,
+      },
+    },
+  })
+
+  beforeEach(() => {
+    queryCache.clear()
+  })
 
   it('should be able to open and close devtools', async () => {
     function Page() {
@@ -21,25 +37,109 @@ describe('ReactQueryDevtools', () => {
       )
     }
 
-    renderWithClient(queryClient, <Page />)
-
-    console.log(queryCache.find('check')?.isFetching())
+    renderWithClient(queryClient, <Page />, { initialIsOpen: false })
 
     // Since the initial is open state is false, expect the close button to not be present
     // in the DOM. Then find the open button and click on it.
-    const closeButton = screen.queryByRole('button', { name: /close react query devtools/i })
+    const closeButton = screen.queryByRole('button', {
+      name: /close react query devtools/i,
+    })
     expect(closeButton).toBeNull()
-    fireEvent.click(screen.getByRole('button', { name: /open react query devtools/i }))
+    fireEvent.click(
+      screen.getByRole('button', { name: /open react query devtools/i })
+    )
 
     // Wait for the animation to finish and the open button to be removed from DOM once the devtools
     // is opened. Then find the close button and click on it.
-    await waitForElementToBeRemoved(() => screen.queryByRole('button', { name: /open react query devtools/i }))
-    fireEvent.click(screen.getByRole('button', { name: /close react query devtools/i }))
+    await waitForElementToBeRemoved(() =>
+      screen.queryByRole('button', { name: /open react query devtools/i })
+    )
+    fireEvent.click(
+      screen.getByRole('button', { name: /close react query devtools/i })
+    )
 
     // Finally once the close animation is completed expect the open button to
     // be present in the DOM again.
     await screen.findByRole('button', { name: /open react query devtools/i })
   })
-})
 
-export {}
+  it('should display the correct query states', async () => {
+    function Page() {
+      const { data = 'default' } = useQuery(
+        'check',
+        async () => {
+          await sleep(100)
+          return 'test'
+        },
+        { staleTime: 300 }
+      )
+
+      return (
+        <div>
+          <h1>{data}</h1>
+        </div>
+      )
+    }
+
+    function PageParent() {
+      const [isPageVisible, setIsPageVisible] = React.useState(true)
+
+      return (
+        <div>
+          <button
+            type="button"
+            aria-label="Toggle page visibility"
+            onClick={() => setIsPageVisible(visible => !visible)}
+          >
+            Toggle Page
+          </button>
+          {isPageVisible && <Page />}
+        </div>
+      )
+    }
+
+    renderWithClient(queryClient, <PageParent />)
+
+    fireEvent.click(
+      screen.getByRole('button', { name: /open react query devtools/i })
+    )
+
+    // Find the current query from the cache
+    const currentQuery = queryCache.find('check')
+
+    // When the query is fetching then expect number of
+    // fetching queries to be 1
+    expect(currentQuery?.isFetching()).toEqual(true)
+    await screen.findByText(
+      getByTextContent('fresh (0) fetching (1) stale (0) inactive (0)')
+    )
+
+    // When we are done fetching the query doesn't go stale
+    // until 300ms after, so expect the number of fresh
+    // queries to be 1
+    await waitFor(() => {
+      expect(currentQuery?.isFetching()).toEqual(false)
+    })
+    await screen.findByText(
+      getByTextContent('fresh (1) fetching (0) stale (0) inactive (0)')
+    )
+
+    // Then wait for the query to go stale and then
+    // expect the number of stale queries to be 1
+    await waitFor(() => {
+      expect(currentQuery?.isStale()).toEqual(false)
+    })
+    await screen.findByText(
+      getByTextContent('fresh (0) fetching (0) stale (1) inactive (0)')
+    )
+
+    // Unmount the page component thus making the query inactive
+    // and expect number of inactive queries to be 1
+    fireEvent.click(
+      screen.getByRole('button', { name: /toggle page visibility/i })
+    )
+    await screen.findByText(
+      getByTextContent('fresh (0) fetching (0) stale (0) inactive (1)')
+    )
+  })
+})
--- src/devtools/tests/utils.tsx
+++ src/devtools/tests/utils.tsx
@@ -11,7 +11,7 @@ export function renderWithClient(
 ) {
   const { rerender, ...result } = render(
     <QueryClientProvider client={client}>
-      <ReactQueryDevtools initialIsOpen={false} {...devtoolsOptions} />
+      <ReactQueryDevtools {...devtoolsOptions} />
       {ui}
     </QueryClientProvider>
   )
@@ -31,4 +31,23 @@ export function sleep(timeout: number): Promise<void> {
   return new Promise(resolve => {
     setTimeout(resolve, timeout)
   })
+}
+
+/**
+ * This method is useful for matching by text content when the text is splitted
+ * across multiple different HTML elements which cannot be searched by normal
+ * *ByText methods. It returns a function that can be passed to the testing
+ * library's *ByText methods.
+ * @param textToMatch The string that needs to be matched
+ * @reference https://stackoverflow.com/a/56859650/8252081
+ */
+export const getByTextContent = (textToMatch: string) => (content: string, node: HTMLElement): boolean => {
+  const hasText = (currentNode: HTMLElement) =>
+    currentNode.textContent === textToMatch
+  const nodeHasText = hasText(node)
+  const childrenDontHaveText = Array.from(node.children).every(
+    child => !hasText(child as HTMLElement)
+  )
+
+  return nodeHasText && childrenDontHaveText
 }
