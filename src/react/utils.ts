19984fae8642d5885d5f4b88d0a84f9ed750074a
Daniel Friesen <d@danf.ca>
2021-09-03 16:53:09 -0700

    Allow useErrorBoundary can override suspense

--- src/react/utils.ts
+++ src/react/utils.ts
@@ -3,9 +3,14 @@ export function shouldThrowError<TError>(
   _useErrorBoundary: boolean | ((err: TError) => boolean) | undefined,
   error: TError
 ): boolean {
+  // Allow useErrorBoundary function to override throwing behavior on a per-error basis
   if (typeof _useErrorBoundary === 'function') {
     return _useErrorBoundary(error)
   }
 
-  return !!suspense || !!_useErrorBoundary
+  // Allow useErrorBoundary to override suspense's throwing behaviour
+  if (typeof _useErrorBoundary === 'boolean') return _useErrorBoundary
+
+  // If suspense is enabled default to throwing errors
+  return !!suspense
 }
