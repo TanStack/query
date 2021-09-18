a8f1cf6964152d3f528052ff264578ee3f7b67d4
Prateek Surana <prateeksurana3255@gmail.com>
2021-09-18 23:42:54 +0530

    ⬆️  Upgrade eslint-plugin-react-hooks

--- src/devtools/useLocalStorage.ts
+++ src/devtools/useLocalStorage.ts
@@ -28,7 +28,6 @@ export default function useLocalStorage<T>(
     } else {
       setValue(initialValue)
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [defaultValue, key])
 
   const setter = React.useCallback(
