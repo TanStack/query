cf8b0a67d8e8ff677278535da065f5e9a48f6de1
arnaudbzn <arnaud.bezancon@advantys.com>
2021-11-02 22:46:48 +0100

    fix(queryObserver): shouldNotifyListeners: remove useless condition
    updateResult function already check the result with shallowEqualObjects

--- src/core/queryObserver.ts
+++ src/core/queryObserver.ts
@@ -590,10 +590,6 @@ export class QueryObserver<
       return true
     }
 
-    if (result === prevResult) {
-      return false
-    }
-
     const { notifyOnChangeProps, notifyOnChangePropsExclusions } = this.options
 
     if (!notifyOnChangeProps && !notifyOnChangePropsExclusions) {
