454a38b1595604fa418daf0bd563050922827957
balazsmatepetro <petrobalazsmate@gmail.com>
2021-11-19 20:14:37 +0100

    chore: add a very basic codemod implementation and tests
    The current codemod is able to change the usages of `useQuery`
    according to the new API, so it will put the existing parameters into
    an object and pass it as the only parameter, but that's all. More
    changes coming soon.

---
+++ codemods/use_query/__testfixtures__/named-import.output.tsx
@@ -0,0 +1,15 @@
+import * as React from 'react'
+import { useQuery as useRenamedQuery } from "react-query";
+
+const Example = () => {
+    const { data } = useRenamedQuery({
+        queryKey: "repoData",
+
+        queryFn: () =>
+            fetch(
+                "https://api.github.com/repos/tannerlinsley/react-query"
+            ).then((res) => res.json())
+    });
+
+    return (<div>{JSON.stringify(data)}</div>);
+}
---
+++ codemods/use_query/__tests__/use-query.test.js
@@ -0,0 +1,12 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const defineTest = require('jscodeshift/dist/testUtils').defineTest
+
+jest.autoMockOff()
+
+defineTest(__dirname, 'use-query', null, 'default-import', {
+    parser: 'tsx',
+})
+
+defineTest(__dirname, 'use-query', null, 'named-import', {
+    parser: 'tsx',
+})
