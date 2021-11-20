27d29302e88ab5dcf4c0741d3fe2faa11cbab470
Prateek Surana <prateeksurana3255@gmail.com>
2021-11-20 21:29:20 +0530

    ✅   Update use infinite query tests

--- src/core/retryer.ts
+++ src/core/retryer.ts
@@ -144,6 +144,13 @@ export class Retryer<TData = unknown, TError = unknown> {
           reject(new CancelledError(cancelOptions))
 
           this.abort?.()
+
+          // Cancel transport if supported
+          if (isCancelable(promiseOrValue)) {
+            try {
+              promiseOrValue.cancel()
+            } catch {}
+          }
         }
       }
 
