2f976326f423f5f2bf13b36d7163169ffb526da2
Dominik Dorfmeister <office@dorfmeister.cc>
2021-11-21 16:09:09 +0100

    feat(useQuery): offline queries
    i messed up the icon order

--- src/devtools/devtools.tsx
+++ src/devtools/devtools.tsx
@@ -660,12 +660,13 @@ export const ReactQueryDevtoolsPanel = React.forwardRef<
                       }
                       style={{
                         padding: '0',
+                        height: '2em',
                       }}
                     >
                       <svg
                         xmlns="http://www.w3.org/2000/svg"
-                        width="23"
-                        height="23"
+                        width="2em"
+                        height="2em"
                         viewBox="0 0 24 24"
                         stroke={isMockOffline ? theme.danger : 'currentColor'}
                         fill="none"
@@ -675,17 +676,17 @@ export const ReactQueryDevtoolsPanel = React.forwardRef<
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <line x1="12" y1="18" x2="12.01" y2="18" />
                             <path d="M9.172 15.172a4 4 0 0 1 5.656 0" />
-                            <path d="M6.343 12.343a8 8 0 0 1 11.314 0" />
-                            <path d="M3.515 9.515c4.686 -4.687 12.284 -4.687 17 0" />
+                            <path d="M6.343 12.343a7.963 7.963 0 0 1 3.864 -2.14m4.163 .155a7.965 7.965 0 0 1 3.287 2" />
+                            <path d="M3.515 9.515a12 12 0 0 1 3.544 -2.455m3.101 -.92a12 12 0 0 1 10.325 3.374" />
+                            <line x1="3" y1="3" x2="21" y2="21" />
                           </>
                         ) : (
                           <>
                             <path stroke="none" d="M0 0h24v24H0z" fill="none" />
                             <line x1="12" y1="18" x2="12.01" y2="18" />
                             <path d="M9.172 15.172a4 4 0 0 1 5.656 0" />
-                            <path d="M6.343 12.343a7.963 7.963 0 0 1 3.864 -2.14m4.163 .155a7.965 7.965 0 0 1 3.287 2" />
-                            <path d="M3.515 9.515a12 12 0 0 1 3.544 -2.455m3.101 -.92a12 12 0 0 1 10.325 3.374" />
-                            <line x1="3" y1="3" x2="21" y2="21" />
+                            <path d="M6.343 12.343a8 8 0 0 1 11.314 0" />
+                            <path d="M3.515 9.515c4.686 -4.687 12.284 -4.687 17 0" />
                           </>
                         )}
                       </svg>
