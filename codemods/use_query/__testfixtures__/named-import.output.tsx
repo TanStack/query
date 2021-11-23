55158c5e8a4dad87508e46a940c8ca7ec9ff8d5a
balazsmatepetro <petrobalazsmate@gmail.com>
2021-11-23 13:06:44 +0100

    chore: fix `prettier` errors in `use_query` codemod

--- codemods/use_query/__testfixtures__/named-import.output.tsx
+++ codemods/use_query/__testfixtures__/named-import.output.tsx
@@ -1,15 +1,15 @@
 import * as React from 'react'
-import { useQuery as useRenamedQuery } from "react-query";
+import { useQuery as useRenamedQuery } from 'react-query'
 
 const Example = () => {
-    const { data } = useRenamedQuery({
-        queryKey: "repoData",
+  const { data } = useRenamedQuery({
+    queryKey: 'repoData',
 
-        queryFn: () =>
-            fetch(
-                "https://api.github.com/repos/tannerlinsley/react-query"
-            ).then((res) => res.json())
-    });
+    queryFn: () =>
+      fetch('https://api.github.com/repos/tannerlinsley/react-query').then(res =>
+        res.json()
+      )
+  })
 
-    return (<div>{JSON.stringify(data)}</div>);
+  return <div>{JSON.stringify(data)}</div>
 }
