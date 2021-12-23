b977cf4ddd912d2ff369ef06683e462be4155ebd
Damian Osipiuk <osipiukd+git@gmail.com>
2021-12-23 10:13:10 +0100

    refactor(types): use core types in useMutation instead of redefining them (#3122)

--- src/react/types.ts
+++ src/react/types.ts
@@ -1,15 +1,12 @@
-import { RetryValue, RetryDelayValue } from '../core/retryer'
 import {
   InfiniteQueryObserverOptions,
   InfiniteQueryObserverResult,
   MutationObserverResult,
-  MutationKey,
   QueryObserverOptions,
   QueryObserverResult,
   QueryKey,
-  MutationFunction,
-  MutateOptions,
-  MutationMeta,
+  MutationObserverOptions,
+  MutateFunction,
 } from '../core/types'
 
 export interface UseBaseQueryOptions<
@@ -73,33 +70,10 @@ export interface UseMutationOptions<
   TError = unknown,
   TVariables = void,
   TContext = unknown
-> {
-  mutationFn?: MutationFunction<TData, TVariables>
-  mutationKey?: MutationKey
-  onMutate?: (
-    variables: TVariables
-  ) => Promise<TContext | undefined> | TContext | undefined
-  onSuccess?: (
-    data: TData,
-    variables: TVariables,
-    context: TContext | undefined
-  ) => Promise<unknown> | void
-  onError?: (
-    error: TError,
-    variables: TVariables,
-    context: TContext | undefined
-  ) => Promise<unknown> | void
-  onSettled?: (
-    data: TData | undefined,
-    error: TError | null,
-    variables: TVariables,
-    context: TContext | undefined
-  ) => Promise<unknown> | void
-  retry?: RetryValue<TError>
-  retryDelay?: RetryDelayValue<TError>
-  useErrorBoundary?: boolean | ((error: TError) => boolean)
-  meta?: MutationMeta
-}
+> extends Omit<
+    MutationObserverOptions<TData, TError, TVariables, TContext>,
+    '_defaulted' | 'variables'
+  > {}
 
 export type UseMutateFunction<
   TData = unknown,
@@ -107,8 +81,7 @@ export type UseMutateFunction<
   TVariables = void,
   TContext = unknown
 > = (
-  variables: TVariables,
-  options?: MutateOptions<TData, TError, TVariables, TContext>
+  ...args: Parameters<MutateFunction<TData, TError, TVariables, TContext>>
 ) => void
 
 export type UseMutateAsyncFunction<
@@ -116,10 +89,7 @@ export type UseMutateAsyncFunction<
   TError = unknown,
   TVariables = void,
   TContext = unknown
-> = (
-  variables: TVariables,
-  options?: MutateOptions<TData, TError, TVariables, TContext>
-) => Promise<TData>
+> = MutateFunction<TData, TError, TVariables, TContext>
 
 export type UseBaseMutationResult<
   TData = unknown,
