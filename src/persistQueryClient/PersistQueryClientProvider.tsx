b301ce21be39acffca9a044fe130b991849e5968
Dominik Dorfmeister <office@dorfmeister.cc>
2022-02-12 15:23:37 +0100

    feat(persistQueryClient): PersistQueryClientProvider
    provide onSuccess and onError callbacks to PersistQueryClientProvider so that you can react to the persisting having finished, to e.g. have a point where you can resumePausedMutations

--- src/persistQueryClient/PersistQueryClientProvider.tsx
+++ src/persistQueryClient/PersistQueryClientProvider.tsx
@@ -1,34 +1,46 @@
 import React from 'react'
 
 import { persistQueryClient, PersistQueryClientOptions } from './persist'
-import {
-  QueryClientProvider,
-  QueryClientProviderProps,
-  IsHydratingProvider,
-} from '../reactjs'
+import { QueryClientProvider, QueryClientProviderProps } from '../reactjs'
+import { IsHydratingProvider } from '../reactjs/Hydrate'
 
 export interface PersistQueryClientProviderProps
   extends QueryClientProviderProps {
   persistOptions: Omit<PersistQueryClientOptions, 'queryClient'>
+  onSuccess?: () => void
+  onError?: () => void
 }
 
 export const PersistQueryClientProvider = ({
   client,
   children,
   persistOptions,
+  onSuccess,
+  onError,
   ...props
 }: PersistQueryClientProviderProps): JSX.Element => {
   const [isHydrating, setIsHydrating] = React.useState(true)
-  const options = React.useRef(persistOptions)
+  const refs = React.useRef({ persistOptions, onSuccess, onError })
+
+  React.useEffect(() => {
+    refs.current = { persistOptions, onSuccess, onError }
+  })
+
   React.useEffect(() => {
     const [unsubscribe, promise] = persistQueryClient({
-      ...options.current,
+      ...refs.current.persistOptions,
       queryClient: client,
     })
 
-    promise.then(() => {
-      setIsHydrating(false)
-    })
+    promise
+      .then(() => {
+        refs.current.onSuccess?.()
+        setIsHydrating(false)
+      })
+      .catch(() => {
+        refs.current.onError?.()
+        setIsHydrating(false)
+      })
 
     return unsubscribe
   }, [client])
