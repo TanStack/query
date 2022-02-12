da52e82f46868697cd62220d4ddb3114b93a5b46
Dominik Dorfmeister <office@dorfmeister.cc>
2022-02-12 20:57:56 +0100

    feat(persistQueryClient): PersistQueryClientProvider
    test for useQueries

--- src/persistQueryClient/tests/PersistQueryClientProvider.test.tsx
+++ src/persistQueryClient/tests/PersistQueryClientProvider.test.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { render, waitFor } from '@testing-library/react'
 
-import { QueryClient, useQuery, UseQueryResult } from '../..'
+import { QueryClient, useQuery, UseQueryResult, useQueries } from '../..'
 import { queryKey } from '../../reactjs/tests/utils'
 import { sleep } from '../../core/utils'
 import { PersistedClient, Persister, persistQueryClientSave } from '../persist'
@@ -41,6 +41,82 @@ describe('PersistQueryClientProvider', () => {
       const state = useQuery(key, async () => {
         await sleep(10)
         return 'fetched'
+      })
+
+      states.push(state)
+
+      return (
+        <div>
+          <h1>{state.data}</h1>
+          <h2>fetchStatus: {state.fetchStatus}</h2>
+        </div>
+      )
+    }
+
+    const rendered = render(
+      <PersistQueryClientProvider
+        client={queryClient}
+        persistOptions={{ persister }}
+      >
+        <Page />
+      </PersistQueryClientProvider>
+    )
+
+    await waitFor(() => rendered.getByText('fetchStatus: idle'))
+    await waitFor(() => rendered.getByText('hydrated'))
+    await waitFor(() => rendered.getByText('fetched'))
+
+    expect(states).toHaveLength(4)
+
+    expect(states[0]).toMatchObject({
+      status: 'loading',
+      fetchStatus: 'idle',
+      data: undefined,
+    })
+
+    expect(states[1]).toMatchObject({
+      status: 'success',
+      fetchStatus: 'fetching',
+      data: 'hydrated',
+    })
+
+    expect(states[2]).toMatchObject({
+      status: 'success',
+      fetchStatus: 'fetching',
+      data: 'hydrated',
+    })
+
+    expect(states[3]).toMatchObject({
+      status: 'success',
+      fetchStatus: 'idle',
+      data: 'fetched',
+    })
+  })
+
+  test('should also put useQueries into idle state', async () => {
+    const key = queryKey()
+    const states: UseQueryResult[] = []
+
+    const queryClient = new QueryClient()
+    await queryClient.prefetchQuery(key, () => Promise.resolve('hydrated'))
+
+    const persister = createMockPersister()
+
+    await persistQueryClientSave({ queryClient, persister })
+
+    queryClient.clear()
+
+    function Page() {
+      const [state] = useQueries({
+        queries: [
+          {
+            queryKey: key,
+            queryFn: async (): Promise<string> => {
+              await sleep(10)
+              return 'fetched'
+            },
+          },
+        ],
       })
 
       states.push(state)
