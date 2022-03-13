702159fa4c5bc05a52d849601a381efb366b6cf2
Dominik Dorfmeister <office@dorfmeister.cc>
2022-03-13 16:53:49 +0100

    offline example

---
+++ examples/offline/src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { worker } from "./api";
+
+worker.start();
+
+ReactDOM.render(
+  <React.StrictMode>
+    <div style={{ padding: "16px" }}>
+      <App />
+    </div>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
