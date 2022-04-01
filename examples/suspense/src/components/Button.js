2144842be0f419bc8d4845d3c0f6205999a11d08
Nika Sepiskveradze <nika.sepiskveradze@gmail.com>
2022-04-02 01:17:13 +0400

    docs: fix useTransition interface

--- examples/suspense/src/components/Button.js
+++ examples/suspense/src/components/Button.js
@@ -3,8 +3,8 @@ import React from "react";
 import Spinner from "./Spinner";
 
 export default function Button({ children, timeoutMs = 3000, onClick }) {
-  const [startTransition, isPending] = React.useTransition({
-    timeoutMs: timeoutMs
+  const [isPending, startTransition] = React.useTransition({
+    timeoutMs: timeoutMs,
   });
 
   const handleClick = e => {
