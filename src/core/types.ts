0c50b9399cff647c7cdc8c1f5a05040c4c365769
Dominik Dorfmeister <office@dorfmeister.cc>
2022-04-16 15:23:48 +0200

    feat(query): functions for refetchOn options
    docs

--- src/core/types.ts
+++ src/core/types.ts
@@ -135,6 +135,7 @@ export interface QueryObserverOptions<
    * If set to `true`, the query will refetch on window focus if the data is stale.
    * If set to `false`, the query will not refetch on window focus.
    * If set to `'always'`, the query will always refetch on window focus.
+   * If set to a function, the function will be executed with the latest data and query to compute the value.
    * Defaults to `true`.
    */
   refetchOnWindowFocus?:
@@ -148,6 +149,7 @@ export interface QueryObserverOptions<
    * If set to `true`, the query will refetch on reconnect if the data is stale.
    * If set to `false`, the query will not refetch on reconnect.
    * If set to `'always'`, the query will always refetch on reconnect.
+   * If set to a function, the function will be executed with the latest data and query to compute the value.
    * Defaults to `true`.
    */
   refetchOnReconnect?:
@@ -161,6 +163,7 @@ export interface QueryObserverOptions<
    * If set to `true`, the query will refetch on mount if the data is stale.
    * If set to `false`, will disable additional instances of a query to trigger background refetches.
    * If set to `'always'`, the query will always refetch on mount.
+   * If set to a function, the function will be executed with the latest data and query to compute the value
    * Defaults to `true`.
    */
   refetchOnMount?:
