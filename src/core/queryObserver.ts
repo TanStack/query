6df2ef28ac88b138c0b4aa72e147867cb289c3c9
Dominik Dorfmeister <office@dorfmeister.cc>
2022-05-13 20:41:40 +0200

    fix(query): updating with setQueryData should not affect fetchStatus
    fix logical condition that I screwed up; thank you, tests

--- src/core/queryObserver.ts
+++ src/core/queryObserver.ts
@@ -650,7 +650,7 @@ export class QueryObserver<
     const notifyOptions: NotifyOptions = {}
 
     if (action.type === 'success') {
-      notifyOptions.onSuccess = action.manual ?? true
+      notifyOptions.onSuccess = !action.manual
     } else if (action.type === 'error' && !isCancelledError(action.error)) {
       notifyOptions.onError = true
     }
