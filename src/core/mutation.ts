b9a3a9c926937ecaaecf6e44597c478f025a5e3e
Sunil Hari <isunilhari@gmail.com>
2022-05-29 17:50:33 +0530

    fix(*): change types
    changes context to optional in onSuccess callback

--- src/core/mutation.ts
+++ src/core/mutation.ts
@@ -178,7 +178,7 @@ export class Mutation<
         this.options.onSuccess?.(
           data,
           this.state.variables!,
-          this.state.context!
+          this.state.context
         )
       )
       .then(() =>
