ddafe0f0ceb90180c4e2a49e00d6c509ad98ae27
Fedir Ushakov <pmahotsava@gmail.com>
2022-06-06 08:19:53 +0300

    fix(persist): introduce in-house synchronous Storage interface

--- src/createWebStoragePersistor-experimental/index.ts
+++ src/createWebStoragePersistor-experimental/index.ts
@@ -1,6 +1,12 @@
 import { noop } from '../core/utils'
 import { PersistedClient, Persistor } from '../persistQueryClient-experimental'
 
+interface Storage {
+  getItem: (key: string) => string | null
+  setItem: (key: string, value: string) => void
+  removeItem: (key: string) => void
+}
+
 interface CreateWebStoragePersistorOptions {
   /** The storage client used for setting an retrieving items from cache */
   storage: Storage
