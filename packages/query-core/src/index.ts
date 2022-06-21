e33a3e6a8d5c09e053a028662c5151921bf25116
Dominik Dorfmeister <office@dorfmeister.cc>
2022-06-21 14:12:44 +0200

    chore: export types separately to make the build work

--- packages/query-core/src/index.ts
+++ packages/query-core/src/index.ts
@@ -16,9 +16,8 @@ export {
   parseFilterArgs,
   parseMutationFilterArgs,
   parseMutationArgs,
-  MutationFilters,
-  QueryFilters,
 } from './utils'
+export type { MutationFilters, QueryFilters } from './utils'
 export { isCancelledError } from './retryer'
 export { dehydrate, hydrate } from './hydration'
 
