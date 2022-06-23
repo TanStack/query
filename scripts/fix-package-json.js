f007065fb4a05f88cb8a85cad147ab90eec3f872
Dominik Dorfmeister <office@dorfmeister.cc>
2022-06-23 08:42:20 +0200

    fix: apply private: false to all workspaces

--- scripts/fix-package-json.js
+++ scripts/fix-package-json.js
@@ -1,4 +1,20 @@
-const path = __dirname + '/../package.json'
-const pkg = require(path)
-pkg.private = false
-require('fs').writeFileSync(path, JSON.stringify(pkg, null, 2))
+const fs = require('fs')
+
+const root = __dirname + '/../'
+const packageJsonPath = root + 'package.json'
+
+function removePrivate(name) {
+  const pkg = require(name)
+  if (pkg.private === true) {
+    pkg.private = false
+    fs.writeFileSync(name, JSON.stringify(pkg, null, 2))
+  }
+}
+
+removePrivate(packageJsonPath)
+
+const packageJson = require(packageJsonPath)
+
+packageJson.workspaces.forEach((workspace) => {
+  removePrivate(root + workspace + '/package.json')
+})
