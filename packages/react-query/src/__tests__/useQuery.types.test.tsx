1cfc8386ed16f5327114ee6c33d48de7a9c9ce5c
Matt Pocock <mattpocockvoice@gmail.com>
2022-07-16 10:04:27 +0100

    Fleshed out test

--- packages/react-query/src/__tests__/useQuery.types.test.tsx
+++ packages/react-query/src/__tests__/useQuery.types.test.tsx
@@ -1,5 +1,7 @@
 import { useQuery } from '../useQuery'
 
+const assert = <T,>(type: T) => void 0
+
 describe('useQuery', () => {
   it('TData should always be defined when initialData is provided', () => {
     const { data } = useQuery({
@@ -13,5 +15,20 @@ describe('useQuery', () => {
         wow: true,
       },
     })
+
+    assert<{ wow: boolean }>(data)
+  })
+
+  it('TData should have undefined in the union when initialData is NOT provided', () => {
+    const { data } = useQuery({
+      //    ^?
+      queryFn: () => {
+        return {
+          wow: true,
+        }
+      },
+    })
+
+    assert<{ wow: boolean } | undefined>(data)
   })
 })
