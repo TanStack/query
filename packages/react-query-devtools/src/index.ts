d673a82da2eea0575bb59eacd686f705a556a2b6
Dominik Dorfmeister <office@dorfmeister.cc>
2022-07-19 19:40:43 +0200

    fix(react-query-devtools): only export devtools in development mode
    revert the node_env check

--- packages/react-query-devtools/src/index.ts
+++ packages/react-query-devtools/src/index.ts
@@ -1,12 +1,1 @@
-if (process.env.NODE_ENV !== 'development') {
-  module.exports = {
-    ReactQueryDevtools: function () {
-      return null
-    },
-    ReactQueryDevtoolsPanel: function () {
-      return null
-    },
-  }
-} else {
-  module.exports = require('./devtools')
-}
+export * from './devtools'
