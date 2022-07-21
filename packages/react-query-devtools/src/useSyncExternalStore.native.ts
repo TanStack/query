b12155d822d4d05fba2451ddae29ef32924f397c
Damian Osipiuk <osipiukd+git@gmail.com>
2022-07-21 10:28:43 +0200

    fix: react-native uSES usage

---
+++ packages/react-query-devtools/src/useSyncExternalStore.native.ts
@@ -0,0 +1,3 @@
+// Temporary workaround due to an issue with react-native uSES - https://github.com/TanStack/query/pull/3601
+// @ts-ignore
+export { useSyncExternalStore } from 'use-sync-external-store/shim/index.native.js'
---
+++ packages/react-query-devtools/src/useSyncExternalStore.ts
@@ -0,0 +1,2 @@
+// Temporary workaround due to an issue with react-native uSES - https://github.com/TanStack/query/pull/3601
+export { useSyncExternalStore } from 'use-sync-external-store/shim/index.js'
