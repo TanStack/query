e42ae646f9862cc6b0914bcbf5c11bcda6c05a3a
Dominik Dorfmeister <office@dorfmeister.cc>
2022-08-20 11:36:34 +0200

    docs(examples): index route

--- examples/react/react-router/src/routes/index.jsx
+++ examples/react/react-router/src/routes/index.jsx
@@ -2,11 +2,16 @@ import * as React from "react";
 
 export default function Index() {
   return (
-    <p id="index">
-      This is a demo for React Router.
+    <p id="zero-state">
+      This is a demo for integrating React Router with React Query.
       <br />
       Check out{" "}
-      <a href="https://reactrouter.com/">the docs at reactrouter.com</a>.
+      <a href="https://reactrouter.com/">
+        the docs at reactrouter.com
+      </a> and{" "}
+      <a href="https://tanstack.com/query/v4/docs/overview">
+        the docs at tanstack.com
+      </a>.
     </p>
   );
 }
