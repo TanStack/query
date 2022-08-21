af01805254235b8a7f221097c2c178cbcb51343e
Nicholas Griffin <me@nicholasgriffin.co.uk>
2022-08-21 22:10:44 +0100

    fix: Adding screenreader text to the DevTools open icon

---
+++ packages/react-query-devtools/src/screenreader.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+export default function ScreenReader({ text }: { text: string }) {
+  return (
+    <span
+      style={{
+        position: 'absolute',
+        width: '0.1px',
+        height: '0.1px',
+        overflow: 'hidden',
+      }}
+      className="screenreader"
+    >
+      {text}
+    </span>
+  )
+}
