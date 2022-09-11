532d21a08d2d5a1b51bc78a7f02837319949aabf
Jen Kaplan <jenniferckaplan@gmail.com>
2022-09-11 15:38:07 -0400

    fix: jest tests, copy over React query index file

--- packages/solid-query/src/index.ts
+++ packages/solid-query/src/index.ts
@@ -1,7 +1,26 @@
-export * from './createQuery'
-export * from './createInfiniteQuery'
-export * from './QueryClientProvider'
-export * from './createMutation'
-export * from './useIsMutating'
-export * from './useIsFetching'
-export { QueryClient } from '@tanstack/query-core'
+// Re-export core
+export * from '@tanstack/query-core'
+
+// Solid Query
+export * from './types'
+// export { useQueries } from './useQueries'
+// export type { QueriesResults, QueriesOptions } from './useQueries'
+export { createQuery } from './createQuery'
+export {
+  QueryClientContext as defaultContext,
+  QueryClientProvider,
+  useQueryClient,
+} from './QueryClientProvider'
+// export type { QueryClientProviderProps } from './QueryClientProvider'
+// export type { QueryErrorResetBoundaryProps } from './QueryErrorResetBoundary'
+// export { useHydrate, Hydrate } from './Hydrate'
+// export type { HydrateProps } from './Hydrate'
+// export {
+//   QueryErrorResetBoundary,
+//   useQueryErrorResetBoundary,
+// } from './QueryErrorResetBoundary'
+export { useIsFetching } from './useIsFetching'
+export { useIsMutating } from './useIsMutating'
+export { createMutation } from './createMutation'
+export { createInfiniteQuery } from './createInfiniteQuery'
+// export { useIsRestoring, IsRestoringProvider } from './isRestoring'
