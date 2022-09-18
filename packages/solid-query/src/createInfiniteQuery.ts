324ebcf2415aa99079c3fbe581094ecdbbdea749
Aryan Deora <adeora@iu.edu>
2022-09-18 17:29:22 -0400

    Prettier fix for infiniteQueries

--- packages/solid-query/src/createInfiniteQuery.ts
+++ packages/solid-query/src/createInfiniteQuery.ts
@@ -1,9 +1,18 @@
-import { QueryObserver, InfiniteQueryObserver, QueryFunction, QueryOptions } from "@tanstack/query-core";
-import { CreateInfiniteQueryOptions, CreateInfiniteQueryResult, SolidQueryKey } from "./types";
-import { createBaseQuery } from "./createBaseQuery";
-import { createComputed } from "solid-js";
-import { createStore } from "solid-js/store";
-import { parseQueryArgs } from "./utils";
+import {
+  QueryObserver,
+  InfiniteQueryObserver,
+  QueryFunction,
+  QueryOptions,
+} from '@tanstack/query-core'
+import {
+  CreateInfiniteQueryOptions,
+  CreateInfiniteQueryResult,
+  SolidQueryKey,
+} from './types'
+import { createBaseQuery } from './createBaseQuery'
+import { createComputed } from 'solid-js'
+import { createStore } from 'solid-js/store'
+import { parseQueryArgs } from './utils'
 
 export function createInfiniteQuery<
   TQueryFnData = unknown,
@@ -11,8 +20,14 @@ export function createInfiniteQuery<
   TData = TQueryFnData,
   TQueryKey extends SolidQueryKey = SolidQueryKey,
 >(
-  options: CreateInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
-): CreateInfiniteQueryResult<TData, TError>;
+  options: CreateInfiniteQueryOptions<
+    TQueryFnData,
+    TError,
+    TData,
+    TQueryFnData,
+    TQueryKey
+  >,
+): CreateInfiniteQueryResult<TData, TError>
 export function createInfiniteQuery<
   TQueryFnData = unknown,
   TError = unknown,
@@ -21,10 +36,16 @@ export function createInfiniteQuery<
 >(
   queryKey: TQueryKey,
   options?: Omit<
-    CreateInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
-    "queryKey"
+    CreateInfiniteQueryOptions<
+      TQueryFnData,
+      TError,
+      TData,
+      TQueryFnData,
+      TQueryKey
+    >,
+    'queryKey'
   >,
-): CreateInfiniteQueryResult<TData, TError>;
+): CreateInfiniteQueryResult<TData, TError>
 export function createInfiniteQuery<
   TQueryFnData = unknown,
   TError = unknown,
@@ -34,10 +55,16 @@ export function createInfiniteQuery<
   queryKey: TQueryKey,
   queryFn: QueryFunction<TQueryFnData, ReturnType<TQueryKey>>,
   options?: Omit<
-    CreateInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
-    "queryKey" | "queryFn"
+    CreateInfiniteQueryOptions<
+      TQueryFnData,
+      TError,
+      TData,
+      TQueryFnData,
+      TQueryKey
+    >,
+    'queryKey' | 'queryFn'
   >,
-): CreateInfiniteQueryResult<TData, TError>;
+): CreateInfiniteQueryResult<TData, TError>
 export function createInfiniteQuery<
   TQueryFnData,
   TError,
@@ -46,24 +73,44 @@ export function createInfiniteQuery<
 >(
   arg1:
     | TQueryKey
-    | CreateInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
+    | CreateInfiniteQueryOptions<
+        TQueryFnData,
+        TError,
+        TData,
+        TQueryFnData,
+        TQueryKey
+      >,
   arg2?:
     | QueryFunction<TQueryFnData, ReturnType<TQueryKey>>
-    | CreateInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
-  arg3?: CreateInfiniteQueryOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
+    | CreateInfiniteQueryOptions<
+        TQueryFnData,
+        TError,
+        TData,
+        TQueryFnData,
+        TQueryKey
+      >,
+  arg3?: CreateInfiniteQueryOptions<
+    TQueryFnData,
+    TError,
+    TData,
+    TQueryFnData,
+    TQueryKey
+  >,
 ): CreateInfiniteQueryResult<TData, TError> {
   // The parseQuery Args functions helps normalize the arguments into the correct form.
   // Whatever the parameters are, they are normalized into the correct form.
-  const [parsedOptions, setParsedOptions] = createStore(parseQueryArgs(arg1, arg2, arg3));
+  const [parsedOptions, setParsedOptions] = createStore(
+    parseQueryArgs(arg1, arg2, arg3),
+  )
 
   // Watch for changes in the options and update the parsed options.
   createComputed(() => {
-    const newParsedOptions = parseQueryArgs(arg1, arg2, arg3);
-    setParsedOptions(newParsedOptions);
-  });
+    const newParsedOptions = parseQueryArgs(arg1, arg2, arg3)
+    setParsedOptions(newParsedOptions)
+  })
 
   return createBaseQuery(
     parsedOptions as QueryOptions<any, any, any, ReturnType<TQueryKey>>,
     InfiniteQueryObserver as typeof QueryObserver,
-  ) as CreateInfiniteQueryResult<TData, TError>;
+  ) as CreateInfiniteQueryResult<TData, TError>
 }
