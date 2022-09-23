26c0a3add1678066af5ef93d26275f77d48e8c17
Luke Murray <lukepigeonmail@gmail.com>
2022-09-23 11:20:41 -0400

    feat(solid-query): Solid Query Adapter for TanStack Query
    Add an adapter to TanStack Query to support SolidJS.
    
    Co-authored-by: Aryan Deora <adeora@iu.edu>
    Co-authored-by: Jen Kaplan <jenniferckaplan@gmail.com>

---
+++ packages/solid-query/src/types.ts
@@ -0,0 +1,167 @@
+import type { Context } from 'solid-js'
+import type {
+  QueryClient,
+  QueryKey,
+  QueryObserverOptions,
+  QueryObserverResult,
+  MutateFunction,
+  MutationObserverOptions,
+  MutationObserverResult,
+  DefinedQueryObserverResult,
+  InfiniteQueryObserverOptions,
+  InfiniteQueryObserverResult,
+  QueryFilters,
+  QueryOptions,
+} from '@tanstack/query-core'
+
+export interface ContextOptions {
+  /**
+   * Use this to pass your Solid Query context. Otherwise, `defaultContext` will be used.
+   */
+  context?: Context<QueryClient | undefined>
+}
+
+/* --- Create Query and Create Base Query  Types --- */
+export type SolidQueryKey = () => readonly unknown[]
+
+export interface CreateBaseQueryOptions<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryData = TQueryFnData,
+  TQueryKey extends QueryKey = QueryKey,
+> extends ContextOptions,
+    QueryObserverOptions<TQueryFnData, TError, TData, TQueryData, TQueryKey> {}
+
+export interface CreateQueryOptions<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryKey extends () => readonly unknown[] = SolidQueryKey,
+> extends Omit<
+    CreateBaseQueryOptions<
+      TQueryFnData,
+      TError,
+      TData,
+      TQueryFnData,
+      ReturnType<TQueryKey>
+    >,
+    'queryKey'
+  > {
+  queryKey?: TQueryKey
+}
+
+export type CreateBaseQueryResult<
+  TData = unknown,
+  TError = unknown,
+> = QueryObserverResult<TData, TError>
+
+export type CreateQueryResult<
+  TData = unknown,
+  TError = unknown,
+> = CreateBaseQueryResult<TData, TError>
+
+export type DefinedCreateBaseQueryResult<
+  TData = unknown,
+  TError = unknown,
+> = DefinedQueryObserverResult<TData, TError>
+
+export type DefinedCreateQueryResult<
+  TData = unknown,
+  TError = unknown,
+> = DefinedCreateBaseQueryResult<TData, TError>
+
+export type ParseQueryArgs<
+  TOptions extends Omit<
+    QueryOptions<any, any, any, ReturnType<TQueryKey>>,
+    'queryKey'
+  > & {
+    queryKey?: TQueryKey
+  },
+  TQueryKey extends () => readonly unknown[] = SolidQueryKey,
+> = TOptions['queryKey'] extends () => infer R
+  ? Omit<TOptions, 'queryKey'> & { queryKey?: R }
+  : TOptions
+
+/* --- Create Infinite Queries Types --- */
+export interface CreateInfiniteQueryOptions<
+  TQueryFnData = unknown,
+  TError = unknown,
+  TData = TQueryFnData,
+  TQueryData = TQueryFnData,
+  TQueryKey extends () => readonly unknown[] = SolidQueryKey,
+> extends ContextOptions,
+    Omit<
+      InfiniteQueryObserverOptions<
+        TQueryFnData,
+        TError,
+        TData,
+        TQueryData,
+        ReturnType<TQueryKey>
+      >,
+      'queryKey'
+    > {
+  queryKey?: TQueryKey
+}
+
+export type CreateInfiniteQueryResult<
+  TData = unknown,
+  TError = unknown,
+> = InfiniteQueryObserverResult<TData, TError>
+
+/* --- Create Mutation Types --- */
+export interface CreateMutationOptions<
+  TData = unknown,
+  TError = unknown,
+  TVariables = void,
+  TContext = unknown,
+> extends ContextOptions,
+    Omit<
+      MutationObserverOptions<TData, TError, TVariables, TContext>,
+      '_defaulted' | 'variables'
+    > {}
+
+export type CreateMutateFunction<
+  TData = unknown,
+  TError = unknown,
+  TVariables = void,
+  TContext = unknown,
+> = (
+  ...args: Parameters<MutateFunction<TData, TError, TVariables, TContext>>
+) => void
+
+export type CreateMutateAsyncFunction<
+  TData = unknown,
+  TError = unknown,
+  TVariables = void,
+  TContext = unknown,
+> = MutateFunction<TData, TError, TVariables, TContext>
+
+export type CreateBaseMutationResult<
+  TData = unknown,
+  TError = unknown,
+  TVariables = unknown,
+  TContext = unknown,
+> = Override<
+  MutationObserverResult<TData, TError, TVariables, TContext>,
+  { mutate: CreateMutateFunction<TData, TError, TVariables, TContext> }
+> & {
+  mutateAsync: CreateMutateAsyncFunction<TData, TError, TVariables, TContext>
+}
+
+export type CreateMutationResult<
+  TData = unknown,
+  TError = unknown,
+  TVariables = unknown,
+  TContext = unknown,
+> = CreateBaseMutationResult<TData, TError, TVariables, TContext>
+
+type Override<A, B> = { [K in keyof A]: K extends keyof B ? B[K] : A[K] }
+
+/* --- Use Is Fetching Types --- */
+export interface SolidQueryFilters extends Omit<QueryFilters, 'queryKey'> {
+  queryKey?: SolidQueryKey
+}
+
+export type ParseFilterArgs<T extends SolidQueryFilters> =
+  T['queryKey'] extends () => infer R ? T & { queryKey: R } : T
---
+++ packages/solid-query/src/useIsMutating.ts
@@ -0,0 +1,44 @@
+import {
+  MutationKey,
+  MutationFilters,
+  parseMutationFilterArgs,
+} from '@tanstack/query-core'
+import { ContextOptions } from './types'
+import { useQueryClient } from './QueryClientProvider'
+import { Accessor, createSignal, onCleanup } from 'solid-js'
+
+interface Options extends ContextOptions {}
+
+export function useIsMutating(
+  filters?: MutationFilters,
+  options?: Options,
+): Accessor<number>
+export function useIsMutating(
+  mutationKey?: MutationKey,
+  filters?: Omit<MutationFilters, 'mutationKey'>,
+  options?: Options,
+): Accessor<number>
+export function useIsMutating(
+  arg1?: MutationKey | MutationFilters,
+  arg2?: Omit<MutationFilters, 'mutationKey'> | Options,
+  arg3?: Options,
+): Accessor<number> {
+  const [filters, options = {}] = parseMutationFilterArgs(arg1, arg2, arg3)
+
+  const queryClient = useQueryClient({ context: options.context })
+  const mutationCache = queryClient.getMutationCache()
+
+  const [mutations, setMutations] = createSignal(
+    queryClient.isMutating(filters),
+  )
+
+  const unsubscribe = mutationCache.subscribe((_result) => {
+    setMutations(queryClient.isMutating(filters))
+  })
+
+  onCleanup(() => {
+    unsubscribe()
+  })
+
+  return mutations
+}
