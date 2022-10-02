90131f9710b27c24a6b24690fb02fdc7943af3ad
Damian Osipiuk <osipiukd+git@gmail.com>
2022-10-02 13:43:01 +0200

    chore: add basic example

---
+++ examples/vue/basic/vite.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ["remove-accents"],
+    exclude: ["vue-query", "vue-demi"],
+  },
+});
