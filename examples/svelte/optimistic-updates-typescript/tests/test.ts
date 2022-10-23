d352e80ff52f9da71c5a4cf42365b648345a80ec
DeAndre Johnson <dre@phreshr.com>
2022-10-23 11:35:29 +0800

    delete tests in examples

--- examples/svelte/optimistic-updates-typescript/tests/test.ts
+++
@@ -1,6 +0,0 @@
-import { expect, test } from '@playwright/test';
-
-test('index page has expected h1', async ({ page }) => {
-	await page.goto('/');
-	expect(await page.textContent('h1')).toBe('Welcome to SvelteKit');
-});
