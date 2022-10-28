b694c1a6e70b7d4418b011e957d1e1c6dd51b4f0
Milan Hauth <milahu@gmail.com>
2022-10-28 19:51:21 +0200

    examples/solid/offline fix: start worker

--- examples/solid/offline/src/index.tsx
+++ examples/solid/offline/src/index.tsx
@@ -3,10 +3,14 @@ import { render } from 'solid-js/web'
 import { App } from './App'
 import { Router } from "@solidjs/router";
 
+import { worker } from "./api"
+
 const app = () => (
   <Router>
     <App />
   </Router>
 )
+
+worker.start()
 
 render(app, document.getElementById('root') as HTMLElement)
