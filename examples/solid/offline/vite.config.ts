328fbd00c07ed9214cd38a61da7fc66663943722
Milan Hauth <milahu@gmail.com>
2022-10-28 21:20:42 +0200

    examples/solid/offline vite.config.ts mention vite-plugin-pwa

--- examples/solid/offline/vite.config.ts
+++ examples/solid/offline/vite.config.ts
@@ -1,8 +1,17 @@
 import { defineConfig } from 'vite'
 import solidPlugin from 'vite-plugin-solid'
 
+// Progressive Web App
+// https://vite-pwa-org.netlify.app/guide/
+// based on workbox
+// https://web.dev/learn/pwa/workbox/
+//import { VitePWA } from 'vite-plugin-pwa'
+
 export default defineConfig({
-  plugins: [solidPlugin()],
+  plugins: [
+    solidPlugin(),
+    //VitePWA({ registerType: 'autoUpdate' }),
+  ],
   server: {
     port: 3000,
   },
