891aa3d2fab879c12b411ddc5741d24d47b500e2
Milan Hauth <milahu@gmail.com>
2022-10-29 12:37:36 +0200

    examples/solid/offline update src/App.tsx

--- examples/solid/offline/src/App.tsx
+++ examples/solid/offline/src/App.tsx
@@ -112,7 +112,6 @@ function Movies() {
   const isRestoring = useIsRestoring();
   return (
     <div>
-      <h1>Movies</h1>
       <Routes>
         <Route
           path="/"
@@ -123,7 +122,7 @@ function Movies() {
           component={Detail}
           // TODO
           //errorComponent={MovieError}
-          //data={({ params: { movieId } }: { params: { movieId: string } }) =>
+          //data={({ props: { movieId } }: { props: { movieId: string } }) =>
           //data={fetchMovie}
         />
         <Route
@@ -140,51 +139,72 @@ function Movies() {
 }
 
 function List() {
+  console.log('List: createQuery', movieKeys.list(), api.fetchMovies)
   const moviesQuery = createQuery(
     () => movieKeys.list(),
     api.fetchMovies
   );
 
-  if (moviesQuery.isLoading && moviesQuery.isFetching) {
-    return "Loading...";
-  }
-
-  if (moviesQuery.data) {
-    return (
+  return (
+    <div>
+      <h1>Movies</h1>
       <div>
-        <h1>Movies</h1>
-        <p>
-          Try to mock offline behaviour with the button in the devtools. You can
-          navigate around as long as there is already data in the cache. You'll
-          get a refetch as soon as you go online again.
-        </p>
-        <ul>
-          {moviesQuery.data.movies.map((movie) => (
-            <li id={movie.id}>
-              {
-                //<Link href={`./${movie.id}`} preload>
-              }
-              <Link href={`./${movie.id}`}>
-                {movie.title}
-              </Link>
-            </li>
-          ))}
-        </ul>
-        <div>
-          Updated at: {new Date(moviesQuery.data.ts).toLocaleTimeString()}
-        </div>
-        <div>{moviesQuery.isFetching && "fetching..."}</div>
+        <Switch>
+          <Match when={moviesQuery.status === 'loading'}>Loading...</Match>
+          <Match when={moviesQuery.isPaused}>We're offline and have no data to show :(</Match>
+          <Match when={moviesQuery.error instanceof Error}>
+            <span>Error: {(moviesQuery.error as Error).message}</span>
+          </Match>
+          <Match when={moviesQuery.data !== undefined}>
+            <>
+              {/* TODO solid devtools
+              <p>
+                Try to mock offline behaviour with the button in the devtools. You can
+                navigate around as long as there is already data in the cache. You'll
+                get a refetch as soon as you go online again.
+              </p>
+              */}
+              <ul>
+                <For each={moviesQuery.data?.movies}>
+                  {(movie) => (
+                    <li
+                      id={movie.id}
+                      style={
+                        // We can access the query data here to show bold links for
+                        // ones that are cached
+                        queryClient.getQueryData(['post', movie.id])
+                          ? {
+                              'font-weight': 'bold',
+                              color: 'green',
+                            }
+                          : {}
+                      }
+                    >
+                      {
+                        // TODO preload
+                        //<Link href={`./${movie.id}`} preload>
+                      }
+                      <Link href={`./${movie.id}`}>
+                        {movie.title}
+                      </Link>
+                    </li>
+                  )}
+                </For>
+              </ul>
+              <div>
+                Updated at: {new Date(moviesQuery.data?.ts || 0).toLocaleTimeString()}
+              </div>
+              <div>{moviesQuery.isFetching ? 'fetching...' : ' '}</div>
+            </>
+          </Match>
+          {
+            // query will be in 'idle' fetchStatus while restoring from localStorage
+          }
+          <Match when={true}>restoring...</Match>
+        </Switch>
       </div>
-    );
-  }
-
-  else if (moviesQuery.isPaused) {
-    return "We're offline and have no data to show :(";
-  }
-
-  // query will be in 'idle' fetchStatus while restoring from localStorage
-  //return null;
-  return "No data"
+    </div>
+  )
 }
 
 /* TODO
@@ -203,10 +223,10 @@ function MovieError() {
 
 function Detail() {
 
-  const params = useParams();
+  const props = useParams();
 
   /*
-  const fetchMovie = ({ params: { movieId } }: { params: any }) => {
+  const fetchMovie = ({ props: { movieId } }: { props: any }) => {
     console.log(`fetchMovie: arguments`, arguments)
     return queryClient.getQueryData(movieKeys.detail(movieId)) ??
     // do not load if we are offline or hydrating because it returns a promise that is pending until we go online again
@@ -219,62 +239,76 @@ function Detail() {
   }
   */
 
-  const { comment, setComment, updateMovie, movieQuery } = useMovie(params.movieId);
-
-  if (movieQuery.isLoading && movieQuery.isFetching) {
-    return "Loading...";
-  }
+  const { comment, setComment, updateMovie, movieQuery } = useMovie(props.movieId);
 
   function submitForm(event: any) {
     event.preventDefault();
 
     updateMovie.mutate({
-      id: params.movieId,
-      comment,
+      id: props.movieId,
+      comment: comment(),
     } as any);
   }
 
-  if (movieQuery.data) {
-    return (
-      <form onSubmit={submitForm}>
-        <Link href="..">Back</Link>
-        <h1>Movie: {movieQuery.data.movie.title}</h1>
-        <p>
-          Try to mock offline behaviour with the button in the devtools, then
-          update the comment. The optimistic update will succeed, but the actual
-          mutation will be paused and resumed once you go online again.
-        </p>
-        <p>
-          You can also reload the page, which will make the persisted mutation
-          resume, as you will be online again when you "come back".
-        </p>
-        <p>
-          <label>
-            Comment: <br />
-            <textarea
-              name="comment"
-              value={comment as any}
-              onChange={(event: any) => setComment(event.target.value)}
-            />
-          </label>
-        </p>
-        <button type="submit">Submit</button>
-        <div>
-          Updated at: {new Date(movieQuery.data.ts).toLocaleTimeString()}
-        </div>
-        <div>{movieQuery.isFetching && "fetching..."}</div>
-        <div>
-          {updateMovie.isPaused
-            ? "mutation paused - offline"
-            : updateMovie.isLoading && "updating..."}
-        </div>
-      </form>
-    );
-  }
-
-  else if (movieQuery.isPaused) {
-    return "We're offline and have no data to show :(";
-  }
-
-  return "No data"
+  return (
+    <div>
+      <Switch>
+        <Match when={!props.movieId}>
+          No movieId
+        </Match>
+        <Match when={movieQuery.status === 'loading'}>
+          Loading...
+        </Match>
+        <Match when={movieQuery.isPaused}>
+          We're offline and have no data to show :(
+        </Match>
+        <Match when={movieQuery.error instanceof Error}>
+          <span>Error: {(movieQuery.error as Error).message}</span>
+        </Match>
+        <Match when={movieQuery.data !== undefined}>
+          <form onSubmit={submitForm}>
+            <Link href="..">Back</Link>
+            <h1>Movie: {movieQuery.data?.movie.title}</h1>
+            {/*
+            <p>
+              Try to mock offline behaviour with the button in the devtools, then
+              update the comment. The optimistic update will succeed, but the actual
+              mutation will be paused and resumed once you go online again.
+            </p>
+            */}
+            <p>
+              You can also reload the page, which will make the persisted mutation
+              resume, as you will be online again when you "come back".
+            </p>
+            <p>
+              <label>
+                Comment:<br/>
+                <textarea
+                  name="comment"
+                  value={comment()} // FIXME undefined after mutate, because reactivity is lost in persister
+                  //value={comment() || ''}
+                  //value={movieQuery.data?.movie.comment}
+                  onChange={(event: any) => setComment(event.target.value)}
+                />
+              </label>
+            </p>
+            <button type="submit">Submit</button>
+            <div>
+              Updated at: {new Date(movieQuery.data?.ts || 0).toLocaleTimeString()}
+            </div>
+            <div>{movieQuery.isFetching && "fetching..."}</div>
+            <div>
+              {updateMovie.isPaused
+                ? "mutation paused - offline"
+                : updateMovie.isLoading && "updating..."}
+            </div>
+          </form>
+        </Match>
+        {
+          // query will be in 'idle' fetchStatus while restoring from localStorage
+        }
+        <Match when={true}>restoring...</Match>
+      </Switch>
+    </div>
+  )
 }
