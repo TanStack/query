432017f9f05454c6dd951ab6cf944b9f854e1ab7
Newbie012 <co.eliya2@gmail.com>
2022-10-31 00:03:05 +0200

    fix indention

--- packages/eslint-plugin-query/src/rules/exhaustive-deps/exhaustive-deps.rule.ts
+++ packages/eslint-plugin-query/src/rules/exhaustive-deps/exhaustive-deps.rule.ts
@@ -24,92 +24,91 @@ export const rule = createRule({
   },
   defaultOptions: [],
 
-  create(context, _) {
+  create(context) {
     return {
       Property(node) {
-        if (ASTUtils.isIdentifierWithName(node.key, QUERY_KEY)) {
-          if (
-            node.parent === undefined ||
-            !ASTUtils.isObjectExpression(node.parent)
-          ) {
-            return
-          }
+        if (
+          node.parent === undefined ||
+          !ASTUtils.isObjectExpression(node.parent) ||
+          !ASTUtils.isIdentifierWithName(node.key, QUERY_KEY)
+        ) {
+          return
+        }
 
-          const scopeManager = context.getSourceCode().scopeManager
-          const queryKey = ASTUtils.findPropertyWithIdentifierKey(
-            node.parent.properties,
-            QUERY_KEY,
-          )
-          const queryFn = ASTUtils.findPropertyWithIdentifierKey(
-            node.parent.properties,
-            QUERY_FN,
-          )
+        const scopeManager = context.getSourceCode().scopeManager
+        const queryKey = ASTUtils.findPropertyWithIdentifierKey(
+          node.parent.properties,
+          QUERY_KEY,
+        )
+        const queryFn = ASTUtils.findPropertyWithIdentifierKey(
+          node.parent.properties,
+          QUERY_FN,
+        )
 
-          if (
-            scopeManager === null ||
-            queryKey === undefined ||
-            queryFn === undefined ||
-            queryFn.value.type !== AST_NODE_TYPES.ArrowFunctionExpression
-          ) {
-            return
-          }
+        if (
+          scopeManager === null ||
+          queryKey === undefined ||
+          queryFn === undefined ||
+          queryFn.value.type !== AST_NODE_TYPES.ArrowFunctionExpression
+        ) {
+          return
+        }
 
-          if (queryKey.value.type !== AST_NODE_TYPES.ArrayExpression) {
-            // TODO support query key factory
-            return
-          }
+        if (queryKey.value.type !== AST_NODE_TYPES.ArrayExpression) {
+          // TODO support query key factory
+          return
+        }
 
-          const sourceCode = context.getSourceCode()
-          const queryKeyValue = queryKey.value
-          const refs = ASTUtils.getExternalRefs({
-            scopeManager,
-            node: queryFn.value,
+        const sourceCode = context.getSourceCode()
+        const queryKeyValue = queryKey.value
+        const refs = ASTUtils.getExternalRefs({
+          scopeManager,
+          node: queryFn.value,
+        })
+
+        const existingKeys = ASTUtils.getNestedIdentifiers(queryKeyValue).map(
+          (identifier) => ASTUtils.mapKeyNodeToText(identifier, sourceCode),
+        )
+
+        const missingRefs = refs
+          .map((ref) => ({
+            ref: ref,
+            text: ASTUtils.mapKeyNodeToText(ref.identifier, sourceCode),
+          }))
+          .filter(({ ref, text }) => {
+            return (
+              !ASTUtils.isAncestorIsCallee(ref.identifier) &&
+              !existingKeys.some((existingKey) => existingKey === text)
+            )
           })
+          .map(({ ref, text }) => ({
+            identifier: ref.identifier,
+            text: text,
+          }))
 
-          const existingKeys = ASTUtils.getNestedIdentifiers(queryKeyValue).map(
-            (identifier) => ASTUtils.mapKeyNodeToText(identifier, sourceCode),
-          )
+        const uniqueMissingRefs = uniqueBy(missingRefs, (x) => x.text)
 
-          const missingRefs = refs
-            .map((ref) => ({
-              ref: ref,
-              text: ASTUtils.mapKeyNodeToText(ref.identifier, sourceCode),
-            }))
-            .filter(({ ref, text }) => {
-              return (
-                !ASTUtils.isAncestorIsCallee(ref.identifier) &&
-                !existingKeys.some((existingKey) => existingKey === text)
-              )
-            })
-            .map(({ ref, text }) => ({
-              identifier: ref.identifier,
-              text: text,
-            }))
+        if (uniqueMissingRefs.length > 0) {
+          context.report({
+            node: node,
+            messageId: 'missingDeps',
+            data: {
+              deps: uniqueMissingRefs.map((ref) => ref.text).join(', '),
+            },
+            fix(fixer) {
+              const missingAsText = uniqueMissingRefs
+                .map((ref) =>
+                  ASTUtils.mapKeyNodeToText(ref.identifier, sourceCode),
+                )
+                .join(', ')
 
-          const uniqueMissingRefs = uniqueBy(missingRefs, (x) => x.text)
+              const existingWithMissing = sourceCode
+                .getText(queryKeyValue)
+                .replace(/\]$/, `, ${missingAsText}]`)
 
-          if (uniqueMissingRefs.length > 0) {
-            context.report({
-              node: node,
-              messageId: 'missingDeps',
-              data: {
-                deps: uniqueMissingRefs.map((ref) => ref.text).join(', '),
-              },
-              fix(fixer) {
-                const missingAsText = uniqueMissingRefs
-                  .map((ref) =>
-                    ASTUtils.mapKeyNodeToText(ref.identifier, sourceCode),
-                  )
-                  .join(', ')
-
-                const existingWithMissing = sourceCode
-                  .getText(queryKeyValue)
-                  .replace(/\]$/, `, ${missingAsText}]`)
-
-                return fixer.replaceText(queryKeyValue, existingWithMissing)
-              },
-            })
-          }
+              return fixer.replaceText(queryKeyValue, existingWithMissing)
+            },
+          })
         }
       },
     }
