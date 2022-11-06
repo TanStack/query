062d5c6cf6102a37119cfd8112e2eb9b904448cb
StefanDjokovic <ste.djokovic@gmail.com>
2022-11-06 22:27:46 +0200

    fix: add logs to investigate iframe copy issue

--- packages/react-query-devtools/src/Explorer.tsx
+++ packages/react-query-devtools/src/Explorer.tsx
@@ -77,13 +77,48 @@ export const Expander = ({ expanded, style = {} }: ExpanderProps) => (
 )
 
 export const Copier = ({ style = {} }: CopierProps) => (
-  <span aria-label="Copy object to clipboard" title="Copy object to clipboard" style={{
-    paddingLeft: '1em',
-    ...style,
-  }} >
+  <span
+    aria-label="Copy object to clipboard"
+    title="Copy object to clipboard"
+    style={{
+      paddingLeft: '1em',
+      ...style,
+    }}
+  >
     <svg aria-hidden="true" height="12" viewBox="0 0 16 16" width="12">
-      <path fill="currentColor" d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"></path><path fill="currentColor" d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"></path>
+      <path
+        fill="currentColor"
+        d="M0 6.75C0 5.784.784 5 1.75 5h1.5a.75.75 0 010 1.5h-1.5a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-1.5a.75.75 0 011.5 0v1.5A1.75 1.75 0 019.25 16h-7.5A1.75 1.75 0 010 14.25v-7.5z"
+      ></path>
+      <path
+        fill="currentColor"
+        d="M5 1.75C5 .784 5.784 0 6.75 0h7.5C15.216 0 16 .784 16 1.75v7.5A1.75 1.75 0 0114.25 11h-7.5A1.75 1.75 0 015 9.25v-7.5zm1.75-.25a.25.25 0 00-.25.25v7.5c0 .138.112.25.25.25h7.5a.25.25 0 00.25-.25v-7.5a.25.25 0 00-.25-.25h-7.5z"
+      ></path>
     </svg>
+  </span>
+)
+
+export const Copied = ({ style = {} }: CopierProps) => (
+  <span
+    aria-label="Object copied to clipboard"
+    title="Object copied to clipboard"
+    style={{
+      paddingLeft: '1em',
+      ...style,
+    }}
+  >
+  <svg
+    aria-hidden="true"
+    height="16"
+    viewBox="0 0 16 16"
+    width="16"
+    color="green"
+  >
+    <path
+      fill="green"
+      d="M13.78 4.22a.75.75 0 010 1.06l-7.25 7.25a.75.75 0 01-1.06 0L2.22 9.28a.75.75 0 011.06-1.06L6 10.94l6.72-6.72a.75.75 0 011.06 0z"
+    ></path>
+  </svg>
   </span>
 )
 
@@ -139,6 +174,8 @@ export const DefaultRenderer: Renderer = ({
   pageSize,
 }) => {
   const [expandedPages, setExpandedPages] = React.useState<number[]>([])
+  // TODO: shouldn't be here
+  const [copied, setCopied] = React.useState<boolean>(false)
 
   return (
     <Entry key={label}>
@@ -151,9 +188,24 @@ export const DefaultRenderer: Renderer = ({
               {subEntries.length} {subEntries.length > 1 ? `items` : `item`}
             </Info>
           </ExpandButton>
-          {copyable ? (<CopyButton onClick={() => navigator.clipboard.writeText(JSON.stringify(value))} >
-            <Copier />
-          </CopyButton>) : null}
+          {copyable ? (
+            <CopyButton
+              onClick={() =>
+                navigator.clipboard.writeText(JSON.stringify(value)).then(
+                  // Adding logging on console to temporarily get data on why it's not working on the iframe
+                  () => {
+                    console.log('successfully copied')
+                    setCopied(true)
+                  },
+                  (err) => {
+                    console.log('failed to copy', err)
+                  },
+                )
+              }
+            >
+              {copied ? <Copied /> : <Copier />}
+            </CopyButton>
+          ) : null}
           {expanded ? (
             subEntryPages.length === 1 ? (
               <SubEntries>{subEntries.map(handleEntry)}</SubEntries>
@@ -196,7 +248,7 @@ export const DefaultRenderer: Renderer = ({
 type ExplorerProps = Partial<RendererProps> & {
   renderer?: Renderer
   defaultExpanded?: true | Record<string, boolean>
-  copyable?: boolean,
+  copyable?: boolean
 }
 
 type Property = {
