8532dcb3eaa62f6ff2a7653e83e4a2964a8e5c46
StefanDjokovic <ste.djokovic@gmail.com>
2022-11-07 21:24:16 +0200

    fix: test import

--- packages/react-query-devtools/src/__tests__/Explorer.test.tsx
+++ packages/react-query-devtools/src/__tests__/Explorer.test.tsx
@@ -1,4 +1,4 @@
-import { fireEvent, render, screen, waitFor } from '@testing-library/react'
+import { fireEvent, render, screen } from '@testing-library/react'
 import * as React from 'react'
 import { act } from 'react-dom/test-utils'
 
