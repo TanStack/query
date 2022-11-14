d1b85ea3381ac13f73bd748f40b830d39c3daea9
DeAndre Johnson <dre@phreshr.com>
2022-11-14 11:04:01 +0800

    remove onMount

--- packages/svelte-query/src/useQueryClient.ts
+++ packages/svelte-query/src/useQueryClient.ts
@@ -1,17 +1,7 @@
 import type { QueryClient } from '@tanstack/query-core'
-import { onMount } from 'svelte'
 import { getQueryClientContext } from './context'
-// import { client } from './store'
 
 export function useQueryClient(): QueryClient {
   const queryClient = getQueryClientContext()
-
-  onMount(() => {
-    queryClient.mount()
-    return () => {
-      queryClient.unmount()
-    }
-  })
-
   return queryClient
 }
