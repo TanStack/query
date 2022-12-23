8aa7493deb6d7e0a58ad0f399e55b4fe208617e0
moshyfawn <oleg.butorov.98@gmail.com>
2022-12-23 19:58:39 +0000

    ♻️ refactor(solid-query): rename useErrorBoundary mutation option to throwError

--- packages/solid-query/src/createMutation.ts
+++ packages/solid-query/src/createMutation.ts
@@ -106,7 +106,7 @@ export function createMutation<
       () => {
         if (
           state.isError &&
-          shouldThrowError(observer.options.useErrorBoundary, [state.error])
+          shouldThrowError(observer.options.throwError, [state.error])
         ) {
           throw state.error
         }
