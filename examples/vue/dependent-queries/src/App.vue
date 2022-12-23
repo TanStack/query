7f1f27a6c93cc87e78cbaa0310856f9b55580844
Johannes Przymusinski <johannes.przymusinski@jop-software.de>
2022-12-23 09:29:17 +0100

    chore: apply code formatting

--- examples/vue/dependent-queries/src/App.vue
+++ examples/vue/dependent-queries/src/App.vue
@@ -1,29 +1,29 @@
 <script lang="ts">
-import { defineComponent, ref } from "vue";
+import { defineComponent, ref } from 'vue'
 
-import Posts from "./Posts.vue";
-import Post from "./Post.vue";
+import Posts from './Posts.vue'
+import Post from './Post.vue'
 
 export default defineComponent({
-  name: "App",
+  name: 'App',
   components: { Posts, Post },
   setup() {
-    const visitedPosts = ref(new Set());
-    const isVisited = (id: number) => visitedPosts.value.has(id);
+    const visitedPosts = ref(new Set())
+    const isVisited = (id: number) => visitedPosts.value.has(id)
 
-    const postId = ref(-1);
+    const postId = ref(-1)
     const setPostId = (id: number) => {
-      visitedPosts.value.add(id);
-      postId.value = id;
-    };
+      visitedPosts.value.add(id)
+      postId.value = id
+    }
 
     return {
       isVisited,
       postId,
       setPostId,
-    };
+    }
   },
-});
+})
 </script>
 
 <template>
@@ -33,10 +33,11 @@ export default defineComponent({
     first time you load them. However, after you return to this list and click
     on any posts you have already visited again, you will see them load
     instantly and background refresh right before your eyes!
-    <br>
-    You will also notice, that after some time the name of the author gets loaded.
-    This Data is not directly included in the <code>/post/id</code> response but gets
-    fetched when the post gets loaded from its <code>userId</code>.
+    <br />
+    You will also notice, that after some time the name of the author gets
+    loaded. This Data is not directly included in the
+    <code>/post/id</code> response but gets fetched when the post gets loaded
+    from its <code>userId</code>.
     <strong>
       (You may need to throttle your network speed to simulate longer loading
       sequences)
--- examples/vue/dependent-queries/src/types.d.ts
+++ examples/vue/dependent-queries/src/types.d.ts
@@ -1,30 +1,30 @@
 export interface Post {
-  userId: number;
-  id: number;
-  title: string;
-  body: string;
+  userId: number
+  id: number
+  title: string
+  body: string
 }
 
 export interface Author {
-  id: number;
-  name: string;
-  username: string;
-  email: string;
+  id: number
+  name: string
+  username: string
+  email: string
   address: {
-    street: string;
-    suite: string;
-    city: string;
-    zipcode: string;
+    street: string
+    suite: string
+    city: string
+    zipcode: string
     geo: {
-      lat: string;
-      lng: string;
+      lat: string
+      lng: string
     }
-  },
-  phone: string;
-  website: string;
+  }
+  phone: string
+  website: string
   company: {
-    name: string;
-    catchPhrase: string;
-    bs: string;
+    name: string
+    catchPhrase: string
+    bs: string
   }
 }
