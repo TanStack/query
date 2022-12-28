e8fb453e1fab1a19052208e4e5cf737ab1271279
tludlow <twjludlow@gmail.com>
2022-12-28 22:34:01 +0000

    refactor(solid-query): remove contextSharing
    remove the contextSharing property from the QueryClientProvider
    
    BREAKING CHANGE:  contextSharing is removed from the QueryClientProvider

--- packages/solid-query/src/QueryClientProvider.tsx
+++ packages/solid-query/src/QueryClientProvider.tsx
@@ -1,52 +1,22 @@
 import type { QueryClient } from '@tanstack/query-core'
 import type { Context, JSX } from 'solid-js'
-import {
-  createContext,
-  useContext,
-  onMount,
-  onCleanup,
-  mergeProps,
-} from 'solid-js'
+import { createContext, useContext, onMount, onCleanup } from 'solid-js'
 import type { ContextOptions } from './types'
 
-declare global {
-  interface Window {
-    SolidQueryClientContext?: Context<QueryClient | undefined>
-  }
-}
-
 export const defaultContext = createContext<QueryClient | undefined>(undefined)
-const QueryClientSharingContext = createContext<boolean>(false)
 
-// If we are given a context, we will use it.
-// Otherwise, if contextSharing is on, we share the first and at least one
-// instance of the context across the window
-// to ensure that if Solid Query is used across
-// different bundles or microfrontends they will
-// all use the same **instance** of context, regardless
-// of module scoping.
 function getQueryClientContext(
   context: Context<QueryClient | undefined> | undefined,
-  contextSharing: boolean,
 ) {
   if (context) {
     return context
-  }
-  if (contextSharing && typeof window !== 'undefined') {
-    if (!window.SolidQueryClientContext) {
-      window.SolidQueryClientContext = defaultContext
-    }
-
-    return window.SolidQueryClientContext
   }
 
   return defaultContext
 }
 
 export const useQueryClient = ({ context }: ContextOptions = {}) => {
-  const queryClient = useContext(
-    getQueryClientContext(context, useContext(QueryClientSharingContext)),
-  )
+  const queryClient = useContext(getQueryClientContext(context))
 
   if (!queryClient) {
     throw new Error('No QueryClient set, use QueryClientProvider to set one')
@@ -59,52 +29,24 @@ type QueryClientProviderPropsBase = {
   client: QueryClient
   children?: JSX.Element
 }
-type QueryClientProviderPropsWithContext = ContextOptions & {
-  contextSharing?: never
-} & QueryClientProviderPropsBase
-type QueryClientProviderPropsWithContextSharing = {
-  context?: never
-  contextSharing?: boolean
-} & QueryClientProviderPropsBase
+type QueryClientProviderPropsWithContext = ContextOptions &
+  QueryClientProviderPropsBase
 
-export type QueryClientProviderProps =
-  | QueryClientProviderPropsWithContext
-  | QueryClientProviderPropsWithContextSharing
+export type QueryClientProviderProps = QueryClientProviderPropsWithContext
 
 export const QueryClientProvider = (
   props: QueryClientProviderProps,
 ): JSX.Element => {
-  const mergedProps = mergeProps(
-    {
-      contextSharing: false,
-    },
-    props,
-  )
   onMount(() => {
-    mergedProps.client.mount()
-
-    if (process.env.NODE_ENV !== 'production' && mergedProps.contextSharing) {
-      mergedProps.client
-        .getLogger()
-        .error(
-          `The contextSharing option has been deprecated and will be removed in the next major version`,
-        )
-    }
+    props.client.mount()
   })
-  onCleanup(() => mergedProps.client.unmount())
+  onCleanup(() => props.client.unmount())
 
-  const QueryClientContext = getQueryClientContext(
-    mergedProps.context,
-    mergedProps.contextSharing,
-  )
+  const QueryClientContext = getQueryClientContext(props.context)
 
   return (
-    <QueryClientSharingContext.Provider
-      value={!mergedProps.context && mergedProps.contextSharing}
-    >
-      <QueryClientContext.Provider value={mergedProps.client}>
-        {mergedProps.children}
-      </QueryClientContext.Provider>
-    </QueryClientSharingContext.Provider>
+    <QueryClientContext.Provider value={props.client}>
+      {props.children}
+    </QueryClientContext.Provider>
   )
 }
