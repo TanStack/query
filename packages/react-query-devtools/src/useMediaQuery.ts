7eb2223d843a4bb2530870c59d70a486ac9ac2e8
girishsontakke <girishsontakke7@gmail.com>
2023-01-04 14:22:15 +0530

    perf(client components): added missing  directives

--- packages/react-query-devtools/src/useMediaQuery.ts
+++ packages/react-query-devtools/src/useMediaQuery.ts
@@ -1,3 +1,4 @@
+'use client'
 import * as React from 'react'
 
 export default function useMediaQuery(query: string): boolean | undefined {
--- packages/react-query-devtools/src/utils.ts
+++ packages/react-query-devtools/src/utils.ts
@@ -1,3 +1,4 @@
+'use client'
 import * as React from 'react'
 import type { Query } from '@tanstack/react-query'
 import SuperJSON from 'superjson'
