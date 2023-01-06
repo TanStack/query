3795933bc52a6afe9b8b5057e530b632bb9fac04
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-01-07 08:08:05 +1100

    Update examples to sveltekit 1.0

--- packages/svelte-query/src/lib/index.ts
+++ packages/svelte-query/src/lib/index.ts
@@ -4,6 +4,7 @@
 export * from '@tanstack/query-core'
 
 // Svelte Query
+export * from './types'
 export { useQueryClient } from './useQueryClient'
 export { useQuery } from './useQuery'
 export { useQueries } from './useQueries'
