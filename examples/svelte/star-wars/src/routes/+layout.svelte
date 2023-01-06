fb1af522da4dde463429a645ec88866d9ba0c648
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-01-07 09:55:13 +1100

    Start adding star-wars example

---
+++ examples/svelte/star-wars/src/routes/+layout.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+  import '../app.css'
+  import { QueryClientProvider, QueryClient } from '@tanstack/svelte-query'
+
+  const queryClient = new QueryClient()
+</script>
+
+<svelte:head>
+  <title>Svelte Query Star Wars Example</title>
+</svelte:head>
+
+<QueryClientProvider client={queryClient}>
+  <slot />
+</QueryClientProvider>
