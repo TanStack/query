e34e1d12fac5ecdda188be451e568da7754b7bdf
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-01-08 10:42:09 +1100

    Add path alias to vitest

--- packages/svelte-query/vite.config.ts
+++ packages/svelte-query/vite.config.ts
@@ -1,4 +1,5 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 import type { UserConfig } from 'vite';
 
 const config: UserConfig = {
@@ -7,6 +8,9 @@ const config: UserConfig = {
     environment: 'jsdom',
     include: ['src/**/*.{test,spec}.{js,ts}'],
     setupFiles: ['vitest.setup.ts'],
+    alias: {
+      "@tanstack/query-core": path.resolve(__dirname, '../query-core/src'),
+    }
   }
 };
 
