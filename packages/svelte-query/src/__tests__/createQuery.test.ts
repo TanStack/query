1bc23a8d768d7facaecb37172a98464926099be7
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-01-08 09:09:45 +1100

    Run svelte-kit sync before vitest

--- packages/svelte-query/src/__tests__/createQuery.test.ts
+++ packages/svelte-query/src/__tests__/createQuery.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, vi } from 'vitest'
+import { describe, it, expect } from 'vitest'
 import { render, screen } from '@testing-library/svelte'
 import CreateQuery from './CreateQuery.svelte'
 import { sleep } from './utils'
