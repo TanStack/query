ab3e11b482ce83802ce99139ce1e4072416d5fb1
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-01-07 23:37:08 +1100

    Misc fixes from feedback
    - Max TS 4.7.4
    - Move root package.json dependencies
    - Use object syntax
    - Use test:jest script (for now)
    - Remove sveltekit autogenerated files

--- examples/svelte/hydration/src/routes/+page.ts
+++ examples/svelte/hydration/src/routes/+page.ts
@@ -1,10 +1,15 @@
 import { dehydrate, QueryClient } from '@tanstack/svelte-query'
 import { getPosts, limit } from '$lib/data.js'
+import type { PageLoad } from "./$types"
 
-/** @type {import('./$types').PageLoad} */
-export async function load() {
+export const load: PageLoad = async () => {
   const queryClient = new QueryClient()
-  await queryClient.prefetchQuery(['posts', limit], () => getPosts(limit))
+
+  await queryClient.prefetchQuery({
+    queryKey: ['posts', limit],
+    queryFn: () => getPosts(limit)
+  })
+
   return {
     dehydratedState: dehydrate(queryClient),
   }
--- packages/svelte-query/src/routes/+page.svelte
+++
@@ -1,4 +0,0 @@
-<h1>Svelte Query</h1>
-<p>This routes foulder is unused</p> 
-<p>Documentation should be written in docs/svelte</p>
-<p>Examples should be added to examples/svelte</p>
