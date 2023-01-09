fd884003691d0a4519faf32ef48b04410ea8c2cf
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-01-10 09:45:37 +1100

    Move /src/lib to /src, ignore __tests__ in build

--- packages/svelte-query/svelte.config.js
+++ packages/svelte-query/svelte.config.js
@@ -3,9 +3,12 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 const config = {
   preprocess: vitePreprocess(),
   package: {
-    source: "./src/lib",
-    dir: "./build/lib"
-  }
+    source: "./src",
+    dir: "./build/lib",
+    files: (filepath) => {
+      return !filepath.includes("__tests__");
+    },
+  },
 };
 
 export default config;
