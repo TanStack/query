d5126828df75e71991cfef8e9b2d4eb99a5f3593
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-01-10 18:09:33 +1100

    chore: Restructure package scripts (#4779)
    * Use rimraf for clean script
    
    Supported on windows
    
    * Restructure jest tests, types test, and building types
    
    * Relative eslint and jest paths are not needed
    
    Now works on windows too, and tested to make sure it doesn't break linux
    
    * Run codemod tests from main jest.config.ts
    
    --collectCoverage false breaks when passed in to the union of these
    
    * Remove unused test:lib scripts
    
    * Add prettier-plugin-svelte
    
    * Call test:types in github workflow
    
    * Add missing build:types to query-async-storage-persister
    
    * Simplify build script
    
    Co-authored-by: Dominik Dorfmeister <office@dorfmeister.cc>

--- packages/react-query/jest.config.ts
+++ packages/react-query/jest.config.ts
@@ -2,4 +2,5 @@ export default {
   displayName: 'react-query',
   preset: '../../jest-preset.js',
   setupFilesAfterEnv: ['./jest.setup.ts'],
+  testMatch: ['<rootDir>/src/**/*.test.tsx', '<rootDir>/codemods/**/*.test.js'],
 }
