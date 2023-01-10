db43aa932d0374c1dbba7108c200aaa7ce793d4f
Gaurav Singh <ckzo@proton.me>
2023-01-10 14:18:15 +0530

    chore(react-query): drop support for React17

--- packages/react-query/src/useSyncExternalStore.ts
+++
@@ -1,4 +0,0 @@
-// Temporary workaround due to an issue with react-native uSES - https://github.com/TanStack/query/pull/3601
-import { useSyncExternalStore as uSES } from 'use-sync-external-store/shim/index.js'
-
-export const useSyncExternalStore = uSES
