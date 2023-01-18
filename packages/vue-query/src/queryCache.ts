59e7f4c3fa5dcd4fa98e837bf70864b117f6b078
Damian Osipiuk <osipiukd+git@gmail.com>
2023-01-18 23:33:14 +0100

    refactor: drop unnecessary type assertions

--- packages/vue-query/src/queryCache.ts
+++ packages/vue-query/src/queryCache.ts
@@ -7,15 +7,10 @@ export class QueryCache extends QC {
   find<TQueryFnData = unknown, TError = Error, TData = TQueryFnData>(
     filters: MaybeRefDeep<WithRequired<QueryFilters, 'queryKey'>>,
   ): Query<TQueryFnData, TError, TData> | undefined {
-    const filtersUnreffed = cloneDeepUnref(filters) as WithRequired<
-      QueryFilters,
-      'queryKey'
-    >
-    return super.find(filtersUnreffed)
+    return super.find(cloneDeepUnref(filters))
   }
 
-  findAll(filters?: MaybeRefDeep<QueryFilters>): Query[] {
-    const filtersUnreffed = cloneDeepUnref(filters) as QueryFilters
-    return super.findAll(filtersUnreffed)
+  findAll(filters: MaybeRefDeep<QueryFilters> = {}): Query[] {
+    return super.findAll(cloneDeepUnref(filters))
   }
 }
