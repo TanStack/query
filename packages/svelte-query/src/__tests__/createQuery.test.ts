1615509973fc4850890f8da028c21b22ba28b829
Dominik Dorfmeister <office@dorfmeister.cc>
2023-01-30 10:43:11 +0100

    fix(core): make sure queries that never fetched can be garbage collected (#4898)
    * fix(core): make sure queries that never fetched can ge garbage collected
    
    This is important if data comes into the cache via a non-fetching method (like setQueryData or hydration) AND there is no active observer for them. Without this, they would never be gc'ed, but they should be after the default cacheTime
    
    * tests: simplify svelte test

--- packages/svelte-query/src/__tests__/createQuery.test.ts
+++ packages/svelte-query/src/__tests__/createQuery.test.ts
@@ -1,11 +1,11 @@
 import { describe, it, expect } from 'vitest'
-import { render, screen } from '@testing-library/svelte'
+import { render, waitFor } from '@testing-library/svelte'
 import CreateQuery from './CreateQuery.svelte'
 import { sleep } from './utils'
 
 describe('createQuery', () => {
   it('Render and wait for success', async () => {
-    render(CreateQuery, {
+    const rendered = render(CreateQuery, {
       props: {
         options: {
           queryKey: ['test'],
@@ -17,14 +17,12 @@ describe('createQuery', () => {
       },
     })
 
-    expect(screen.queryByText('Loading')).toBeInTheDocument()
-    expect(screen.queryByText('Error')).not.toBeInTheDocument()
-    expect(screen.queryByText('Success')).not.toBeInTheDocument()
+    await waitFor(() => {
+      expect(rendered.getByText('Loading')).toBeInTheDocument()
+    })
 
-    await sleep(20)
-
-    expect(screen.queryByText('Success')).toBeInTheDocument()
-    expect(screen.queryByText('Loading')).not.toBeInTheDocument()
-    expect(screen.queryByText('Error')).not.toBeInTheDocument()
+    await waitFor(() => {
+      expect(rendered.getByText('Success')).toBeInTheDocument()
+    })
   })
 })
