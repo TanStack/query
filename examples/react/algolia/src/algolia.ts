1a38d17710c7a38a85f8eacee455f63db924e89c
Arnaud <arnaud.bezancon@advantys.com>
2023-02-12 21:37:17 +0100

    examples(react): Algolia search example (#4971)
    * docs: example: algolia
    
    * fix: remove empty line if desc is null
    
    * fix: stale time value
    
    * refactor: use vite and examples structure
    
    * fix: remove deprecated doc files
    
    * docs: add algolia example in the config.json

---
+++ examples/react/algolia/src/algolia.ts
@@ -0,0 +1,38 @@
+import algoliasearch from "algoliasearch";
+import { Hit } from "@algolia/client-search";
+
+// From Algolia example
+// https://github.com/algolia/react-instantsearch
+const ALGOLIA_APP_ID = "latency";
+const ALGOLIA_SEARCH_API_KEY = "6be0576ff61c053d5f9a3225e2a90f76";
+
+type SearchOptions = {
+  indexName: string;
+  query: string;
+  pageParam: number;
+  hitsPerPage: number;
+};
+
+export async function search<TData>({
+  indexName,
+  query,
+  pageParam = 0,
+  hitsPerPage = 10,
+}: SearchOptions): Promise<{
+  hits: Hit<TData>[];
+  nextPage: number | undefined;
+}> {
+  const client = algoliasearch(ALGOLIA_APP_ID, ALGOLIA_SEARCH_API_KEY);
+  const index = client.initIndex(indexName);
+
+  console.log("alogolia:search", { indexName, query, pageParam, hitsPerPage });
+
+  const { hits, page, nbPages } = await index.search<TData>(query, {
+    page: pageParam,
+    hitsPerPage,
+  });
+
+  const nextPage = page + 1 < nbPages ? page + 1 : undefined;
+
+  return { hits, nextPage };
+}
