90f6cdda5e50e5583e35540541f0443364deffb6
Zack DeRose <zack.derose@gmail.com>
2023-03-05 17:04:16 -0700

    removing unneeded setup-jest.js file

--- packages/eslint-plugin-query/vitest.config.ts
+++ packages/eslint-plugin-query/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     name: 'eslint-plugin-query',
-    setupFiles: ['../../jest-preset.js'],
     watch: false,
   },
 })
--- packages/query-async-storage-persister/vitest.config.ts
+++ packages/query-async-storage-persister/vitest.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   test: {
     name: 'query-async-storage-persister',
-    setupFiles: ['../../jest-preset.js'],
     watch: false,
   },
 })
