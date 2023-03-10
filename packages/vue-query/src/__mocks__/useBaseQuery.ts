8bd70bf42e6290b25bd567e605bd8d3187e42a97
Zack DeRose <zack.derose@gmail.com>
2023-03-09 23:28:47 -0700

    reverting vue-query tsconfig to not use esnext

--- packages/vue-query/src/__mocks__/useBaseQuery.ts
+++ packages/vue-query/src/__mocks__/useBaseQuery.ts
@@ -1,6 +1,7 @@
 import { vi } from 'vitest'
 
 const { useBaseQuery: originImpl, unrefQueryArgs: originalParse } =
+  // @ts-expect-error - vitest uses esmodules; tsconfig is not set to use them
   (await vi.importActual('../useBaseQuery')) as any
 
 export const useBaseQuery = vi.fn(originImpl)
