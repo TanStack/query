6c36772e2bc61d17dd747b510549196c9ecd7721
Zack DeRose <zack.derose@gmail.com>
2023-03-18 00:58:54 -0700

    fixing lint errors in vite config files

---
+++ packages/codemods/.eslintrc.cjs
@@ -0,0 +1,21 @@
+// @ts-check
+
+/** @type {import('eslint').Linter.Config} */
+const config = {
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.eslint.json',
+    sourceType: 'module',
+  },
+  overrides: [
+    {
+      files: ['**/__testfixtures__/*'],
+      rules: {
+        'import/no-unresolved': 'off',
+        '@typescript-eslint/no-unused-vars': 'off',
+      },
+    },
+  ],
+}
+
+module.exports = config
