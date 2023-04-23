195772e8f2768d1d8db064ae8955b2dd7e0efd0f
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-04-23 12:23:39 +1000

    chore(svelte-query): Update @sveltejs/package to v2 (#5242)

--- packages/svelte-query/svelte.config.js
+++ packages/svelte-query/svelte.config.js
@@ -2,13 +2,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 const config = {
   preprocess: vitePreprocess(),
-  package: {
-    source: "./src",
-    dir: "./build/lib",
-    files: (filepath) => {
-      return !filepath.includes("__tests__");
-    },
-  },
 };
 
 export default config;
