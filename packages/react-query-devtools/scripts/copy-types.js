ce5ce40e3223bf17935116915898aacef35c4b0f
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-05-26 12:30:51 +1000

    Use cpy-cli

--- packages/react-query-devtools/scripts/copy-types.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-
-fs.cp('./build/lib/index.d.ts', './build/lib/index.prod.d.ts', (err) => {
-  if (err) {
-    console.error(err);
-  }
-});
--- packages/react-query/scripts/copy-codemods.js
+++
@@ -1,7 +0,0 @@
-const fs = require('fs')
-
-fs.cp('../codemods/src', './build/codemods', { recursive: true }, (err) => {
-  if (err) {
-    console.error(err);
-  }
-});
