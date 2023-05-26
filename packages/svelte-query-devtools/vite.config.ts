6447f58c1f9256141dd1d3a89bca0d4cca3c8b81
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-05-26 18:47:18 +1000

    Fix test:lib

--- packages/svelte-query-devtools/vite.config.ts
+++ packages/svelte-query-devtools/vite.config.ts
@@ -1,6 +1,14 @@
+import { resolve } from 'node:path';
 import { svelte } from '@sveltejs/vite-plugin-svelte';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  plugins: [svelte()]
+  plugins: [svelte()],
+  resolve: {
+    alias: {
+      "@tanstack/query-core": resolve(__dirname, '..', 'query-core', 'src'),
+      "@tanstack/query-devtools": resolve(__dirname, '..', 'query-devtools', 'src'),
+      "@tanstack/svelte-query": resolve(__dirname, '..', 'svelte-query', 'src'),
+    }
+  }
 });
