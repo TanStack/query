748b4e798b2ee05c01273cc63c358b3c13405dcd
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-06-03 13:17:10 +1000

    fix: Publint error in solid-query (#5522)

--- scripts/validatePackages.js
+++ scripts/validatePackages.js
@@ -58,7 +58,13 @@ async function run() {
       const publintResult = await publint({ pkgDir: pkg.packageDir })
 
       publintResult.forEach((message) => {
-        console.log(`Publint warning: ${JSON.stringify(message, null, 2)}`)
+        failedValidations.push(
+          `Publint warning: ${pkg.packageDir} ${JSON.stringify(
+            message,
+            null,
+            2,
+          )}`,
+        )
       })
     }),
   )
