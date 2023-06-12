f4e2b1529489341b25e63d73f0dad8740d74963c
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-06-12 14:46:56 +1000

    fix(solid-query): Use rollup-preset-solid

--- packages/solid-query/rollup.config.js
+++ packages/solid-query/rollup.config.js
@@ -1,12 +1,31 @@
 // @ts-check
 
 import { defineConfig } from 'rollup'
-import { buildConfigs } from '../../scripts/getRollupConfig.js'
+import withSolid from 'rollup-preset-solid'
 
-export default defineConfig(
-  buildConfigs({
-    name: 'solid-query',
-    outputFile: 'index',
-    entryFile: './src/index.ts',
-  }),
-)
+export function createSolidQueryConfig() {
+  const solidRollupOptions = /** @type {import('rollup').RollupOptions} */ (
+    withSolid({
+      input: `./src/index.ts`,
+      targets: ['esm', 'cjs'],
+      external: ['@tanstack/query-core'],
+    })
+  )
+
+  const outputs = !solidRollupOptions.output
+    ? []
+    : Array.isArray(solidRollupOptions.output)
+    ? solidRollupOptions.output
+    : [solidRollupOptions.output]
+
+  outputs.forEach((output) => {
+    if (output.format === 'cjs') {
+      output.entryFileNames = '[name].cjs'
+      output.chunkFileNames = '[name]-[hash].cjs'
+    }
+  })
+
+  return solidRollupOptions  
+}
+
+export default defineConfig(createSolidQueryConfig())
