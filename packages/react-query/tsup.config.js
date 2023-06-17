6fe8290bf0caa491d656da02dd0d4ff08330580f
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-06-17 10:35:16 +1000

    Update tsup target

--- packages/react-query/tsup.config.js
+++ packages/react-query/tsup.config.js
@@ -1,9 +1,11 @@
+// @ts-check
+
 import { defineConfig } from 'tsup'
 
 export default defineConfig({
   entry: ['src/*.ts', 'src/*.tsx'],
   format: ['cjs', 'esm'],
-  target: 'es2020',
+  target: ['es2020', 'node16'],
   bundle: false,
   splitting: false,
   sourcemap: true,
