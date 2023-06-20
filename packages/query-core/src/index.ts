1b25d90bd2ed7e6056ba62fcd22a25c29526b697
Dominik Dorfmeister <office@dorfmeister.cc>
2023-06-20 17:46:59 +0200

    Update packages/query-core/src/index.ts

--- packages/query-core/src/index.ts
+++ packages/query-core/src/index.ts
@@ -1,7 +1,8 @@
 /* istanbul ignore file */
 
 export { CancelledError } from './retryer'
-export { QueryCache, QueryCacheNotifyEvent } from './queryCache'
+export { QueryCache } from './queryCache'
+export type { QueryCacheNotifyEvent } from './queryCache'
 export { QueryClient } from './queryClient'
 export { QueryObserver } from './queryObserver'
 export { QueriesObserver } from './queriesObserver'
