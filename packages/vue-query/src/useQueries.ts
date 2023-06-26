3930efcf4b18d7a1341621f081dd4319768f89a8
Mohamed EL AYADI <moh.el.ayadi@gmail.com>
2023-06-26 21:29:01 +0100

    maybe fix for vue reactivity cbs

--- packages/vue-query/src/useQueries.ts
+++ packages/vue-query/src/useQueries.ts
@@ -215,7 +215,10 @@ export function useQueries<
         defaultedQueries.value,
         options as QueriesObserverOptions<TCombinedResult>,
       )
-      state.value = observer.getCurrentResult()
+      const [, getCombinedResultPersisted] = observer.getOptimisticResult(
+        defaultedQueries.value,
+      )
+      state.value = getCombinedResultPersisted()
     },
     { deep: true },
   )
