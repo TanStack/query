01125e6ebe86903a95504d9bf5dd8ccb1e406473
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-03 17:53:54 +0200

    fix(eslint-plugin-query): Use tsup to generate types (#5666)

--- packages/eslint-plugin-query/tsup.config.js
+++ packages/eslint-plugin-query/tsup.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   format: ['cjs', 'esm'],
   target: ['es2020', 'node16'],
   outDir: 'build/lib',
-  external: [/eslint/],
+  dts: true,
   sourcemap: true,
   clean: true,
 })
