07c56a8d9dce293c93c2cefb8643b1b4dd45ec98
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-04 02:23:47 +1000

    Generate modern and legacy builds

--- packages/query-core/tsup.config.js
+++ packages/query-core/tsup.config.js
@@ -2,14 +2,27 @@
 
 import { defineConfig } from 'tsup'
 
-export default defineConfig({
-  entry: ['src/*.ts'],
-  format: ['cjs', 'esm'],
-  target: ['chrome84', 'firefox90', 'edge84', 'safari15', 'ios15', 'opera70'],
-  outDir: 'build/lib',
-  bundle: false,
-  splitting: false,
-  dts: true,
-  sourcemap: true,
-  clean: true,
-})
+export default defineConfig([
+  {
+    entry: ['src/*.ts'],
+    format: ['cjs', 'esm'],
+    target: ['chrome84', 'firefox90', 'edge84', 'safari15', 'ios15', 'opera70'],
+    outDir: 'build/modern',
+    bundle: false,
+    splitting: false,
+    dts: true,
+    sourcemap: true,
+    clean: true,
+  },
+  {
+    entry: ['src/*.ts'],
+    format: ['cjs', 'esm'],
+    target: ['es2020', 'node16'],
+    outDir: 'build/legacy',
+    bundle: false,
+    splitting: false,
+    dts: true,
+    sourcemap: true,
+    clean: true,
+  },
+])
