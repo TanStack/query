4d571d1030d184c84843a3d908e8ec2d810a6884
Speros Kokenes <skokenes@users.noreply.github.com>
2023-07-03 17:02:22 -0400

    fix: Skip queryClient context lookup if client passed directly (#5669)

--- packages/svelte-query/src/useQueryClient.ts
+++ packages/svelte-query/src/useQueryClient.ts
@@ -2,11 +2,6 @@ import type { QueryClient } from '@tanstack/query-core'
 import { getQueryClientContext } from './context'
 
 export function useQueryClient(queryClient?: QueryClient): QueryClient {
-  const client = getQueryClientContext()
-
-  if (queryClient) {
-    return queryClient
-  }
-
-  return client
+  if (queryClient) return queryClient
+  return getQueryClientContext()
 }
