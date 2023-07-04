5a15ed3c9d3e43c2bb765ded064f5563facd3f9c
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-04 15:14:56 +1000

    Add tests

--- packages/svelte-query/src/__tests__/createQuery.test.ts
+++ packages/svelte-query/src/__tests__/createQuery.test.ts
@@ -1,12 +1,12 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, test } from 'vitest'
 import { render, waitFor } from '@testing-library/svelte'
-import { writable } from 'svelte/store'
+import { derived, writable } from 'svelte/store'
 import CreateQuery from './CreateQuery.svelte'
 import { sleep } from './utils'
 import type { CreateQueryOptions } from '../types'
 
 describe('createQuery', () => {
-  it('Render and wait for success', async () => {
+  test('Render and wait for success', async () => {
     const rendered = render(CreateQuery, {
       props: {
         options: {
@@ -28,8 +28,8 @@ describe('createQuery', () => {
     })
   })
 
-  it('should keep previous data when returned as placeholder data', async () => {
-    const options: CreateQueryOptions = writable({
+  test('Keep previous data when returned as placeholder data', async () => {
+    const options = writable({
       queryKey: ['test', [1]],
       queryFn: async ({ queryKey }) => {
         await sleep(10)
@@ -38,7 +38,8 @@ describe('createQuery', () => {
         return ids.map((id) => ({ id }))
       },
       placeholderData: (previousData: { id: number }[]) => previousData,
-    })
+    }) satisfies CreateQueryOptions
+  
     const rendered = render(CreateQuery, { props: { options } })
 
     await waitFor(() => {
@@ -61,6 +62,48 @@ describe('createQuery', () => {
     await waitFor(() => {
       expect(rendered.queryByText('id: 1')).toBeInTheDocument()
       expect(rendered.queryByText('id: 2')).toBeInTheDocument()
+    })
+  })
+
+  test('Accept a writable store for options', async () => {
+    const optionsStore = writable({
+      queryKey: ['test'],
+      queryFn: async () => {
+        await sleep(10)
+        return 'Success'
+      },
+    }) satisfies CreateQueryOptions
+    
+    const rendered = render(CreateQuery, {
+      props: {
+        options: optionsStore,
+      }
+    })
+
+    await waitFor(() => {
+      expect(rendered.getByText('Success')).toBeInTheDocument()
+    })
+  })
+
+  test('Accept a derived store for options', async () => {
+    const writableStore = writable("test");
+
+    const derivedStore = derived(writableStore, ($store) => ({
+      queryKey: [$store],
+      queryFn: async () => {
+        await sleep(10)
+        return 'Success'
+      },
+    })) satisfies CreateQueryOptions
+    
+    const rendered = render(CreateQuery, {
+      props: {
+        options: derivedStore,
+      }
+    })
+
+    await waitFor(() => {
+      expect(rendered.getByText('Success')).toBeInTheDocument()
     })
   })
 })
