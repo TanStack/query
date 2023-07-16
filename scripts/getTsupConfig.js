f2fb747451b13e92100765a97c2b5760fb263c72
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-16 20:27:55 +1000

    Preserve optional chaining

--- scripts/getTsupConfig.js
+++ scripts/getTsupConfig.js
@@ -10,7 +10,7 @@ export function modernConfig(opts) {
   return {
     entry: opts.entry,
     format: ['cjs', 'esm'],
-    target: ['chrome84', 'firefox90', 'edge84', 'safari15', 'ios15', 'opera70'],
+    target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
     outDir: 'build/modern',
     bundle: opts.bundle,
     splitting: opts.bundle,
