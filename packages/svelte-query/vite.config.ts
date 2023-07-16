a7b2a72c15f2083dba70369c635a70ae4b4da13a
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-16 11:49:30 +1000

    Use export default

--- packages/svelte-query/vite.config.ts
+++ packages/svelte-query/vite.config.ts
@@ -2,7 +2,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 import { defineConfig } from 'vitest/config'
 import path from 'node:path'
 
-const config = defineConfig({
+export default defineConfig({
   plugins: [svelte()],
   resolve: {
     alias: {
@@ -23,5 +23,3 @@ const config = defineConfig({
     setupFiles: ['vitest.setup.ts'],
   },
 })
-
-export default config
