e40237afd3c981f01835e5f6ade7afbad4993c2e
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-20 09:18:22 +1000

    Make all query-devtools packages devDeps

--- packages/query-devtools/tsup.config.js
+++ packages/query-devtools/tsup.config.js
@@ -12,18 +12,7 @@ export default defineConfig(
     cjs: true,
     tsupOptions: (config) => ({
       ...config,
-      outDir: 'build',
-      noExternal: [
-        'solid-js',
-        'solid-js/web',
-        '@emotion/css',
-        '@solid-primitives/keyed',
-        '@solid-primitives/resize-observer',
-        '@solid-primitives/storage',
-        '@tanstack/match-sorter-utils',
-        'solid-transition-group',
-        'superjson',
-      ],
+      outDir: 'build'
     }),
   },
 )
