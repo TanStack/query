b87c1961471c79b1ddb23987835c9288387b92f4
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-21 10:24:36 +1000

    Add react-cra5 test

---
+++ integrations/react-cra5/src/index.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import App from './App';
+
+const queryClient = new QueryClient()
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+      <ReactQueryDevtools />
+    </QueryClientProvider>
+  </React.StrictMode>
+);
