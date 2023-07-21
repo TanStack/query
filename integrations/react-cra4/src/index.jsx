a3b5ccbc6b128c42c144b12076e6455df45a6449
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-07-21 10:46:18 +1000

    Add react-cra4 test

--- integrations/react-cra4/src/index.jsx
+++ integrations/react-cra4/src/index.jsx
@@ -6,9 +6,7 @@ import App from './App';
 
 const queryClient = new QueryClient()
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
