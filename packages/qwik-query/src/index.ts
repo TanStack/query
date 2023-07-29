60f01846d7e6b00b196ad051e017f9e06ee246ea
gioboa <giorgiob.boa@gmail.com>
2023-07-29 10:53:24 +0200

    add useMutation

--- packages/qwik-query/src/index.ts
+++ packages/qwik-query/src/index.ts
@@ -2,6 +2,9 @@
 export * from '@tanstack/query-core'
 
 export * as QueryClientProvider from './QueryClientProvider'
+
 export { useQuery } from './useQuery'
 export { useInfiniteQuery } from './useInfiniteQuery'
 export { useIsFetching } from './useIsFetching'
+
+export { useMutation } from './useMutation'
---
+++ packages/qwik-query/src/useMutation.ts
@@ -0,0 +1,49 @@
+import type { NoSerialize } from '@builder.io/qwik';
+import {
+	noSerialize,
+	useSignal,
+	useStore,
+	useVisibleTask$,
+} from '@builder.io/qwik';
+import { MutationObserver, notifyManager } from '@tanstack/query-core';
+import { getQueryClient } from './useQueryClient';
+
+export function useMutation(options: any) {
+	const store = useStore<any>({});
+	const observerSig = useSignal<NoSerialize<MutationObserver>>();
+
+	useVisibleTask$(({ cleanup }) => {
+		const { observer, unsubscribe } = createMutationObserver(store, options);
+		observerSig.value = observer;
+
+		cleanup(unsubscribe);
+	});
+
+	useVisibleTask$(({ track }) => {
+		track(() => store.options);
+		if (observerSig.value) {
+			observerSig.value.setOptions(options);
+		}
+	});
+
+	return store;
+}
+
+const createMutationObserver = (store: any, options: any) => {
+	const client = getQueryClient();
+	const observer = new MutationObserver(client, options);
+
+	const unsubscribe = observer.subscribe(
+		notifyManager.batchCalls((result: any) => {
+			console.log('mutation subscribe', result);
+			store.result = noSerialize(observer.getCurrentResult());
+			store.mutateAsync = noSerialize(result.mutate);
+		})
+	);
+
+	store.mutate = noSerialize((variables: any, mutateOptions: any) => {
+		observer.mutate(variables, mutateOptions).catch(() => {});
+	});
+
+	return { observer: noSerialize(observer), unsubscribe };
+};
