b7757b157639b834665838f2a05b3ec8c50e2b06
gioboa <giorgiob.boa@gmail.com>
2023-07-29 10:35:01 +0200

    add getQueryClient method

--- packages/qwik-query/src/index.ts
+++ packages/qwik-query/src/index.ts
@@ -1,1 +1,5 @@
+// Re-export core
+export * from '@tanstack/query-core';
+
 export { test } from './utils'
+
---
+++ packages/qwik-query/src/useQueryClient.ts
@@ -0,0 +1,23 @@
+import { isServer } from '@builder.io/qwik/build';
+import { QueryClient, hydrate } from '@tanstack/query-core';
+
+let queryClient: QueryClient | null = null;
+
+export const getQueryClient = (dehydratedData?: any) => {
+	if (isServer) {
+		throw 'You can use getQueryClient only it the client side!';
+	}
+
+	if (queryClient && !dehydratedData) {
+		console.log('reuse');
+		return queryClient;
+	}
+
+	queryClient = new QueryClient();
+	if (dehydratedData) {
+		hydrate(queryClient, dehydratedData);
+	}
+
+	queryClient.mount();
+	return queryClient;
+};
