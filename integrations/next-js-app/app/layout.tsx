63b38f3912dfdc3a501de397ad4b391a18b32fb0
Dominik Dorfmeister <office@dorfmeister.cc>
2023-07-31 18:02:36 +0200

    fix: reduce next-js-app to bare minimum

--- integrations/next-js-app/app/layout.tsx
+++ integrations/next-js-app/app/layout.tsx
@@ -1,4 +1,3 @@
-import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 
--- integrations/next-js-app/next.config.js
+++ integrations/next-js-app/next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+}
 
 module.exports = nextConfig
