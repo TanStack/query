1c3838b76668ec083884f9425f1ddf3d000f8ba1
gioboa <giorgiob.boa@gmail.com>
2023-08-12 21:38:22 +0200

    add correct types

--- packages/qwik-query/cypress.config.ts
+++ packages/qwik-query/cypress.config.ts
@@ -7,4 +7,5 @@ export default defineConfig({
       bundler: "vite",
     },
   },
+  video: false
 });
--- packages/qwik-query/src/useBaseQuery.ts
+++ packages/qwik-query/src/useBaseQuery.ts
@@ -1,94 +1,111 @@
 import {
-	noSerialize,
-	useSignal,
-	useStore,
-	useVisibleTask$,
-	type NoSerialize,
-} from '@builder.io/qwik';
+  noSerialize,
+  useSignal,
+  useStore,
+  useVisibleTask$,
+  type NoSerialize,
+} from '@builder.io/qwik'
 import {
-	InfiniteQueryObserver,
-	QueryClient,
-	QueryObserver,
-	hydrate,
-	notifyManager,
-	type DehydratedState,
-} from '@tanstack/query-core';
-import { createQueryClient } from './useQueryClient';
+  InfiniteQueryObserver,
+  QueryClient,
+  QueryObserver,
+  hydrate,
+  notifyManager,
+  type DehydratedState,
+  DefaultedQueryObserverOptions,
+  QueryKey,
+} from '@tanstack/query-core'
+import { createQueryClient } from './useQueryClient'
 
 export enum ObserverType {
-	base,
-	inifinite,
+  base,
+  inifinite,
 }
 
 export const useBaseQuery = (
-	observerType: ObserverType,
-	options: any,
-	initialState?: DehydratedState
+  observerType: ObserverType,
+  options: DefaultedQueryObserverOptions<
+    unknown,
+    Error,
+    unknown,
+    unknown,
+    QueryKey
+  >,
+  initialState?: DehydratedState,
 ) => {
-	const queryClient = new QueryClient();
-	if (initialState) {
-		hydrate(queryClient, initialState);
-	}
-	const store = useStore<any>({
-		result: initialState
-			? queryClient.getQueryState(options.queryKey)
-			: undefined,
-		options,
-	});
-	const observerSig = useSignal<NoSerialize<QueryObserver>>();
+  const queryClient = new QueryClient()
+  if (initialState) {
+    hydrate(queryClient, initialState)
+  }
+  const store = useStore<any>({
+    result: initialState
+      ? queryClient.getQueryState(options.queryKey || [])
+      : undefined,
+    options,
+  })
+  const observerSig = useSignal<NoSerialize<QueryObserver>>()
 
-	useVisibleTask$(({ cleanup }) => {
-		const { observer, unsubscribe, defaultedOptions } = createQueryObserver(
-			store,
-			options,
-			observerType
-		);
-		observerSig.value = observer;
-		store.options = defaultedOptions;
+  useVisibleTask$(
+    ({ cleanup }) => {
+      const { observer, unsubscribe, defaultedOptions } = createQueryObserver(
+        store,
+        options,
+        observerType,
+      )
+      observerSig.value = observer
+      store.options = defaultedOptions
 
-		cleanup(unsubscribe);
-	}, { strategy: 'document-ready'});
+      cleanup(unsubscribe)
+    },
+    { strategy: 'document-ready' },
+  )
 
-	useVisibleTask$(({ track }) => {
-		track(() => store.options);
-		if (observerSig.value) {
-			observerSig.value.setOptions(store.options || options);
-		}
-	});
+  useVisibleTask$(({ track }) => {
+    track(() => store.options)
+    if (observerSig.value) {
+      observerSig.value.setOptions(store.options || options)
+    }
+  })
 
-	return store;
-};
+  return store
+}
 
 const createQueryObserver = (
-	store: any,
-	options: any,
-	observerType: ObserverType
+  store: any,
+  options: DefaultedQueryObserverOptions<
+    unknown,
+    Error,
+    unknown,
+    unknown,
+    QueryKey
+  >,
+  observerType: ObserverType,
 ) => {
-	const Observer =
-		observerType === ObserverType.base
-			? QueryObserver
-			: (InfiniteQueryObserver as typeof QueryObserver);
-	const client = createQueryClient();
+  const Observer =
+    observerType === ObserverType.base
+      ? QueryObserver
+      : (InfiniteQueryObserver as typeof QueryObserver)
+  const client = createQueryClient()
 
-	const defaultedOptions = client.defaultQueryOptions(options);
-	defaultedOptions._optimisticResults = 'optimistic';
-	defaultedOptions.structuralSharing = false;
+  const defaultedOptions = client.defaultQueryOptions(options)
+  defaultedOptions._optimisticResults = 'optimistic'
+  defaultedOptions.structuralSharing = false
 
-	const observer = new Observer(client, defaultedOptions);
-	if (!store.result) {
-		const result = observer.getOptimisticResult(defaultedOptions);
-		store.result = !defaultedOptions.notifyOnChangeProps
-			? noSerialize(observer.trackResult(result))
-			: noSerialize(result);
-	}
+  const observer = new Observer(client, defaultedOptions)
+  if (!store.result) {
+    const result = observer.getOptimisticResult(defaultedOptions)
+    store.result = !defaultedOptions.notifyOnChangeProps
+      ? noSerialize(observer.trackResult(result))
+      : noSerialize(result)
+  }
 
-	const unsubscribe = observer.subscribe(
-		notifyManager.batchCalls((result: any) => {
-			store.result = !defaultedOptions.notifyOnChangeProps
-				? noSerialize(observer.trackResult(result))
-				: noSerialize(result);
-		})
-	);
+  const unsubscribe = observer.subscribe(
+    notifyManager.batchCalls((result: any) => {
+      store.result = !defaultedOptions.notifyOnChangeProps
+        ? noSerialize(observer.trackResult(result))
+        : noSerialize(result)
+    }),
+  )
 
-	return { observer: noSerialize(observer), unsubscribe, defaultedOptions };
-};
+  return { observer: noSerialize(observer), unsubscribe, defaultedOptions }
+}
