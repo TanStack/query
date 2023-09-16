9ee89675667756d4b0982b4ee9aa22ec0c8acd6b
Dominik Dorfmeister <office@dorfmeister.cc>
2023-09-16 12:17:02 +0200

    chore: don't add bun-types globally

--- setup-test.ts
+++ setup-test.ts
@@ -1,3 +1,4 @@
+// @ts-expect-error we don't want to add bun-types to our global tsconfig
 import { afterEach, beforeEach } from 'bun:test'
 import { GlobalRegistrator } from '@happy-dom/global-registrator'
 
