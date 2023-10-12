fe4f84f10173886c2bb9932366d1894faea33ad4
Dominik Dorfmeister <office@dorfmeister.cc>
2023-10-12 13:56:38 +0200

    chore: manual re-export of the core

--- packages/react-query/src/index.ts
+++ packages/react-query/src/index.ts
@@ -1,7 +1,49 @@
 /* istanbul ignore file */
 
 // Re-export core
-export * from '@tanstack/query-core'
+export { CancelledError } from '@tanstack/query-core'
+export { QueryCache } from '@tanstack/query-core'
+export type { QueryCacheNotifyEvent } from '@tanstack/query-core'
+export { QueryClient } from '@tanstack/query-core'
+export { QueryObserver } from '@tanstack/query-core'
+export { QueriesObserver } from '@tanstack/query-core'
+export { InfiniteQueryObserver } from '@tanstack/query-core'
+export { MutationCache } from '@tanstack/query-core'
+export { MutationObserver } from '@tanstack/query-core'
+export { notifyManager } from '@tanstack/query-core'
+export { focusManager } from '@tanstack/query-core'
+export { onlineManager } from '@tanstack/query-core'
+export {
+  hashKey,
+  replaceEqualDeep,
+  isServer,
+  matchQuery,
+  keepPreviousData,
+} from '@tanstack/query-core'
+export type {
+  MutationFilters,
+  QueryFilters,
+  Updater,
+} from '@tanstack/query-core'
+export { isCancelledError } from '@tanstack/query-core'
+export {
+  dehydrate,
+  hydrate,
+  defaultShouldDehydrateQuery,
+  defaultShouldDehydrateMutation,
+} from '@tanstack/query-core'
+
+// Types
+export type * from './types'
+export type { QueryState } from '@tanstack/query-core'
+export { Query } from '@tanstack/query-core'
+export type { Mutation, MutationState } from '@tanstack/query-core'
+export type {
+  DehydrateOptions,
+  DehydratedState,
+  HydrateOptions,
+} from '@tanstack/query-core'
+export type { QueriesObserverOptions } from '@tanstack/query-core'
 
 // React Query
 export type * from './types'
