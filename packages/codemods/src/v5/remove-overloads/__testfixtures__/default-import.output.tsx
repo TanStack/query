7933834b3878c84aecd4d60abb1058d185aa3fc8
Balázs Máté Petró <petrobalazsmate@gmail.com>
2023-10-20 11:28:32 +0200

    fix(codemod): remove the function overloads when the second argument is a function expression (#6191)
    * fix(codemod): remove the function overloads when the second argument is a function expression
    
    After a quick sync with @TkDodo, it turned out in this case it's safe to do the transformation, because the second argument is a function expression, so it means the first parameter must be the query key.
    
    * fix(codemod): add the `useQuery` hook to the transformation list, because it was missing
    
    * chore(codemod): rename the `parameters` constant to `functionArguments`
    
    The naming was wrong from the beginning. The codemod does transformations with the arguments of the functions, not with their parameters. :)
    
    * chore(codemod): fix the import order
    
    The CI was failing because of it.
    
    ---------
    
    Co-authored-by: Dominik Dorfmeister <office@dorfmeister.cc>

--- packages/codemods/src/v5/remove-overloads/__testfixtures__/default-import.output.tsx
+++ packages/codemods/src/v5/remove-overloads/__testfixtures__/default-import.output.tsx
@@ -2,6 +2,7 @@ import * as React from 'react'
 import {
   useIsFetching,
   useIsMutating,
+  useQuery,
   useQueryClient,
 } from '@tanstack/react-query'
 import { queryKeysFromAnotherModule } from '../another/module'
@@ -481,4 +482,20 @@ export const WithIdentifiers = () => {
   queryClient.fetchQuery(queryKeysFromAnotherModule)
   queryClient.fetchQuery(queryKeysFromAnotherModule, fetchOptions)
   queryClient.fetchQuery(queryKeysFromAnotherModule, queryFn, fetchOptions)
+}
+
+export const SecondArgumentIsAFunctionExample = () => {
+  useQuery({
+    queryKey: ordersCacheKeys.groupOrders(ouuid),
+    queryFn: () => api.getPatientGroupOrders(ouuid).then((r) => r.data)
+  })
+
+  const rest = 'rest'
+  const of = 1
+  const functionArguments = { foo: 'bar' }
+
+  useQuery({
+    queryKey: ordersCacheKeys.groupOrders(ouuid),
+    queryFn: () => api.getPatientGroupOrders(ouuid).then((r) => r.data)
+  }, rest, of, functionArguments)
 }
