06820543d2b432e81ac3e63b564f1c9d162f45e1
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-10-22 09:43:54 +1100

    Fix svelte tests

--- packages/svelte-query-persist-client/vitest-setup.ts
+++ packages/svelte-query-persist-client/vitest-setup.ts
@@ -1,8 +1,5 @@
 import '@testing-library/jest-dom/vitest'
-import matchers from '@testing-library/jest-dom/matchers'
 import { cleanup } from '@testing-library/svelte'
-import { afterEach, expect } from 'vitest'
-
-expect.extend(matchers)
+import { afterEach } from 'vitest'
 
 afterEach(() => cleanup())
