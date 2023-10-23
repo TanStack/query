b35fe32f58960bc4b6e89fa8b7bd00f7f000700b
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-10-23 12:16:56 +1100

    fix(svelte-query): Make `combine` work for createQueries (#6222)

--- packages/svelte-query/src/__tests__/CreateQueries.svelte
+++ packages/svelte-query/src/__tests__/CreateQueries.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
-  import { QueryClient } from '@tanstack/query-core'
-  import { setQueryClientContext } from '../context'
   import { createQueries } from '../createQueries'
-  import type { QueriesOptions } from '../createQueries'
+  import type { QueryClient } from '@tanstack/query-core'
+  import type { QueriesOptions, QueriesResults } from '../createQueries'
 
-  export let options: { queries: [...QueriesOptions<any>] }
+  export let options: {
+    queries: [...QueriesOptions<any>]
+    combine?: (result: QueriesResults<Array<any>>) => any
+  }
+  export let queryClient: QueryClient
 
-  const queryClient = new QueryClient()
-  setQueryClientContext(queryClient)
-
-  const queries = createQueries(options)
+  const queries = createQueries(options, queryClient)
 </script>
 
-{#each $queries as query, index}
-  {#if query.isPending}
-    <p>Loading {index + 1}</p>
-  {:else if query.isSuccess}
-    <p>{query.data}</p>
-  {/if}
-{/each}
+{#if Array.isArray($queries)}
+  {#each $queries as query, index}
+    {#if query.isPending}
+      <p>Loading {index + 1}</p>
+    {:else if query.isSuccess}
+      <p>{query.data}</p>
+    {/if}
+  {/each}
+{:else if $queries.isPending}
+  <p>Loading</p>
+{:else if $queries.isSuccess}
+  <p>{$queries.data}</p>
+{/if}
--- packages/svelte-query/src/__tests__/createQueries.test.ts
+++ packages/svelte-query/src/__tests__/createQueries.test.ts
@@ -1,5 +1,6 @@
 import { describe, expect, it } from 'vitest'
 import { render, waitFor } from '@testing-library/svelte'
+import { QueryClient } from '@tanstack/query-core'
 import CreateQueries from './CreateQueries.svelte'
 import { sleep } from './utils'
 
@@ -25,6 +26,7 @@ describe('createQueries', () => {
             },
           ],
         },
+        queryClient: new QueryClient(),
       },
     })
 
@@ -36,6 +38,40 @@ describe('createQueries', () => {
     await waitFor(() => {
       expect(rendered.getByText('Success 1')).toBeInTheDocument()
       expect(rendered.getByText('Success 2')).toBeInTheDocument()
+    })
+  })
+
+  it('should combine queries', async () => {
+    const ids = [1, 2, 3]
+
+    const rendered = render(CreateQueries, {
+      props: {
+        options: {
+          queries: ids.map((id) => ({
+            queryKey: [id],
+            queryFn: async () => {
+              await sleep(10)
+              return id
+            },
+          })),
+          combine: (results) => {
+            return {
+              isPending: results.some((result) => result.isPending),
+              isSuccess: results.every((result) => result.isSuccess),
+              data: results.map((res) => res.data).join(','),
+            }
+          },
+        },
+        queryClient: new QueryClient(),
+      },
+    })
+
+    await waitFor(() => {
+      expect(rendered.getByText('Loading')).toBeInTheDocument()
+    })
+
+    await waitFor(() => {
+      expect(rendered.getByText('1,2,3')).toBeInTheDocument()
     })
   })
 })
