9fd29542826b96b968dc0d75d2a90c475d01bf33
balazsmatepetro <petrobalazsmate@gmail.com>
2023-10-30 18:57:19 +0100

    refactor(codemod): transform the usage only when the `keepPreviousData` has value `true`

--- packages/codemods/src/v5/keep-previous-data/__testfixtures__/default.input.tsx
+++ packages/codemods/src/v5/keep-previous-data/__testfixtures__/default.input.tsx
@@ -8,13 +8,18 @@ type Post = {
   body: string
 }
 
+const queryFn = async (): Promise<Array<Post>> => {
+  const { data } = await axios.get(
+    'https://jsonplaceholder.typicode.com/posts',
+  )
+  return data
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with value `true`, so the codemod should transform
+ * this usage.
+ */
 export const Example1 = () => {
-  const queryFn = async (): Promise<Array<Post>> => {
-    const { data } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts',
-    )
-    return data
-  }
   const { data } = useQuery({
     queryKey: ['posts'],
     queryFn: queryFn,
@@ -24,13 +29,11 @@ export const Example1 = () => {
   return <div>{JSON.stringify(data)}</div>
 }
 
+/**
+ * The object expression has a `keepPreviousData` property with value `true`, but the `placeholderData` is a function.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
 export const Example2 = () => {
-  const queryFn = async (): Promise<Array<Post>> => {
-    const { data } = await axios.get(
-        'https://jsonplaceholder.typicode.com/posts',
-    )
-    return data
-  }
   const { data } = useQueries({
     queryKey: ['posts'],
     queryFn: queryFn,
@@ -41,18 +44,46 @@ export const Example2 = () => {
   return <div>{JSON.stringify(data)}</div>
 }
 
+/**
+ * The object expression has a `keepPreviousData` property with value `true`, but the `placeholderData` is a string.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
 export const Example3 = () => {
-  const queryFn = async (): Promise<Array<Post>> => {
-    const { data } = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts',
-    )
-    return data
-  }
   const { data } = useQueries({
     queryKey: ['posts'],
     queryFn: queryFn,
     keepPreviousData: true,
     placeholderData: "somePlaceholderData"
+  })
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with value `false`.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
+export const Example4 = () => {
+  const { data } = useQueries({
+    queryKey: ['posts'],
+    queryFn: queryFn,
+    keepPreviousData: false,
+  })
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with which is an identifier.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
+export const Example5 = () => {
+  const keepPreviousDataIdentifier = false
+  const { data } = useQueries({
+    queryKey: ['posts'],
+    queryFn: queryFn,
+    keepPreviousData: keepPreviousDataIdentifier,
+    placeholderData: () => previousData
   })
 
   return <div>{JSON.stringify(data)}</div>
--- packages/codemods/src/v5/keep-previous-data/keep-previous-data.js
+++ packages/codemods/src/v5/keep-previous-data/keep-previous-data.js
@@ -14,15 +14,46 @@ const AlreadyHasPlaceholderDataProperty = require('./utils/already-has-placehold
  */
 const transformUsages = ({ jscodeshift, utils, root, filePath, config }) => {
   /**
+   * @param {import('jscodeshift').CallExpression} callExpression
+   * @returns {{start: number, end: number}}
+   */
+  const getCallExpressionLocation = (callExpression) => {
+    const location = callExpression.callee.loc
+    const start = location.start.line
+    const end = location.end.line
+
+    return { start, end }
+  }
+
+  /**
+   * @param {import('jscodeshift').ObjectProperty} objectProperty
+   * @returns {boolean}
+   */
+  const isKeepPreviousDataObjectProperty = (objectProperty) => {
+    return jscodeshift.match(objectProperty.key, {
+      type: jscodeshift.Identifier.name,
+      name: 'keepPreviousData',
+    })
+  }
+
+  /**
+   * @param {import('jscodeshift').ObjectProperty} objectProperty
+   * @returns {boolean}
+   */
+  const isObjectPropertyHasTrueBooleanLiteralValue = (objectProperty) => {
+    return jscodeshift.match(objectProperty.value, {
+      type: jscodeshift.BooleanLiteral.name,
+      value: true,
+    })
+  }
+
+  /**
    * @param {import('jscodeshift').ObjectExpression} objectExpression
    * @returns {Array<import('jscodeshift').ObjectProperty>}
    */
   const filterKeepPreviousDataProperty = (objectExpression) => {
-    return objectExpression.properties.filter((property) => {
-      return !jscodeshift.match(property.key, {
-        type: jscodeshift.Identifier.name,
-        name: 'keepPreviousData',
-      })
+    return objectExpression.properties.filter((objectProperty) => {
+      return !isKeepPreviousDataObjectProperty(objectProperty)
     })
   }
 
@@ -35,15 +66,14 @@ const transformUsages = ({ jscodeshift, utils, root, filePath, config }) => {
 
   /**
    * @param {import('jscodeshift').ObjectExpression} objectExpression
-   * @param {string} propertyName
    * @returns {boolean}
    */
-  const hasObjectProperty = (objectExpression, propertyName) => {
+  const hasPlaceholderDataProperty = (objectExpression) => {
     return (
-      objectExpression.properties.findIndex((property) => {
-        return jscodeshift.match(property.key, {
+      objectExpression.properties.findIndex((objectProperty) => {
+        return jscodeshift.match(objectProperty.key, {
           type: jscodeshift.Identifier.name,
-          name: propertyName,
+          name: 'placeholderData',
         })
       }) !== -1
     )
@@ -51,18 +81,10 @@ const transformUsages = ({ jscodeshift, utils, root, filePath, config }) => {
 
   /**
    * @param {import('jscodeshift').ObjectExpression} objectExpression
-   * @returns {boolean}
+   * @returns {import('jscodeshift').ObjectProperty | undefined}
    */
-  const hasPlaceholderDataProperty = (objectExpression) => {
-    return hasObjectProperty(objectExpression, 'placeholderData')
-  }
-
-  /**
-   * @param {import('jscodeshift').ObjectExpression} objectExpression
-   * @returns {boolean}
-   */
-  const hasKeepPreviousDataProperty = (objectExpression) => {
-    return hasObjectProperty(objectExpression, 'keepPreviousData')
+  const getKeepPreviousDataProperty = (objectExpression) => {
+    return objectExpression.properties.find(isKeepPreviousDataObjectProperty)
   }
 
   let isKeepPreviousDataInUse = false
@@ -70,32 +92,65 @@ const transformUsages = ({ jscodeshift, utils, root, filePath, config }) => {
   const replacer = (path) => {
     const node = path.node
     const functionArguments = []
+    const { start, end } = getCallExpressionLocation(node)
 
     try {
       const firstArgument = node.arguments[0] ?? null
 
-      if (
-        firstArgument &&
-        utils.isObjectExpression(firstArgument) &&
-        hasKeepPreviousDataProperty(firstArgument)
-      ) {
+      if (firstArgument && utils.isObjectExpression(firstArgument)) {
+        const isPlaceholderDataPropertyPresent =
+          hasPlaceholderDataProperty(firstArgument)
+
         if (hasPlaceholderDataProperty(firstArgument)) {
           throw new AlreadyHasPlaceholderDataProperty(node, filePath)
         }
 
-        const filteredProperties = filterKeepPreviousDataProperty(firstArgument)
+        const keepPreviousDataProperty =
+          getKeepPreviousDataProperty(firstArgument)
 
-        functionArguments.push(
-          jscodeshift.objectExpression([
-            ...filteredProperties,
-            createPlaceholderDataObjectProperty(),
-          ]),
-        )
+        const keepPreviousDataPropertyHasTrueValue =
+          isObjectPropertyHasTrueBooleanLiteralValue(keepPreviousDataProperty)
+
+        if (!keepPreviousDataPropertyHasTrueValue) {
+          utils.warn(
+            `The usage in file "${filePath}" at line ${start}:${end} already contains a "keepPreviousData" property but its value is not "true". Please migrate this usage manually.`,
+          )
+
+          return node
+        }
+
+        if (keepPreviousDataPropertyHasTrueValue) {
+          // Removing the `keepPreviousData` property from the object.
+          const mutableObjectExpressionProperties =
+            filterKeepPreviousDataProperty(firstArgument)
+
+          if (!isPlaceholderDataPropertyPresent) {
+            isKeepPreviousDataInUse = true
+
+            // When the `placeholderData` property is not present, the `placeholderData: keepPreviousData` property will be added.
+            mutableObjectExpressionProperties.push(
+              createPlaceholderDataObjectProperty(),
+            )
+          }
+
+          functionArguments.push(
+            jscodeshift.objectExpression(mutableObjectExpressionProperties),
+          )
+
+          return jscodeshift.callExpression(
+            node.original.callee,
+            functionArguments,
+          )
+        }
       }
 
-      isKeepPreviousDataInUse = true
+      utils.warn(
+        `The usage in file "${filePath}" at line ${start}:${end} could not be transformed, because the first parameter is not an object expression. Please migrate this usage manually.`,
+      )
 
-      return jscodeshift.callExpression(node.original.callee, functionArguments)
+      return node
+
+      return node
     } catch (error) {
       utils.warn(
         error.name === AlreadyHasPlaceholderDataProperty.name
