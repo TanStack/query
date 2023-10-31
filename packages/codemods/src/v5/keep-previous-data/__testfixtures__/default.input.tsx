d4c9f420b338bbc2d288ad4c9f0cc9ee21045618
Balázs Máté Petró <petrobalazsmate@gmail.com>
2023-10-31 11:25:40 +0100

    feat: `placeholderData` codemod (#6196)
    * feat(codemod): add initial implementation of the `placeholderData` codemod
    
    * chore(codemod): remove the parameter from the `placeholderData` function examples
    
    It was a request by @TkDodo, since in `v4` the `placeholderData` function didn't have a parameter.
    
    For further details see: https://github.com/TanStack/query/pull/6196#discussion_r1366618334
    
    * chore(codemod): add an example where the `placeholderData` property is not a function
    
    It was a request by @TkDodo, see: https://github.com/TanStack/query/pull/6196#discussion_r1366618334
    
    * refactor(codemod): transform the usage only when the `keepPreviousData` has value `true`
    
    ---------
    
    Co-authored-by: Dominik Dorfmeister <office@dorfmeister.cc>

---
+++ packages/codemods/src/v5/keep-previous-data/__testfixtures__/default.input.tsx
@@ -0,0 +1,90 @@
+import * as React from 'react'
+import axios from 'axios'
+import { useQueries, useQuery } from '@tanstack/react-query'
+
+type Post = {
+  id: number
+  title: string
+  body: string
+}
+
+const queryFn = async (): Promise<Array<Post>> => {
+  const { data } = await axios.get(
+    'https://jsonplaceholder.typicode.com/posts',
+  )
+  return data
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with value `true`, so the codemod should transform
+ * this usage.
+ */
+export const Example1 = () => {
+  const { data } = useQuery({
+    queryKey: ['posts'],
+    queryFn: queryFn,
+    keepPreviousData: true,
+  })
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with value `true`, but the `placeholderData` is a function.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
+export const Example2 = () => {
+  const { data } = useQueries({
+    queryKey: ['posts'],
+    queryFn: queryFn,
+    keepPreviousData: true,
+    placeholderData: () => previousData
+  })
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with value `true`, but the `placeholderData` is a string.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
+export const Example3 = () => {
+  const { data } = useQueries({
+    queryKey: ['posts'],
+    queryFn: queryFn,
+    keepPreviousData: true,
+    placeholderData: "somePlaceholderData"
+  })
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with value `false`.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
+export const Example4 = () => {
+  const { data } = useQueries({
+    queryKey: ['posts'],
+    queryFn: queryFn,
+    keepPreviousData: false,
+  })
+
+  return <div>{JSON.stringify(data)}</div>
+}
+
+/**
+ * The object expression has a `keepPreviousData` property with which is an identifier.
+ * The codemod shouldn't transform this case, only warn the user about the manual migration.
+ */
+export const Example5 = () => {
+  const keepPreviousDataIdentifier = false
+  const { data } = useQueries({
+    queryKey: ['posts'],
+    queryFn: queryFn,
+    keepPreviousData: keepPreviousDataIdentifier,
+    placeholderData: () => previousData
+  })
+
+  return <div>{JSON.stringify(data)}</div>
+}
