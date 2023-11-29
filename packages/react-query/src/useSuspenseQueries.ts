3319d80d598a0600feed4bf7d7becc351e711ca2
Charlotte Grace (Bone) <charlotte.bone@gmail.com>
2023-11-29 07:28:27 +0000

    fix(types): Fix types when using select with the queryOptions helper (#6454)
    useQueries, createQueries and useSuspenseQueries now return the correct data type if using the queryOptions helper with the select option.
    
    fixes #6442
    
    Co-authored-by: Dominik Dorfmeister <office@dorfmeister.cc>

--- packages/react-query/src/useSuspenseQueries.ts
+++ packages/react-query/src/useSuspenseQueries.ts
@@ -34,7 +34,7 @@ type GetSuspenseOptions<T> =
               : // Part 3: responsible for inferring and enforcing type if no explicit parameter was provided
                 T extends {
                     queryFn?: QueryFunction<infer TQueryFnData, infer TQueryKey>
-                    select: (data: any) => infer TData
+                    select?: (data: any) => infer TData
                     throwOnError?: ThrowOnError<any, infer TError, any, any>
                   }
                 ? UseSuspenseQueryOptions<
@@ -76,12 +76,12 @@ type GetSuspenseResults<T> =
               ? UseSuspenseQueryResult<TQueryFnData>
               : // Part 3: responsible for mapping inferred type to results, if no explicit parameter was provided
                 T extends {
-                    queryFn?: QueryFunction<unknown, any>
-                    select: (data: any) => infer TData
+                    queryFn?: QueryFunction<infer TQueryFnData, any>
+                    select?: (data: any) => infer TData
                     throwOnError?: ThrowOnError<any, infer TError, any, any>
                   }
                 ? UseSuspenseQueryResult<
-                    TData,
+                    unknown extends TData ? TQueryFnData : TData,
                     unknown extends TError ? DefaultError : TError
                   >
                 : T extends {
