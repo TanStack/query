5d29f8ff47958a2349fdc4ca682c3630beeabc9d
Lars Kappert <lars@webpro.nl>
2023-11-29 15:58:02 +0100

    chore: introduce Knip as dead-code removal tool (#6368)
    * Add knip with inital config
    
    * Rename vite.config.js → vite.config.js (`__dirname` not in ESM scope)
    
    * chore: upgrade to knip v3
    
    * chore(knip): remove duplicate exports
    
    * chore(knip): remove unused types from query devtools
    
    * chore(knip): do not export unused types
    
    * chore(knip): run knip in ci
    
    * chore(knip): ignore certain .d.ts files
    
    * chore(knip): remove unused file
    
    * chore(knip): use devtools in next integration
    
    so the dependency doesn't show up as unused
    
    * chore(knip): remove unused dependencies
    
    * chore(knip): remove unused exports
    
    * chore(knip): add unlisted dependencies
    
    * chore(knip): remove unnecessary scope-manager dependency
    
    * Update knip to 3.3.0
    
    * Update knip to 3.3.1
    
    * chore: ignore custom deps, remove unneeded file
    
    * chore: remove unused util
    
    * chore: remove esbuild dep as it's not directly used
    
    * chore: bump tsup to match esbuild types with the plugin
    
    * chore: remove unused peer deps
    
    * chore: remove unneeded deps
    
    ---------
    
    Co-authored-by: Dominik Dorfmeister <office@dorfmeister.cc>
    Co-authored-by: Damian Osipiuk <osipiukd+git@gmail.com>

---
+++ knip.ts
@@ -0,0 +1,28 @@
+// These aren't actual compilers, but performant & avoids root dependencies
+const svelteCompiler = (text: string) => text.matchAll(/import[^]*?'.*?'\n/gs)
+const vueCompiler = /<script\b[^>]*>([\s\S]*?)<\/script>/gm
+
+export default {
+  $schema: 'https://unpkg.com/knip@2/schema.json',
+  ignoreWorkspaces: ['examples/**'],
+  ignore: ['**/react-app-env.d.ts', '**/vite-env.d.ts'],
+  workspaces: {
+    'packages/codemods': {
+      entry: ['src/v4/*.js', 'src/v5/*/*.js'],
+      ignore: ['**/__testfixtures__/**'],
+    },
+    'packages/vue-query': {
+      ignore: ['**/__mocks__/**'],
+      ignoreDependencies: ['vue2', 'vue2.7'],
+    },
+  },
+  compilers: {
+    svelte: (text: string) => [...svelteCompiler(text)].join('\n'),
+    vue: (text) => {
+      const scripts = []
+      let match
+      while ((match = vueCompiler.exec(text))) scripts.push(match[1])
+      return scripts.join(';')
+    },
+  },
+}
