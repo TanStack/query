425aaf6680424fe56cc0018d4359050501e10ff4
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-29 20:49:16 +1100

    chore: Ensure each package has Vite framework adapter (#6609)
    * Add react plugin
    
    * Add vue and solid plugins
    
    * Add angular plugin
    
    * Rename to vite.config.ts
    
    * Angular can do whatever it wants

--- packages/solid-query/vite.config.ts
+++ packages/solid-query/vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vitest/config'
+import solid from 'vite-plugin-solid'
 
 export default defineConfig({
+  plugins: [solid()],
   test: {
-    name: 'react-query-next-experimental',
+    name: 'solid-query',
     dir: './src',
     watch: false,
     setupFiles: ['test-setup.ts'],
     environment: 'jsdom',
     coverage: { provider: 'istanbul', include: ['src/**/*'] },
+    server: {
+      deps: {
+        // https://github.com/solidjs/solid-testing-library#known-issues
+        inline: [/solid-js/],
+      },
+    },
   },
 })
