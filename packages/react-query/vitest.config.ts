652f28928e132cd8afcaf65c316c3df3fed5c88f
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-29 19:56:04 +1100

    Add react plugin

--- packages/react-query/vitest.config.ts
+++ packages/react-query/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  plugins: [react()],
   test: {
     name: 'react-query',
     dir: './src',
