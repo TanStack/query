cbaaef2e6a1b1c6bc0deb33523fa3c94988f9cc7
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-29 14:04:44 +1100

    Move to its own file

--- packages/react-query/vite.config.js
+++ packages/react-query/vite.config.js
@@ -1,76 +1,8 @@
 // @ts-check
 
-import { readdirSync, renameSync } from 'node:fs'
-import { externalizeDeps } from 'vite-plugin-externalize-deps'
-import dts from 'vite-plugin-dts'
-import { defineConfig } from 'vite'
+import { getViteConfig } from '../../scripts/getViteConfig.js'
 
-/**
- * @param {import("vite").UserConfig} config
- * @returns {import('vite').UserConfig}
- */
-export const tanstackBundler = (config) => {
-  /** @type {import("vite").PluginOption[]} */
-  const plugins = [
-    dts({
-      entryRoot: `./src`,
-      outDir: `./dist/esm`,
-      exclude: './src/__tests__',
-      compilerOptions: {
-        // @ts-expect-error
-        module: 'esnext',
-        declarationMap: true,
-      },
-    }),
-    dts({
-      entryRoot: `./src`,
-      outDir: `./dist/cjs`,
-      exclude: './src/__tests__',
-      compilerOptions: {
-        // @ts-expect-error
-        module: 'commonjs',
-        declarationMap: false,
-      },
-      afterBuild: () => {
-        const path = './dist/cjs'
-        readdirSync(path).forEach((file) => {
-          if (file.includes('.d.ts')) {
-            renameSync(
-              `${path}/${file}`,
-              `${path}/${file.replace('.d.ts', '.d.cts')}`,
-            )
-          }
-        })
-      },
-    }),
-    externalizeDeps(),
-  ]
-
-  config.plugins = config.plugins ? config.plugins.concat(plugins) : plugins
-
-  config.build = {
-    outDir: `./dist`,
-    minify: false,
-    sourcemap: true,
-    lib: {
-      entry: './src/index.ts',
-      formats: ['es', 'cjs'],
-      fileName: (format) => {
-        if (format === 'cjs') return `cjs/[name].cjs`
-        return `esm/[name].js`
-      },
-    },
-    rollupOptions: {
-      output: {
-        preserveModules: true,
-      },
-    },
-  }
-
-  return defineConfig(config)
-}
-
-export default tanstackBundler({
+export default getViteConfig({
   test: {
     name: 'react-query',
     dir: './src',
