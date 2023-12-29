d4acdc82552270086ffb44c04feca697d827a05b
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2023-12-29 20:09:14 +1100

    Add angular plugin

--- packages/angular-query-devtools-experimental/vitest.config.ts
+++ packages/angular-query-devtools-experimental/vitest.config.ts
@@ -1,6 +1,8 @@
 import { defineConfig } from 'vitest/config'
+import angular from '@analogjs/vite-plugin-angular'
 
 export default defineConfig({
+  plugins: [angular()],
   test: {
     name: 'angular-query-devtools-experimental',
     dir: './src',
