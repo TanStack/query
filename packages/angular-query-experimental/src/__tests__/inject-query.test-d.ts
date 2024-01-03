0779914093721ea07d8790008f48bd0b2ddfd1f2
Arnoud de Vries <6420061+arnoud-dv@users.noreply.github.com>
2024-01-03 15:13:41 +0100

    chore(angular-query): query and mutation type tests

---
+++ packages/angular-query-experimental/src/__tests__/inject-query.test-d.ts
@@ -0,0 +1,59 @@
+import { describe, expectTypeOf } from 'vitest'
+import { injectQuery } from '../inject-query'
+import { simpleFetcher } from './test-utils'
+import type { Signal } from '@angular/core'
+
+describe('Discriminated union return type', () => {
+  test('data should be possibly undefined by default', () => {
+    const query = injectQuery(() => ({
+      queryKey: ['key'],
+      queryFn: simpleFetcher,
+    }))
+
+    expectTypeOf(query.data).toEqualTypeOf<Signal<string> | Signal<undefined>>()
+  })
+
+  test('data should be defined when query is success', () => {
+    const query = injectQuery(() => ({
+      queryKey: ['key'],
+      queryFn: simpleFetcher,
+    }))
+
+    if (query.isSuccess()) {
+      expectTypeOf(query.data).toEqualTypeOf<Signal<string>>()
+    }
+  })
+
+  test('error should be null when query is success', () => {
+    const query = injectQuery(() => ({
+      queryKey: ['key'],
+      queryFn: simpleFetcher,
+    }))
+
+    if (query.isSuccess()) {
+      expectTypeOf(query.error).toEqualTypeOf<Signal<null>>()
+    }
+  })
+
+  test('data should be undefined when query is pending', () => {
+    const query = injectQuery(() => ({
+      queryKey: ['key'],
+      queryFn: simpleFetcher,
+    }))
+
+    if (query.isPending()) {
+      expectTypeOf(query.data).toEqualTypeOf<Signal<undefined>>()
+    }
+  })
+
+  test('error should be defined when query is error', () => {
+    const query = injectQuery(() => ({
+      queryKey: ['key'],
+      queryFn: simpleFetcher,
+    }))
+
+    if (query.isError()) {
+      expectTypeOf(query.error).toEqualTypeOf<Signal<Error>>()
+    }
+  })
+})
