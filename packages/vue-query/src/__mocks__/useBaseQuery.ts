f62df97ae17cf0d87b82ac6fce888bbecd70209f
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-05 21:46:54 +1100

    Remove ts-expect-error

--- packages/vue-query/src/__mocks__/useBaseQuery.ts
+++ packages/vue-query/src/__mocks__/useBaseQuery.ts
@@ -2,7 +2,6 @@ import { vi } from 'vitest'
 import type { Mock } from 'vitest'
 
 const { useBaseQuery: originImpl, unrefQueryArgs: originalParse } =
-  // @ts-expect-error - vitest uses esmodules; tsconfig is not set to use them
   (await vi.importActual('../useBaseQuery')) as any
 
 export const useBaseQuery: Mock<Array<any>, any> = vi.fn(originImpl)
