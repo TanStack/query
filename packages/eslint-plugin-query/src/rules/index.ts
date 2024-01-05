69a0c87e624e651607083b2b35e78e4d9e8e9613
minjee <minjeeson.dev@gmail.com>
2024-01-05 19:54:35 +0000

    rename a reference

--- packages/eslint-plugin-query/src/rules/index.ts
+++ packages/eslint-plugin-query/src/rules/index.ts
@@ -1,11 +1,11 @@
 import * as exhaustiveDeps from './exhaustive-deps/exhaustive-deps.rule'
-import * as noCallbacks from './no-deprecated-options/no-deprecated-options.rule'
+import * as noDeprecatedOptions from './no-deprecated-options/no-deprecated-options.rule'
 import * as preferObjectSyntax from './prefer-query-object-syntax/prefer-query-object-syntax'
 import * as stableQueryClient from './stable-query-client/stable-query-client.rule'
 
 export const rules = {
   [exhaustiveDeps.name]: exhaustiveDeps.rule,
-  [noCallbacks.name]: noCallbacks.rule,
+  [noDeprecatedOptions.name]: noDeprecatedOptions.rule,
   [preferObjectSyntax.name]: preferObjectSyntax.rule,
   [stableQueryClient.name]: stableQueryClient.rule,
 }
