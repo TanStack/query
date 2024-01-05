93ba62359c9c83de29c11a8a0bc49e58ec159b0d
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-05 21:39:20 +1100

    fix: Correctly exit publish script

--- scripts/publish.js
+++ scripts/publish.js
@@ -3,14 +3,11 @@
 import { publish } from '@tanstack/config/publish'
 import { branchConfigs, packages, rootDir } from './config.js'
 
-publish({
+await publish({
   branchConfigs,
   packages,
   rootDir,
   branch: process.env.BRANCH,
   tag: process.env.TAG,
   ghToken: process.env.GH_TOKEN,
-}).catch((err) => {
-  console.info(err)
-  process.exit(1)
 })
