669cce93337b6d13fc39e2d3288763e4ca3d57e8
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-21 08:29:23 +1100

    refactor(eslint-plugin-query): Use `@tanstack/config` build (#6739)
    * refactor(eslint-plugin-query): Use @tanstack/config build
    
    * Remove unused file (reported by knip)

--- packages/eslint-plugin-query/tsup.config.js
+++
@@ -1,6 +0,0 @@
-// @ts-check
-
-import { defineConfig } from 'tsup'
-import { legacyConfig } from '../../scripts/getTsupConfig.js'
-
-export default defineConfig([legacyConfig({ entry: ['src/**/*.ts'] })])
