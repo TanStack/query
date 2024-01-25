365e97b64a7434fe411e7f39e4a2b78eb8f7caf9
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-25 20:00:36 +1100

    docs: Re-organise for v2 docs layout (v3)

--- docs/src/components/MDXComponents.js
+++
@@ -1,8 +0,0 @@
-import * as React from 'react';
-import dynamic from 'next/dynamic';
-export default {
-  // default tags
-  pre: p => <pre {...p} />,
-  code: dynamic(() => import('./Highlight2')) // Counter: dynamic(() => import('./counter')),
-
-};
--- docs/src/lib/docs/utils.js
+++
@@ -1,4 +0,0 @@
-export function removeFromLast(path, key) {
-  const i = path.lastIndexOf(key)
-  return i === -1 ? path : path.substring(0, i)
-}
