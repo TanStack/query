549a2f242920d6efb17570222b45cbe56fc63a2f
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-02-17 15:18:49 +1100

    Remove unnecessary test config

--- packages/query-core/vite.config.modern.ts
+++ packages/query-core/vite.config.modern.ts
@@ -1,18 +1,9 @@
 import { defineConfig, mergeConfig } from 'vite'
 import { tanstackBuildConfig } from '@tanstack/config/build'
-import packageJson from './package.json'
 
 const config = defineConfig({
   build: {
     target: ['chrome91', 'firefox90', 'edge91', 'safari15', 'ios15', 'opera77'],
-  },
-  test: {
-    name: packageJson.name,
-    dir: './src',
-    watch: false,
-    environment: 'jsdom',
-    coverage: { enabled: true, provider: 'istanbul', include: ['src/**/*'] },
-    typecheck: { enabled: true },
   },
 })
 
