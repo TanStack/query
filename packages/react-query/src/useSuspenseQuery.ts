1edb493f17f3ad1a8dd49d452c1e02d71bf63835
Jaaneek <jankiewiczmilosz@gmail.com>
2024-03-03 21:16:16 +0100

    refactor(react-query): remove unnecesary check for skipToken

--- packages/react-query/src/useSuspenseQuery.ts
+++ packages/react-query/src/useSuspenseQuery.ts
@@ -1,5 +1,5 @@
 'use client'
-import { QueryObserver, skipToken } from '@tanstack/query-core'
+import { QueryObserver } from '@tanstack/query-core'
 import { useBaseQuery } from './useBaseQuery'
 import { defaultThrowOnError } from './suspense'
 import type { UseSuspenseQueryOptions, UseSuspenseQueryResult } from './types'
@@ -14,13 +14,6 @@ export function useSuspenseQuery<
   options: UseSuspenseQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
   queryClient?: QueryClient,
 ): UseSuspenseQueryResult<TData, TError> {
-  // @ts-expect-error
-  if (options.queryFn === skipToken) {
-    throw new Error(
-      `You have provided a \`skipToken\` to \`queryFn\` which is not allowed in useSuspenseQuery.`,
-    )
-  }
-
   return useBaseQuery(
     {
       ...options,
