30e885ef55a0a9d336f3a72d4704803f31fa2609
Aryan Deora <adeora@iu.edu>
2024-04-15 15:53:36 -0400

    docs(solid-query): Update SolidStart Example to RC

--- examples/solid/solid-start-streaming/src/entry-client.tsx
+++ examples/solid/solid-start-streaming/src/entry-client.tsx
@@ -1,3 +1,4 @@
+// @refresh reload
 import { mount, StartClient } from '@solidjs/start/client'
 
-mount(() => <StartClient />, document.getElementById('app'))
+mount(() => <StartClient />, document.getElementById('app')!)
