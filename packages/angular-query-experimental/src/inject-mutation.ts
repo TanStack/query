ivf1219/Tanstack

fix(angular-query-experimental): run mutation callback in injection context
Previously, the injectMutation callback was run in injection context only if accessed in the same task as the component initialization (i.e. before the `effect` callback run). By running the effect run in injection context, it is ensured that the callback will always run in injection context, and any `inject` calls will not fail.

Note there is a separate issue here, and it's that the callback is always run twice - once synchronously when initialization the mutation, and secondly as the first callback of the `effect`. This is something that can potentially be improved.

No breaking changes, any code that worked before should still work.

packages/angular-query-experimental/src/inject-mutation.ts
@@ -61,7 +61,9 @@ export function injectMutation<
         }
 
         effect(() => {
-          observer.setOptions(options(queryClient))
+          observer.setOptions(
+            runInInjectionContext(currentInjector, () => options(queryClient)),
+          )
         })
 
         const result = signal(observer.getCurrentResult())

TanStack/query

packages/angular-query-experimental/src/inject-mutation.ts
@@ -70,7 +70,9 @@ export function injectMutation<
         }
 
         effect(() => {
-          observer.setOptions(optionsFn(queryClient))
+          observer.setOptions(
+            runInInjectionContext(currentInjector, () => optionsFn(queryClient)),
+          )
         })
 
         const result = signal(observer.getCurrentResult())