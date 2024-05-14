4c216d1cf639bbbd2ae68c52415ca603d19496b2
Aponia <brianmv@uci.edu>
2024-05-14 09:52:19 -0700

    fix(svelte-query): skip token not included in queryFn union

--- packages/svelte-query/src/createQueries.ts
+++ packages/svelte-query/src/createQueries.ts
@@ -110,7 +110,7 @@ type GetResults<T> =
                     unknown extends TError ? DefaultError : TError
                   >
                 : T extends {
-                      queryFn?: QueryFunction<infer TQueryFnData, any>
+                      queryFn?: QueryFunction<infer TQueryFnData, any> | SkipToken
                       throwOnError?: ThrowOnError<any, infer TError, any, any>
                     }
                   ? QueryObserverResult<
