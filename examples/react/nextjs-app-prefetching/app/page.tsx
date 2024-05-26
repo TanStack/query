b3873738570820a4e56703802001f80077f164d3
Dominik Dorfmeister <office@dorfmeister.cc>
2024-05-26 20:40:25 +0200

    docs: prefetching

--- examples/react/nextjs-app-prefetching/app/page.tsx
+++ examples/react/nextjs-app-prefetching/app/page.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import { HydrationBoundary, dehydrate } from '@tanstack/react-query'
-import { getQueryClient } from '@/app/get-query-client'
 import { pokemonOptions } from '@/app/pokemon'
+import { getQueryClient } from '@/app/get-query-client'
 import { PokemonInfo } from './pokemon-info'
 
-export default async function Home() {
+export default function Home() {
   const queryClient = getQueryClient()
 
   void queryClient.prefetchQuery(pokemonOptions)
