9951df1ec47175c71c3282e907807603f5f1b857
Tanner Linsley <tannerlinsley@gmail.com>
2024-06-30 00:07:11 -0600

    fix: experimental before/after query options

--- packages/react-query/src/useBaseQuery.ts
+++ packages/react-query/src/useBaseQuery.ts
@@ -49,7 +49,7 @@ export function useBaseQuery<
   const errorResetBoundary = useQueryErrorResetBoundary()
   const defaultedOptions = client.defaultQueryOptions(options)
 
-  ;(client.getDefaultOptions().queries as any)._experimental_beforeQuery?.(
+  ;(client.getDefaultOptions().queries as any)?._experimental_beforeQuery?.(
     defaultedOptions,
   )
 
@@ -125,7 +125,7 @@ export function useBaseQuery<
     throw result.error
   }
 
-  ;(client.getDefaultOptions().queries as any)._experimental_afterQuery?.(
+  ;(client.getDefaultOptions().queries as any)?._experimental_afterQuery?.(
     defaultedOptions,
     result,
   )
