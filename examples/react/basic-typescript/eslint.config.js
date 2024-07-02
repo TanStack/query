9760cd60d19f1142aa9b18f9c045724a9a724b83
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-03 09:55:53 +1000

    Add react and react-hooks back

--- examples/react/basic-typescript/eslint.config.js
+++ examples/react/basic-typescript/eslint.config.js
@@ -1,4 +1,19 @@
 import pluginQuery from '@tanstack/eslint-plugin-query'
+import pluginReact from '@eslint-react/eslint-plugin'
+import pluginReactHooks from 'eslint-plugin-react-hooks'
 import rootConfig from '../../../eslint.config.js'
 
-export default [...rootConfig, ...pluginQuery.configs['flat/recommended']]
+export default [
+  ...rootConfig,
+  ...pluginQuery.configs['flat/recommended'],
+  pluginReact.configs.recommended,
+  {
+    plugins: {
+      'react-hooks': pluginReactHooks,
+    },
+    rules: {
+      'react-hooks/exhaustive-deps': 'error',
+      'react-hooks/rules-of-hooks': 'error',
+    },
+  },
+]
