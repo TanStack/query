2443a095b7023e2489a50d4b9b6db8822293f945
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-03 09:33:25 +1000

    Use in basic-typescript

---
+++ examples/react/basic-typescript/eslint.config.js
@@ -0,0 +1,12 @@
+import pluginQuery from '@tanstack/eslint-plugin-query'
+import rootConfig from '../../../eslint.config.js'
+
+export default [
+  ...rootConfig,
+  ...pluginQuery.configs['flat/recommended'],
+  {
+    plugins: {
+      '@tanstack/query': pluginQuery,
+    },
+  },
+]
