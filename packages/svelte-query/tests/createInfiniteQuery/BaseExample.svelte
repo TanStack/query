8200ebf5ec0ebd8a427e039ddbd17c61de0b0c7e
zhihengzhanga@gmail.com <zhihengzhanga@gmail.com>
2024-07-13 22:59:50 -0700

    fix

--- packages/svelte-query/tests/createInfiniteQuery/BaseExample.svelte
+++ packages/svelte-query/tests/createInfiniteQuery/BaseExample.svelte
@@ -1,13 +1,16 @@
 <script lang="ts">
   import { QueryClient } from '@tanstack/query-core'
   import { createInfiniteQuery } from '../../src/createInfiniteQuery'
+  import { sleep } from '../utils'
 
   const queryClient = new QueryClient()
 
   const query = createInfiniteQuery(
     {
       queryKey: ['test'],
-      queryFn: ({ pageParam }) => Number(pageParam),
+      queryFn: ({ pageParam }) => {
+        return Number(pageParam)
+      },
       getNextPageParam: (lastPage) => lastPage + 1,
       initialPageParam: 0,
     },
--- packages/svelte-query/tests/createInfiniteQuery/createInfiniteQuery.test.ts
+++ packages/svelte-query/tests/createInfiniteQuery/createInfiniteQuery.test.ts
@@ -1,19 +1,22 @@
 import { describe, test } from 'vitest'
-import { render } from '@testing-library/svelte'
+import { render, waitFor } from '@testing-library/svelte'
 
 import { sleep } from '../utils'
 import BaseExample from './BaseExample.svelte'
 
-describe.only('createQuery', () => {
+describe('createQuery', () => {
   test('Render and wait for success', async () => {
     const rendered = render(BaseExample)
-
-    // await rendered.findByText('Data: undefined')
-    // await rendered.findByText('Status: pending')
+    waitFor(async () => {
+      await rendered.findByText('Data: undefined')
+      await rendered.findByText('Status: pending')
+    })
 
     await sleep(20)
 
-    await rendered.findByText('Data: {"pages":[0],"pageParams":[0]}')
-    await rendered.findByText('Status: success')
+    waitFor(async () => {
+      await rendered.findByText('Data: {"pages":[0],"pageParams":[0]}')
+      await rendered.findByText('Status: success')
+    })
   })
 })
