f1fb2344e2cd818f20391373b334c499f450fa7c
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-07-20 20:34:30 +1000

    Fix createQuery types

--- packages/svelte-query/tests/createQueries/createQueries.test-d.ts
+++ packages/svelte-query/tests/createQueries/createQueries.test-d.ts
@@ -1,6 +1,9 @@
 import { describe, expectTypeOf, test } from 'vitest'
 import { get } from 'svelte/store'
+import { skipToken } from '@tanstack/query-core'
 import { createQueries, queryOptions } from '../../src/index'
+import type { OmitKeyof, QueryObserverResult } from '@tanstack/query-core'
+import type { CreateQueryOptions } from '../../src/index'
 
 describe('createQueries', () => {
   test('TData should be defined when passed through queryOptions', () => {
@@ -20,5 +23,46 @@ describe('createQueries', () => {
     const data = get(queryResults)[0].data
 
     expectTypeOf(data).toEqualTypeOf<{ wow: boolean }>()
+  })
+
+  test('Allow custom hooks using UseQueryOptions', () => {
+    type Data = string
+
+    const useCustomQueries = (
+      options?: OmitKeyof<CreateQueryOptions<Data>, 'queryKey' | 'queryFn'>,
+    ) => {
+      return createQueries({
+        queries: [
+          {
+            ...options,
+            queryKey: ['todos-key'],
+            queryFn: () => Promise.resolve('data'),
+          },
+        ],
+      })
+    }
+
+    const query = useCustomQueries()
+    const data = get(query)[0].data
+
+    expectTypeOf(data).toEqualTypeOf<Data | undefined>()
+  })
+
+  test('TData should have correct type when conditional skipToken is passed', () => {
+    const queryResults = createQueries({
+      queries: [
+        {
+          queryKey: ['withSkipToken'],
+          queryFn: Math.random() > 0.5 ? skipToken : () => Promise.resolve(5),
+        },
+      ],
+    })
+
+    const firstResult = get(queryResults)[0]
+
+    expectTypeOf(firstResult).toEqualTypeOf<
+      QueryObserverResult<number, Error>
+    >()
+    expectTypeOf(firstResult.data).toEqualTypeOf<number | undefined>()
   })
 })
