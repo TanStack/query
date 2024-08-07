23750e78763e34745eaf670f4ce8374919837f14
Alvar Lagerlöf <14835120+alvarlagerlof@users.noreply.github.com>
2024-08-07 14:55:16 +0200

    [WIP] repro hydrate() throws after test completion
    WIP

--- packages/react-query/src/__tests__/useQuery.test.tsx
+++ packages/react-query/src/__tests__/useQuery.test.tsx
@@ -6581,6 +6581,7 @@ describe('useQuery', () => {
       defaultOptions: { hydrate: { queries: { retry: 1, retryDelay: 10 } } },
     })
     hydrate(clientQueryClient, dehydrated)
+    await sleep(400)
 
     const rendered = renderWithClient(clientQueryClient, <Page />)
     await waitFor(() => rendered.getByText('failure: redacted'))
