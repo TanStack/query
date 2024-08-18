b74f5cebd40c2440a982c31c46c515036a2f3b97
Manuel Schiller <manuel.schiller@caligano.de>
2024-08-18 18:33:36 +0200

    add rule to recommended rules

--- packages/eslint-plugin-query/src/index.ts
+++ packages/eslint-plugin-query/src/index.ts
@@ -28,6 +28,7 @@ Object.assign(plugin.configs, {
       '@tanstack/query/exhaustive-deps': 'error',
       '@tanstack/query/no-rest-destructuring': 'warn',
       '@tanstack/query/stable-query-client': 'error',
+      '@tanstack/query/no-mutation-in-deps': 'error',
     },
   },
   'flat/recommended': [
@@ -39,6 +40,7 @@ Object.assign(plugin.configs, {
         '@tanstack/query/exhaustive-deps': 'error',
         '@tanstack/query/no-rest-destructuring': 'warn',
         '@tanstack/query/stable-query-client': 'error',
+        '@tanstack/query/no-mutation-in-deps': 'error',
       },
     },
   ],
