0df1dc3621efa751584c009201ecab5909386018
Alex / KATT <alexander@n1s.se>
2024-09-19 11:14:57 +0200

    Update packages/react-query/src/useBaseQuery.ts

--- packages/react-query/src/useBaseQuery.ts
+++ packages/react-query/src/useBaseQuery.ts
@@ -143,7 +143,7 @@ export function useBaseQuery<
     !observer.hasListeners()
   ) {
     const promise = isNewCacheEntry
-      ? // Fetch immediately on mount in order to ensure `.promise` is resolved even if the component is unmounted
+      ? // Fetch immediately on render in order to ensure `.promise` is resolved even if the component is unmounted
         fetchOptimistic(defaultedOptions, observer, errorResetBoundary)
       : // subscribe to the "cache promise" so that we can finalize the currentThenable once data comes in
         client.getQueryCache().get(defaultedOptions.queryHash)?.promise
