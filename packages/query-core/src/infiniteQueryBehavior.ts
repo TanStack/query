b4e17e192096711823786c3a381f61846caebff8
sinasun <sinakhodaveisi@gmail.com>
2024-10-05 16:47:25 -0400

    fixed typing and concised the if statement

--- packages/query-core/src/infiniteQueryBehavior.ts
+++ packages/query-core/src/infiniteQueryBehavior.ts
@@ -132,14 +132,13 @@ function getNextPageParam(
   options: InfiniteQueryPageParamsOptions<any>,
   { pages, pageParams }: InfiniteData<unknown>,
 ): unknown | undefined {
-  if (!pages || pages.length === 0 || !pageParams || pageParams.length === 0) {
-    return undefined;
+  if (!pages?.length || !pageParams?.length) {
+    return undefined
   }
-
   const lastIndex = pages.length - 1
 
   if (lastIndex >= pageParams.length) {
-    return undefined;
+    return undefined
   }
 
   return pages.length > 0
