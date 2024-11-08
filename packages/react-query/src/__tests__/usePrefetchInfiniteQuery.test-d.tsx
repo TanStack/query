74c65cc2db0fa378c108448445f38464e1acd27a
Jonghyeon Ko <manudeli.ko@gmail.com>
2024-11-09 02:11:01 +0900

    test(react-query): refactor test file structure 1 file by 1 api (#8262)
    Co-authored-by: Dominik Dorfmeister <office@dorfmeister.cc>

--- packages/react-query/src/__tests__/usePrefetchInfiniteQuery.test-d.tsx
+++ packages/react-query/src/__tests__/usePrefetchInfiniteQuery.test-d.tsx
@@ -1,39 +1,5 @@
 import { describe, expectTypeOf, it } from 'vitest'
-import { usePrefetchInfiniteQuery, usePrefetchQuery } from '..'
-
-describe('usePrefetchQuery', () => {
-  it('should return nothing', () => {
-    const result = usePrefetchQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-    })
-
-    expectTypeOf(result).toEqualTypeOf<void>()
-  })
-
-  it('should not allow refetchInterval, enabled or throwOnError options', () => {
-    usePrefetchQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-      // @ts-expect-error TS2345
-      refetchInterval: 1000,
-    })
-
-    usePrefetchQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-      // @ts-expect-error TS2345
-      enabled: true,
-    })
-
-    usePrefetchQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-      // @ts-expect-error TS2345
-      throwOnError: true,
-    })
-  })
-})
+import { usePrefetchInfiniteQuery } from '..'
 
 describe('useInfinitePrefetchQuery', () => {
   it('should return nothing', () => {
