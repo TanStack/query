918f7edb198002c9386f33f6c2d086990db715e5
Nick Lucas <me@nicklucas.co.uk>
2024-11-23 20:27:47 +0000

    Add a type test

---
+++ packages/query-core/src/__tests__/utils.test-d.tsx
@@ -0,0 +1,45 @@
+import { describe, expectTypeOf, it } from 'vitest'
+import { QueryClient } from '../queryClient'
+import type { QueryFilters } from '../utils'
+import { DataTag } from '../types'
+
+describe('QueryFilters', () => {
+  it('should be typed if generics are passed', () => {
+    type TData = { a: number; b: string }
+    type TError = { message: string }
+    
+    const a: QueryFilters<TData, TError> = {
+      predicate(query) {
+        expectTypeOf(query.setData({a: 1, b: '1'})).toEqualTypeOf<TData>()
+        return true
+      },
+      queryKey: ['key'] as DataTag<undefined, TData>
+    }
+
+    const queryClient = new QueryClient()
+
+    const data = queryClient.getQueryData(a.queryKey!)
+    expectTypeOf(data).toEqualTypeOf<TData | undefined>()
+
+    const error = queryClient.getQueryState(a.queryKey!)?.error
+    expectTypeOf(error).toEqualTypeOf<Error | null | undefined>() // maybe one day this can return TError
+  })
+
+  it('should be loose typed if generics are defaults', () => {
+    const a: QueryFilters = {
+      predicate(query) {
+        expectTypeOf(query.setData({a: 1, b: '1'})).toEqualTypeOf<unknown>()
+        return true
+      },
+      queryKey: ['key']
+    }
+
+    const queryClient = new QueryClient()
+
+    const data = queryClient.getQueryData(a.queryKey!)
+    expectTypeOf(data).toEqualTypeOf<unknown>()
+
+    const error = queryClient.getQueryState(a.queryKey!)?.error
+    expectTypeOf(error).toEqualTypeOf<Error | null | undefined>()
+  })
+})
