547ae9e71211e6b8788fe0c166242c607eabd913
omergronich <gronichomer@gmail.com>
2024-11-23 22:11:51 +0200

    docs(angular-query): add basic persister example

---
+++ examples/angular/basic-persister/src/app/app.config.ts
@@ -0,0 +1,37 @@
+import { provideHttpClient, withFetch } from '@angular/common/http'
+import {
+  QueryClient,
+  provideTanStackQuery,
+  withDevtools,
+} from '@tanstack/angular-query-experimental'
+import { withPersistQueryClient } from '@tanstack/angular-query-persist-client-experimental'
+import { createSyncStoragePersister } from '@tanstack/query-sync-storage-persister'
+import type { ApplicationConfig } from '@angular/core'
+
+const localStoragePersister = createSyncStoragePersister({
+  storage: window.localStorage,
+})
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideHttpClient(withFetch()),
+    provideTanStackQuery(
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 1000 * 60,
+            gcTime: 1000 * 60 * 60 * 24, // 24 hours
+          },
+        },
+      }),
+      withDevtools(),
+      withPersistQueryClient([
+        {
+          persistOptions: {
+            persister: localStoragePersister,
+          },
+        },
+      ]),
+    ),
+  ],
+}
