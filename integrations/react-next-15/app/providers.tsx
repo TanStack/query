85ee5bcf07cf00be96de39eb1f3db0c9acb48e23
juliusmarminge <julius0216@outlook.com>
2024-12-03 16:38:24 +0100

    revert

--- integrations/react-next-15/app/providers.tsx
+++ integrations/react-next-15/app/providers.tsx
@@ -1,19 +1,11 @@
 'use client'
-import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { QueryClientProvider } from '@tanstack/react-query'
 // import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
 import * as React from 'react'
 import { makeQueryClient } from '@/app/make-query-client'
 
-let queryClientSingleton: QueryClient | undefined
-const getQueryClientSingleton = () => {
-  if (typeof window === 'undefined') {
-    return makeQueryClient()
-  }
-  return (queryClientSingleton ??= makeQueryClient())
-}
 export default function Providers({ children }: { children: React.ReactNode }) {
-  // const [queryClient] = React.useState(() => makeQueryClient())
-  const queryClient = getQueryClientSingleton()
+  const [queryClient] = React.useState(() => makeQueryClient())
 
   return (
     <QueryClientProvider client={queryClient}>
