7d3fcccd28c923482856ed42ae8dd3709e770ba0
Alexander Johansson <alexander@n1s.se>
2024-12-13 12:09:44 +0100

    wip

--- packages/react-query/vite.config.ts
+++ packages/react-query/vite.config.ts
@@ -7,7 +7,6 @@ export default defineConfig({
   test: {
     name: packageJson.name,
     dir: './src',
-    watch: false,
     environment: 'jsdom',
     setupFiles: ['test-setup.ts'],
     coverage: { enabled: true, provider: 'istanbul', include: ['src/**/*'] },
