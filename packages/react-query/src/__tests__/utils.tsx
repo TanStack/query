e2dca98c3c9b85d4c33d4145476074fa36b60f2e
Alexander Johansson <alexander@n1s.se>
2024-12-13 16:39:04 +0100

    cleanup

--- packages/react-query/src/__tests__/utils.tsx
+++ packages/react-query/src/__tests__/utils.tsx
@@ -95,18 +95,6 @@ export function setIsServer(isServer: boolean) {
 
 export const doNotExecute = (_func: () => void) => true
 
-export function createDeferred<TValue>() {
-  let resolve: (value: TValue) => void
-  let reject: (error: unknown) => void
-  const promise = new Promise<TValue>((res, rej) => {
-    resolve = res
-    reject = rej
-  })
-
-  return { promise, resolve: resolve!, reject: reject! }
-}
-export type Deferred<TValue> = ReturnType<typeof createDeferred<TValue>>
-
 export function pick<T extends object, TKey extends keyof T>(
   obj: T,
   keys: Array<TKey>,
