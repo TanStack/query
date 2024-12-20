847e7da1ec436d854b05b2c01ba2cbdcc88c1da0
Alexander Johansson <alexander@n1s.se>
2024-12-20 01:09:12 +0100

    why doesn’t this failllll

--- packages/react-query/src/__tests__/transition.test.tsx
+++ packages/react-query/src/__tests__/transition.test.tsx
@@ -1,5 +1,5 @@
 /* eslint-disable @typescript-eslint/require-await */
-import { act, render, screen } from '@testing-library/react'
+import { act, render, screen, waitFor } from '@testing-library/react'
 import * as React from 'react'
 import { afterAll, beforeAll, expect, it, vi } from 'vitest'
 import { QueryClientProvider, useQuery } from '..'
@@ -195,4 +195,94 @@ it('should handle parallel queries with shared parent key in transition', async 
   screen.getByText('A data: A-1')
   screen.getByText('B data: B-1')
   expect(screen.queryByText('pending...')).toBeNull()
+})
+
+it('should work to interrupt a transition', async () => {
+  const resolversByCount: Record<number, () => void> = {}
+
+  const key = queryKey()
+
+  function Component(props: { count: number }) {
+    const { count } = props
+
+    const query = useQuery({
+      queryKey: [key, count],
+      queryFn: async () => {
+        await new Promise<void>((resolve) => {
+          resolversByCount[count] = resolve
+        })
+
+        return 'test' + count
+      },
+      staleTime: 1000,
+    })
+    const data = React.use(query.promise)
+    return <div>data: {data}</div>
+  }
+
+  function Page() {
+    const [isPending, startTransition] = React.useTransition()
+    const [count, setCount] = React.useState(0)
+
+    return (
+      <div>
+        <button onClick={() => startTransition(() => setCount((c) => c + 1))}>
+          increment
+        </button>
+        <React.Suspense fallback="loading...">
+          <Component count={count} />
+        </React.Suspense>
+        {isPending && 'pending...'}
+      </div>
+    )
+  }
+  // Initial render should show fallback
+  await act(async () => {
+    render(
+      <QueryClientProvider client={queryClient}>
+        <Page />
+      </QueryClientProvider>,
+    )
+  })
+
+  screen.getByText('loading...')
+  expect(screen.queryByText('button')).toBeNull()
+  expect(screen.queryByText('pending...')).toBeNull()
+  expect(screen.queryByText('data: test0')).toBeNull()
+
+  // Resolve the query, should show the data
+  await act(async () => {
+    resolversByCount[0]!()
+  })
+
+  screen.getByText('data: test0')
+
+  // increment
+  await act(async () => {
+    screen.getByRole('button', { name: 'increment' }).click()
+  })
+
+  // should show pending state, and existing content
+  screen.getByText('pending...')
+  screen.getByText('data: test0')
+
+  // Before the query is resolved, increment again
+  await act(async () => {
+    screen.getByRole('button', { name: 'increment' }).click()
+  })
+
+  await act(async () => {
+    // resolve the second query
+    resolversByCount[1]!()
+  })
+
+  screen.getByText('pending...')
+  screen.getByText('data: test0')
+
+  await act(async () => {
+    // resolve the third query
+    resolversByCount[2]!()
+  })
+
+  screen.getByText('data: test2')
 })
