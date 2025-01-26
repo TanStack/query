9ebf869050afc4cbaf49d1bcec4d156e712ac023
juliusmarminge <julius0216@outlook.com>
2025-01-26 12:04:33 +0100

    kick off ci again

--- integrations/react-next-15/app/providers.tsx
+++ integrations/react-next-15/app/providers.tsx
@@ -33,7 +33,7 @@ export default function Providers({ children }: { children: React.ReactNode }) {
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      {<ReactQueryDevtools />}
+      <ReactQueryDevtools />
     </QueryClientProvider>
   )
 }
