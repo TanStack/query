d07a3db968f9d836a1b658c043bc723ac424b304
Matan Shushan <matan22993@gmail.com>
2025-02-18 21:06:08 +0200

    docs(angular-query): Add Optimistic Updates Example for Angular (#8439)
    * feat: add optimistic updates example for Angular
    
    - Implemented OptimisticUpdatesComponent with Angular Query for managing optimistic UI updates.
    - Added TasksService to handle query and mutation logic with , , and  handlers.
    - Included mock API interceptor for simulating backend responses.
    
    * Remove comment
    
    * add lock file
    
    * ci: apply automated fixes
    
    * fix build, add example to docs
    
    * ci: apply automated fixes
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>
    Co-authored-by: Arnoud <6420061+arnoud-dv@users.noreply.github.com>

---
+++ examples/angular/optimistic-updates/src/app/interceptor/mock-api.interceptor.ts
@@ -0,0 +1,55 @@
+/**
+ * MockApiInterceptor is used to simulate API responses for `/api/tasks` endpoints.
+ * It handles the following operations:
+ * - GET: Fetches all tasks from sessionStorage.
+ * - POST: Adds a new task to sessionStorage.
+ * Simulated responses include a delay to mimic network latency.
+ */
+import { HttpResponse } from '@angular/common/http'
+import { delay, of } from 'rxjs'
+import type {
+  HttpEvent,
+  HttpHandlerFn,
+  HttpInterceptorFn,
+  HttpRequest,
+} from '@angular/common/http'
+import type { Observable } from 'rxjs'
+
+export const mockInterceptor: HttpInterceptorFn = (
+  req: HttpRequest<unknown>,
+  next: HttpHandlerFn,
+): Observable<HttpEvent<any>> => {
+  const respondWith = (status: number, body: any) =>
+    of(new HttpResponse({ status, body })).pipe(delay(1000))
+  if (req.url === '/api/tasks') {
+    switch (req.method) {
+      case 'GET':
+        return respondWith(
+          200,
+          JSON.parse(
+            sessionStorage.getItem('optimistic-updates-tasks') || '[]',
+          ),
+        )
+      case 'POST':
+        const tasks = JSON.parse(
+          sessionStorage.getItem('optimistic-updates-tasks') || '[]',
+        )
+        tasks.push(req.body)
+        sessionStorage.setItem(
+          'optimistic-updates-tasks',
+          JSON.stringify(tasks),
+        )
+        return respondWith(201, {
+          status: 'success',
+          task: req.body,
+        })
+    }
+  }
+  if (req.url === '/api/tasks-wrong-url') {
+    return respondWith(500, {
+      status: 'error',
+    })
+  }
+
+  return next(req)
+}
---
+++ examples/angular/optimistic-updates/src/app/services/tasks.service.ts
@@ -0,0 +1,84 @@
+import { HttpClient } from '@angular/common/http'
+import { Injectable, inject } from '@angular/core'
+import {
+  QueryClient,
+  mutationOptions,
+  queryOptions,
+} from '@tanstack/angular-query-experimental'
+
+import { lastValueFrom } from 'rxjs'
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TasksService {
+  #queryClient = inject(QueryClient) // Manages query state and caching
+  #http = inject(HttpClient) // Handles HTTP requests
+
+  /**
+   * Fetches all tasks from the API.
+   * Returns an observable containing an array of task strings.
+   */
+  allTasks = () =>
+    queryOptions({
+      queryKey: ['tasks'],
+      queryFn: () => {
+        return lastValueFrom(this.#http.get<Array<string>>('/api/tasks'))
+      },
+    })
+
+  /**
+   * Creates a mutation for adding a task.
+   * On success, invalidates and refetches the "tasks" query cache to update the task list.
+   */
+  addTask() {
+    return mutationOptions({
+      mutationFn: ({
+        task,
+        failMutation = false,
+      }: {
+        task: string
+        failMutation: boolean
+      }) =>
+        lastValueFrom(
+          this.#http.post(
+            `/api/tasks${failMutation ? '-wrong-url' : ''}`,
+            task,
+          ),
+        ),
+      mutationKey: ['tasks'],
+      onSuccess: () => {
+        this.#queryClient.invalidateQueries({ queryKey: ['tasks'] })
+      },
+      onMutate: async ({ task }) => {
+        // Cancel any outgoing refetches
+        // (so they don't overwrite our optimistic update)
+        await this.#queryClient.cancelQueries({ queryKey: ['tasks'] })
+
+        // Snapshot the previous value
+        const previousTodos = this.#queryClient.getQueryData<Array<string>>([
+          'tasks',
+        ])
+
+        // Optimistically update to the new value
+        if (previousTodos) {
+          this.#queryClient.setQueryData<Array<string>>(
+            ['tasks'],
+            [...previousTodos, task],
+          )
+        }
+
+        return previousTodos
+      },
+      onError: (err, variables, context) => {
+        if (context) {
+          this.#queryClient.setQueryData<Array<string>>(['tasks'], context)
+        }
+      },
+      // Always refetch after error or success:
+      onSettled: () => {
+        this.#queryClient.invalidateQueries({ queryKey: ['tasks'] })
+      },
+    })
+  }
+}
