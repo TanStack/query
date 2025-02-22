53c901d6171733816b620d823835b251f415947a
Corbin Crutchley <git@crutchcorn.dev>
2025-02-22 14:10:31 -0800

    fix(angular-query-experimental): typings for injectQueries should now behave properly

--- packages/angular-query-experimental/src/inject-queries.ts
+++ packages/angular-query-experimental/src/inject-queries.ts
@@ -12,6 +12,11 @@ import {
   signal,
 } from '@angular/core'
 import { assertInjector } from './util/assert-injector/assert-injector'
+import {
+  CreateQueryOptions,
+  CreateQueryResult,
+  DefinedCreateQueryResult,
+} from './types'
 import type { Injector, Signal } from '@angular/core'
 import type {
   DefaultError,
@@ -26,14 +31,14 @@ import type {
 } from '@tanstack/query-core'
 
 // This defines the `CreateQueryOptions` that are accepted in `QueriesOptions` & `GetOptions`.
-// `placeholderData` function does not have a parameter
+// `placeholderData` function always gets undefined passed
 type QueryObserverOptionsForCreateQueries<
   TQueryFnData = unknown,
   TError = DefaultError,
   TData = TQueryFnData,
   TQueryKey extends QueryKey = QueryKey,
 > = OmitKeyof<
-  QueryObserverOptions<TQueryFnData, TError, TData, TQueryFnData, TQueryKey>,
+  CreateQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
   'placeholderData'
 > & {
   placeholderData?: TQueryFnData | QueriesPlaceholderDataFunction<TQueryFnData>
@@ -43,9 +48,9 @@ type QueryObserverOptionsForCreateQueries<
 type MAXIMUM_DEPTH = 20
 
 // Widen the type of the symbol to enable type inference even if skipToken is not immutable.
-type SkipTokenForUseQueries = symbol
+type SkipTokenForCreateQueries = symbol
 
-type GetOptions<T> =
+type GetCreateQueryOptionsForCreateQueries<T> =
   // Part 1: responsible for applying explicit type parameter to function arguments, if object { queryFnData: TQueryFnData, error: TError, data: TData }
   T extends {
     queryFnData: infer TQueryFnData
@@ -68,8 +73,8 @@ type GetOptions<T> =
                 T extends {
                     queryFn?:
                       | QueryFunction<infer TQueryFnData, infer TQueryKey>
-                      | SkipTokenForUseQueries
-                    select: (data: any) => infer TData
+                      | SkipTokenForCreateQueries
+                    select?: (data: any) => infer TData
                     throwOnError?: ThrowOnError<any, infer TError, any, any>
                   }
                 ? QueryObserverOptionsForCreateQueries<
@@ -81,54 +86,71 @@ type GetOptions<T> =
                 : // Fallback
                   QueryObserverOptionsForCreateQueries
 
-type GetResults<T> =
+// A defined initialData setting should return a DefinedCreateQueryResult rather than CreateQueryResult
+type GetDefinedOrUndefinedQueryResult<T, TData, TError = unknown> = T extends {
+  initialData?: infer TInitialData
+}
+  ? unknown extends TInitialData
+    ? CreateQueryResult<TData, TError>
+    : TInitialData extends TData
+      ? DefinedCreateQueryResult<TData, TError>
+      : TInitialData extends () => infer TInitialDataResult
+        ? unknown extends TInitialDataResult
+          ? CreateQueryResult<TData, TError>
+          : TInitialDataResult extends TData
+            ? DefinedCreateQueryResult<TData, TError>
+            : CreateQueryResult<TData, TError>
+        : CreateQueryResult<TData, TError>
+  : CreateQueryResult<TData, TError>
+
+type GetCreateQueryResult<T> =
   // Part 1: responsible for mapping explicit type parameter to function result, if object
   T extends { queryFnData: any; error?: infer TError; data: infer TData }
-    ? QueryObserverResult<TData, TError>
+    ? GetDefinedOrUndefinedQueryResult<T, TData, TError>
     : T extends { queryFnData: infer TQueryFnData; error?: infer TError }
-      ? QueryObserverResult<TQueryFnData, TError>
+      ? GetDefinedOrUndefinedQueryResult<T, TQueryFnData, TError>
       : T extends { data: infer TData; error?: infer TError }
-        ? QueryObserverResult<TData, TError>
+        ? GetDefinedOrUndefinedQueryResult<T, TData, TError>
         : // Part 2: responsible for mapping explicit type parameter to function result, if tuple
           T extends [any, infer TError, infer TData]
-          ? QueryObserverResult<TData, TError>
+          ? GetDefinedOrUndefinedQueryResult<T, TData, TError>
           : T extends [infer TQueryFnData, infer TError]
-            ? QueryObserverResult<TQueryFnData, TError>
+            ? GetDefinedOrUndefinedQueryResult<T, TQueryFnData, TError>
             : T extends [infer TQueryFnData]
-              ? QueryObserverResult<TQueryFnData>
+              ? GetDefinedOrUndefinedQueryResult<T, TQueryFnData>
               : // Part 3: responsible for mapping inferred type to results, if no explicit parameter was provided
                 T extends {
                     queryFn?:
                       | QueryFunction<infer TQueryFnData, any>
-                      | SkipTokenForUseQueries
-                    select: (data: any) => infer TData
+                      | SkipTokenForCreateQueries
+                    select?: (data: any) => infer TData
                     throwOnError?: ThrowOnError<any, infer TError, any, any>
                   }
-                ? QueryObserverResult<
+                ? GetDefinedOrUndefinedQueryResult<
+                    T,
                     unknown extends TData ? TQueryFnData : TData,
                     unknown extends TError ? DefaultError : TError
                   >
                 : // Fallback
-                  QueryObserverResult
+                  CreateQueryResult
 
 /**
  * QueriesOptions reducer recursively unwraps function arguments to infer/enforce type param
- * @public
  */
 export type QueriesOptions<
   T extends Array<any>,
-  TResult extends Array<any> = [],
+  TResults extends Array<any> = [],
   TDepth extends ReadonlyArray<number> = [],
 > = TDepth['length'] extends MAXIMUM_DEPTH
   ? Array<QueryObserverOptionsForCreateQueries>
   : T extends []
     ? []
     : T extends [infer Head]
-      ? [...TResult, GetOptions<Head>]
-      : T extends [infer Head, ...infer Tail]
+      ? [...TResults, GetCreateQueryOptionsForCreateQueries<Head>]
+      : T extends [infer Head, ...infer Tails]
         ? QueriesOptions<
-            [...Tail],
-            [...TResult, GetOptions<Head>],
+            [...Tails],
+            [...TResults, GetCreateQueryOptionsForCreateQueries<Head>],
             [...TDepth, 1]
           >
         : ReadonlyArray<unknown> extends T
@@ -160,101 +182,90 @@ export type QueriesOptions<
  */
 export type QueriesResults<
   T extends Array<any>,
-  TResult extends Array<any> = [],
+  TResults extends Array<any> = [],
   TDepth extends ReadonlyArray<number> = [],
 > = TDepth['length'] extends MAXIMUM_DEPTH
-  ? Array<QueryObserverResult>
+  ? Array<CreateQueryResult>
   : T extends []
     ? []
     : T extends [infer Head]
-      ? [...TResult, GetResults<Head>]
-      : T extends [infer Head, ...infer Tail]
+      ? [...TResults, GetCreateQueryResult<Head>]
+      : T extends [infer Head, ...infer Tails]
         ? QueriesResults<
-            [...Tail],
-            [...TResult, GetResults<Head>],
+            [...Tails],
+            [...TResults, GetCreateQueryResult<Head>],
             [...TDepth, 1]
           >
-        : T extends Array<
-              QueryObserverOptionsForCreateQueries<
-                infer TQueryFnData,
-                infer TError,
-                infer TData,
-                any
-              >
-            >
-          ? // Dynamic-size (homogenous) CreateQueryOptions array: map directly to array of results
-            Array<
-              QueryObserverResult<
-                unknown extends TData ? TQueryFnData : TData,
-                unknown extends TError ? DefaultError : TError
-              >
-            >
-          : // Fallback
-            Array<QueryObserverResult>
+        : { [K in keyof T]: GetCreateQueryResult<T[K]> }
+
+export interface InjectQueriesOptions<
+  T extends Array<any>,
+  TCombinedResult = QueriesResults<T>,
+> {
+  queries:
+    | readonly [...QueriesOptions<T>]
+    | readonly [
+        ...{ [K in keyof T]: GetCreateQueryOptionsForCreateQueries<T[K]> },
+      ]
+  combine?: (result: QueriesResults<T>) => TCombinedResult
+}
 
 /**
- * @param root0
- * @param root0.queries
- * @param root0.combine
- * @param injector
+ * @param optionsFn - A function that returns queries' options.
+ * @param injector - The Angular injector to use.
  * @public
  */
 export function injectQueries<
   T extends Array<any>,
   TCombinedResult = QueriesResults<T>,
 >(
-  {
-    queries,
-    ...options
-  }: {
-    queries: Signal<[...QueriesOptions<T>]>
-    combine?: (result: QueriesResults<T>) => TCombinedResult
-  },
+  optionsFn: () => InjectQueriesOptions<T, TCombinedResult>,
   injector?: Injector,
 ): Signal<TCombinedResult> {
-  return assertInjector(injectQueries, injector, () => {
-    const destroyRef = inject(DestroyRef)
-    const ngZone = inject(NgZone)
-    const queryClient = inject(QueryClient)
-
-    const defaultedQueries = computed(() => {
-      return queries().map((opts) => {
-        const defaultedOptions = queryClient.defaultQueryOptions(opts)
-        // Make sure the results are already in fetching state before subscribing or updating options
-        defaultedOptions._optimisticResults = 'optimistic'
-
-        return defaultedOptions as QueryObserverOptions
-      })
-    })
-
-    const observer = new QueriesObserver<TCombinedResult>(
-      queryClient,
-      defaultedQueries(),
-      options as QueriesObserverOptions<TCombinedResult>,
-    )
-
-    // Do not notify on updates because of changes in the options because
-    // these changes should already be reflected in the optimistic result.
-    effect(() => {
-      observer.setQueries(
-        defaultedQueries(),
-        options as QueriesObserverOptions<TCombinedResult>,
-        { listeners: false },
-      )
-    })
-
-    const [, getCombinedResult] = observer.getOptimisticResult(
-      defaultedQueries(),
-      (options as QueriesObserverOptions<TCombinedResult>).combine,
-    )
-
-    const result = signal(getCombinedResult() as any)
-
-    const unsubscribe = ngZone.runOutsideAngular(() =>
-      observer.subscribe(notifyManager.batchCalls(result.set)),
-    )
-    destroyRef.onDestroy(unsubscribe)
-
-    return result
-  })
+  return 0 as never
+  // return assertInjector(injectQueries, injector, () => {
+  //   const destroyRef = inject(DestroyRef)
+  //   const ngZone = inject(NgZone)
+  //   const queryClient = inject(QueryClient)
+  //
+  //   const defaultedQueries = computed(() => {
+  //     return queries().map((opts) => {
+  //       const defaultedOptions = queryClient.defaultQueryOptions(opts)
+  //       // Make sure the results are already in fetching state before subscribing or updating options
+  //       defaultedOptions._optimisticResults = 'optimistic'
+  //
+  //       return defaultedOptions as QueryObserverOptions
+  //     })
+  //   })
+  //
+  //   const observer = new QueriesObserver<TCombinedResult>(
+  //     queryClient,
+  //     defaultedQueries(),
+  //     options as QueriesObserverOptions<TCombinedResult>,
+  //   )
+  //
+  //   // Do not notify on updates because of changes in the options because
+  //   // these changes should already be reflected in the optimistic result.
+  //   effect(() => {
+  //     observer.setQueries(
+  //       defaultedQueries(),
+  //       options as QueriesObserverOptions<TCombinedResult>,
+  //       { listeners: false },
+  //     )
+  //   })
+  //
+  //   const [, getCombinedResult] = observer.getOptimisticResult(
+  //     defaultedQueries(),
+  //     (options as QueriesObserverOptions<TCombinedResult>).combine,
+  //   )
+  //
+  //   const result = signal(getCombinedResult() as any)
+  //
+  //   const unsubscribe = ngZone.runOutsideAngular(() =>
+  //     observer.subscribe(notifyManager.batchCalls(result.set)),
+  //   )
+  //   destroyRef.onDestroy(unsubscribe)
+  //
+  //   return result
+  // })
 }
