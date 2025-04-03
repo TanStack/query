fb7cf514c01a90ef3481d03c3a8d172f3901531e
Birk Skyum <birk.skyum@pm.me>
2025-04-03 19:53:45 +0200

    deprecate create types

--- packages/solid-query/src/index.ts
+++ packages/solid-query/src/index.ts
@@ -26,23 +26,63 @@ export type {
   SolidMutationOptions,
 } from './types'
 
-// Compatibility types
-export type {
-  UseQueryOptions as CreateQueryOptions,
-  UseBaseQueryResult as CreateBaseQueryResult,
-  UseQueryResult as CreateQueryResult,
-  DefinedUseBaseQueryResult as DefinedCreateBaseQueryResult,
-  DefinedUseQueryResult as DefinedCreateQueryResult,
-  UseInfiniteQueryOptions as CreateInfiniteQueryOptions,
-  UseInfiniteQueryResult as CreateInfiniteQueryResult,
-  DefinedUseInfiniteQueryResult as DefinedCreateInfiniteQueryResult,
-  UseMutationOptions as CreateMutationOptions,
-  UseMutateFunction as CreateMutateFunction,
-  UseMutateAsyncFunction as CreateMutateAsyncFunction,
-  UseBaseMutationResult as CreateBaseMutationResult,
-  UseMutationResult as CreateMutationResult,
-  UseBaseQueryOptions as CreateBaseQueryOptions,
-} from './types'
+// Compatibility types (deprecated)
+/**
+ * @deprecated Use UseQueryOptions instead
+ */
+export type { UseQueryOptions as CreateQueryOptions } from './types'
+/**
+ * @deprecated Use UseBaseQueryResult instead
+ */
+export type { UseBaseQueryResult as CreateBaseQueryResult } from './types'
+/**
+ * @deprecated Use UseQueryResult instead
+ */
+export type { UseQueryResult as CreateQueryResult } from './types'
+/**
+ * @deprecated Use DefinedUseBaseQueryResult instead
+ */
+export type { DefinedUseBaseQueryResult as DefinedCreateBaseQueryResult } from './types'
+/**
+ * @deprecated Use DefinedUseQueryResult instead
+ */
+export type { DefinedUseQueryResult as DefinedCreateQueryResult } from './types'
+/**
+ * @deprecated Use UseInfiniteQueryOptions instead
+ */
+export type { UseInfiniteQueryOptions as CreateInfiniteQueryOptions } from './types'
+/**
+ * @deprecated Use UseInfiniteQueryResult instead
+ */
+export type { UseInfiniteQueryResult as CreateInfiniteQueryResult } from './types'
+/**
+ * @deprecated Use DefinedUseInfiniteQueryResult instead
+ */
+export type { DefinedUseInfiniteQueryResult as DefinedCreateInfiniteQueryResult } from './types'
+/**
+ * @deprecated Use UseMutationOptions instead
+ */
+export type { UseMutationOptions as CreateMutationOptions } from './types'
+/**
+ * @deprecated Use UseMutateFunction instead
+ */
+export type { UseMutateFunction as CreateMutateFunction } from './types'
+/**
+ * @deprecated Use UseMutateAsyncFunction instead
+ */
+export type { UseMutateAsyncFunction as CreateMutateAsyncFunction } from './types'
+/**
+ * @deprecated Use UseBaseMutationResult instead
+ */
+export type { UseBaseMutationResult as CreateBaseMutationResult } from './types'
+/**
+ * @deprecated Use UseMutationResult instead
+ */
+export type { UseMutationResult as CreateMutationResult } from './types'
+/**
+ * @deprecated Use UseBaseQueryOptions instead
+ */
+export type { UseBaseQueryOptions as CreateBaseQueryOptions } from './types'
 
 export { QueryClient } from './QueryClient'
 export type {
@@ -52,6 +92,9 @@ export type {
   InfiniteQueryObserverOptions,
 } from './QueryClient'
 export { useQuery } from './useQuery'
+/**
+ * @deprecated Use useQuery instead
+ */
 export { useQuery as createQuery } from './useQuery'
 export { queryOptions } from './queryOptions'
 export type {
@@ -66,6 +109,9 @@ export {
 export type { QueryClientProviderProps } from './QueryClientProvider'
 export { useIsFetching } from './useIsFetching'
 export { useInfiniteQuery } from './useInfiniteQuery'
+/**
+ * @deprecated Use useInfiniteQuery instead
+ */
 export { useInfiniteQuery as createInfiniteQuery } from './useInfiniteQuery'
 export { infiniteQueryOptions } from './infiniteQueryOptions'
 export type {
@@ -73,9 +119,15 @@ export type {
   UndefinedInitialDataInfiniteOptions,
 } from './infiniteQueryOptions'
 export { useMutation } from './useMutation'
+/**
+ * @deprecated Use useMutation instead
+ */
 export { useMutation as createMutation } from './useMutation'
 export { useIsMutating } from './useIsMutating'
 export { useMutationState } from './useMutationState'
 export { useQueries } from './useQueries'
+/**
+ * @deprecated Use useQueries instead
+ */
 export { useQueries as createQueries } from './useQueries'
 export { useIsRestoring, IsRestoringProvider } from './isRestoring'
