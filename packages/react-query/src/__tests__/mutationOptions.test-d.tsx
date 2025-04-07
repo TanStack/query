ff15e5d33c24f1d0067db745989a27927c1283a6
ubinquitous <ubinquitous1@gmail.com>
2025-04-07 17:14:42 +0900

    test(react-query): add DataTag test case

--- packages/react-query/src/__tests__/mutationOptions.test-d.tsx
+++ packages/react-query/src/__tests__/mutationOptions.test-d.tsx
@@ -1,4 +1,5 @@
 import { describe, expectTypeOf, it } from 'vitest'
+import { dataTagSymbol } from '@tanstack/query-core'
 import { mutationOptions } from '../mutationOptions'
 
 describe('mutationOptions', () => {
@@ -19,5 +20,32 @@ describe('mutationOptions', () => {
         expectTypeOf(data).toEqualTypeOf<number>()
       },
     })
+  })
+
+  it('should tag the mutationKey with the result type of the MutationFn', () => {
+    const { mutationKey } = mutationOptions({
+      mutationKey: ['key'],
+      mutationFn: () => Promise.resolve(5),
+    })
+
+    expectTypeOf(mutationKey[dataTagSymbol]).toEqualTypeOf<number>()
+  })
+
+  it('should tag the mutationKey with unknown if there is no mutationFn', () => {
+    const { mutationKey } = mutationOptions({
+      mutationKey: ['key'],
+    })
+
+    expectTypeOf(mutationKey[dataTagSymbol]).toEqualTypeOf<unknown>()
+  })
+
+  it('should tag the mutationKey with the result type of the MutationFn if onSuccess is used', () => {
+    const { mutationKey } = mutationOptions({
+      mutationKey: ['key'],
+      mutationFn: () => Promise.resolve(5),
+      onSuccess: () => {},
+    })
+
+    expectTypeOf(mutationKey[dataTagSymbol]).toEqualTypeOf<number>()
   })
 })
