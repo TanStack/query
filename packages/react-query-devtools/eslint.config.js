9d028c1a3a13977d129c7954613dd288a1e7769e
TkDodo <office@dorfmeister.cc>
2025-04-26 10:35:26 +0200

    chore: update to eslint-plugin-react-hooks v6
    and remove the separate compiler linter plugin, as those rules are now integrated

--- packages/react-query-devtools/eslint.config.js
+++ packages/react-query-devtools/eslint.config.js
@@ -1,22 +1,21 @@
 // @ts-check
 
 import pluginReact from '@eslint-react/eslint-plugin'
-import pluginReactHooks from 'eslint-plugin-react-hooks'
+import * as reactHooks from 'eslint-plugin-react-hooks'
 import rootConfig from './root.eslint.config.js'
 
 export default [
   ...rootConfig,
+  reactHooks.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     ...pluginReact.configs.recommended,
   },
   {
-    plugins: {
-      'react-hooks': pluginReactHooks,
-    },
     rules: {
       'react-hooks/exhaustive-deps': 'error',
       'react-hooks/rules-of-hooks': 'error',
+      'react-hooks/react-compiler': 'error',
     },
   },
 ]
--- packages/react-query/eslint.config.js
+++ packages/react-query/eslint.config.js
@@ -1,25 +1,20 @@
 // @ts-check
 
 import pluginReact from '@eslint-react/eslint-plugin'
-// @ts-expect-error
-import pluginReactCompiler from 'eslint-plugin-react-compiler'
-import pluginReactHooks from 'eslint-plugin-react-hooks'
+import * as reactHooks from 'eslint-plugin-react-hooks'
 import rootConfig from './root.eslint.config.js'
 
 export default [
   ...rootConfig,
+  reactHooks.configs.recommended,
   {
     files: ['**/*.{ts,tsx}'],
     ...pluginReact.configs.recommended,
   },
   {
-    plugins: {
-      'react-hooks': pluginReactHooks,
-      'react-compiler': pluginReactCompiler,
-    },
     rules: {
       '@eslint-react/dom/no-missing-button-type': 'off',
-      'react-compiler/react-compiler': 'error',
+      'react-hooks/react-compiler': 'error',
       'react-hooks/exhaustive-deps': 'error',
       'react-hooks/rules-of-hooks': 'error',
     },
@@ -28,7 +23,6 @@ export default [
     files: ['**/__tests__/**'],
     rules: {
       '@typescript-eslint/no-unnecessary-condition': 'off',
-      'react-compiler/react-compiler': 'off',
     },
   },
 ]
