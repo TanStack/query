66ea5f2fcc2d17485bf3ce7c38ecf86ecececbdd
Jonghyeon Ko <manudeli.ko@gmail.com>
2025-05-08 04:48:20 +0900

    chore: add typescript-eslint/require-await rule  (#9116)
    Co-authored-by: MINSEONG KIM <65233075+minseong0324@users.noreply.github.com>

--- packages/query-codemods/eslint.config.js
+++ packages/query-codemods/eslint.config.js
@@ -1,6 +1,5 @@
 // @ts-check
 
-import vitest from '@vitest/eslint-plugin'
 import rootConfig from './root.eslint.config.js'
 
 export default [
@@ -9,6 +8,7 @@ export default [
     rules: {
       'cspell/spellchecker': 'off',
       '@typescript-eslint/no-unnecessary-condition': 'off',
+      '@typescript-eslint/require-await': 'off',
       'import/no-duplicates': 'off',
       'import/no-unresolved': 'off',
       'import/order': 'off',
