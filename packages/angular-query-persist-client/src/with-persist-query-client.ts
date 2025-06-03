0d1294b1b7d43f86c1ec8d9cb54d808af35d9dfc
Braden Wong <13159333+braden-w@users.noreply.github.com>
2025-06-03 08:26:53 -0700

    fix: update callback return types in persist client to support Promise<Array<void>> for improved handling in onSuccess and onError

--- packages/angular-query-persist-client/src/with-persist-query-client.ts
+++ packages/angular-query-persist-client/src/with-persist-query-client.ts
@@ -20,8 +20,8 @@ import type { PersistQueryClientFeature } from '@tanstack/angular-query-experime
 
 type PersistQueryClientOptions = {
   persistOptions: Omit<PersistQueryClientOptionsCore, 'queryClient'>
-  onSuccess?: () => Promise<void> | void
-  onError?: () => Promise<void> | void
+  onSuccess?: () => void | Promise<void> | Promise<Array<void>>
+  onError?: () => void | Promise<void> | Promise<Array<void>>
 }
 
 /**
