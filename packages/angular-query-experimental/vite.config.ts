c251358624c0a104236cb583f08e6e460934e3a6
Arnoud <6420061+arnoud-dv@users.noreply.github.com>
2025-06-17 23:37:10 +0200

    Revert "refactor(angular-query): migrate from tsup to vite (#9281)" (#9282)
    This reverts commit 1c7c276a54e1cb940fbe9b9202113d153bedbf0b.

--- packages/angular-query-experimental/vite.config.ts
+++ packages/angular-query-experimental/vite.config.ts
@@ -1,32 +1,8 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import { externalizeDeps } from 'vite-plugin-externalize-deps'
-import tsconfigPaths from 'vite-tsconfig-paths'
-import dts from 'vite-plugin-dts'
+import { defineConfig } from 'vitest/config'
+
 import packageJson from './package.json'
-import type { Options } from '@tanstack/config/vite'
 
-function ensureImportFileExtension({
-  content,
-  extension,
-}: {
-  content: string
-  extension: string
-}) {
-  // replace e.g. `import { foo } from './foo'` with `import { foo } from './foo.js'`
-  content = content.replace(
-    /(im|ex)port\s[\w{}/*\s,]+from\s['"](?:\.\.?\/)+?[^.'"]+(?=['"];?)/gm,
-    `$&.${extension}`,
-  )
-
-  // replace e.g. `import('./foo')` with `import('./foo.js')`
-  content = content.replace(
-    /import\(['"](?:\.\.?\/)+?[^.'"]+(?=['"];?)/gm,
-    `$&.${extension}`,
-  )
-  return content
-}
-
-const config = defineConfig({
+export default defineConfig({
   // fix from https://github.com/vitest-dev/vitest/issues/6992#issuecomment-2509408660
   resolve: {
     conditions: ['@tanstack/custom-condition'],
@@ -50,73 +26,3 @@ const config = defineConfig({
     restoreMocks: true,
   },
 })
-
-// copy from @tanstack/config/vite with changes:
-// - dts outDir: dist/types
-// - build - lib - fileName: [name.mjs]
-// - rollup - output - preserveModulesRoot: src
-export const tanstackViteConfig = (options: Options) => {
-  const outDir = options.outDir ?? 'dist'
-  const cjs = options.cjs ?? true
-
-  return defineConfig({
-    plugins: [
-      externalizeDeps({ include: options.externalDeps ?? [] }),
-      tsconfigPaths({
-        projects: options.tsconfigPath ? [options.tsconfigPath] : undefined,
-      }),
-      dts({
-        outDir: `dist/types`,
-        entryRoot: options.srcDir,
-        include: options.srcDir,
-        exclude: options.exclude,
-        tsconfigPath: options.tsconfigPath,
-        compilerOptions: {
-          module: 99, // ESNext
-          declarationMap: false,
-        },
-        beforeWriteFile: (filePath, content) => {
-          // content =
-          //   options.beforeWriteDeclarationFile?.(filePath, content) || content
-          return {
-            filePath,
-            content: ensureImportFileExtension({ content, extension: 'js' }),
-          }
-        },
-        afterDiagnostic: (diagnostics) => {
-          if (diagnostics.length > 0) {
-            console.error('Please fix the above type errors')
-            process.exit(1)
-          }
-        },
-      }),
-    ],
-    build: {
-      outDir,
-      minify: false,
-      sourcemap: true,
-      lib: {
-        entry: options.entry,
-        formats: cjs ? ['es', 'cjs'] : ['es'],
-        fileName: () => '[name].mjs',
-      },
-      rollupOptions: {
-        output: {
-          preserveModules: true,
-          preserveModulesRoot: 'src',
-        },
-      },
-    },
-  })
-}
-
-export default mergeConfig(
-  config,
-  tanstackViteConfig({
-    cjs: false,
-    entry: ['./src/index.ts'],
-    exclude: ['src/__tests__'],
-    srcDir: './src',
-    tsconfigPath: 'tsconfig.prod.json',
-  }),
-)
