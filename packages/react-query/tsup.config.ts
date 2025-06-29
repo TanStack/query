87ea568e5b8d2ded771063c38d23a96105ba8d54
Jonghyeon Ko <manudeli.ko@gmail.com>
2025-06-29 15:56:28 +0900

    chore(*): migrate tsup configuration files to TypeScript format

--- packages/react-query/tsup.config.ts
+++ packages/react-query/tsup.config.ts
@@ -1,9 +1,7 @@
-// @ts-check
-
 import { defineConfig } from 'tsup'
 import { legacyConfig, modernConfig } from './root.tsup.config.js'
 
 export default defineConfig([
-  modernConfig({ entry: ['src/*.ts'] }),
-  legacyConfig({ entry: ['src/*.ts'] }),
+  modernConfig({ entry: ['src/*.ts', 'src/*.tsx'] }),
+  legacyConfig({ entry: ['src/*.ts', 'src/*.tsx'] }),
 ])
