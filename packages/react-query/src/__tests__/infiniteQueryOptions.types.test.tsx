6866f7acf3e7985ccc51e83cc3d3bb204d70e693
Jonghyeon Ko <manudeli.ko@gmail.com>
2025-07-16 17:26:48 +0900

    refactor(react-query): update infinite query tests and add type checks for useSuspenseInfiniteQuery
    - Refactored infinite query tests to use a unified options function.
    - Added type checks for the new `useSuspenseInfiniteQuery` hook.
    - Improved test structure for better readability and maintainability.

--- packages/react-query/src/__tests__/infiniteQueryOptions.types.test.tsx
+++ packages/react-query/src/__tests__/infiniteQueryOptions.types.test.tsx
@@ -11,14 +11,15 @@ import {
   useSuspenseInfiniteQuery,
 } from '../useSuspenseInfiniteQuery'
 import { infiniteQueryOptions } from '../infiniteQueryOptions'
+import { doNotExecute } from './utils'
 
 const infiniteQuery = {
-  options1: () =>
+  options: () =>
     infiniteQueryOptions({
       queryKey: ['key', 1] as const,
       queryFn: () => Promise.resolve({ field: 'success' }),
     }),
-  options2: () =>
+  optionsWithInitialData: () =>
     infiniteQueryOptions({
       queryKey: ['key', 2] as const,
       queryFn: () => Promise.resolve({ field: 'success' }),
@@ -26,7 +27,7 @@ const infiniteQuery = {
     }),
 }
 
-const Ex = () => {
+const Example = () => {
   useInfiniteQuery({
     queryKey: ['key', 2] as const,
     queryFn: () => Promise.resolve({ field: 'success' }),
@@ -36,63 +37,68 @@ const Ex = () => {
 
 describe('infiniteQueryOptions', () => {
   it('should be used with useInfiniteQuery', () => {
-    const keyFn1Query = useInfiniteQuery(infiniteQuery.options1())
-    expectTypeOf(keyFn1Query).toEqualTypeOf<
-      UseInfiniteQueryResult<{ field: string }>
-    >()
-    expectTypeOf(keyFn1Query.data).toEqualTypeOf<
-      InfiniteData<{ field: string }> | undefined
-    >()
-    const keyFn1Query_Select = useInfiniteQuery({
-      ...infiniteQuery.options1(),
-      select: (data) => ({
-        pages: data.pages.map(({ field }) => field),
-        pageParams: data.pageParams,
-      }),
+    doNotExecute(() => {
+      const keyFn1Query = useInfiniteQuery(infiniteQuery.options())
+      expectTypeOf(keyFn1Query).toEqualTypeOf<
+        UseInfiniteQueryResult<{ field: string }>
+      >()
+      expectTypeOf(keyFn1Query.data).toEqualTypeOf<
+        InfiniteData<{ field: string }> | undefined
+      >()
+      const keyFn1Query_Select = useInfiniteQuery({
+        ...infiniteQuery.options(),
+        select: (data) => ({
+          pages: data.pages.map(({ field }) => field),
+          pageParams: data.pageParams,
+        }),
+      })
+      expectTypeOf(keyFn1Query_Select).toEqualTypeOf<
+        UseInfiniteQueryResult<string>
+      >()
+      expectTypeOf(keyFn1Query_Select.data).toEqualTypeOf<
+        InfiniteData<string> | undefined
+      >()
     })
-    expectTypeOf(keyFn1Query_Select).toEqualTypeOf<
-      UseInfiniteQueryResult<string>
-    >()
-    expectTypeOf(keyFn1Query_Select.data).toEqualTypeOf<
-      InfiniteData<string> | undefined
-    >()
   })
   it('should be used with useSuspenseInfiniteQuery', () => {
-    const dd = infiniteQuery.options1()
-    const keyFn1SuspenseQuery = useSuspenseInfiniteQuery(
-      infiniteQuery.options1(),
-    )
-    expectTypeOf(keyFn1SuspenseQuery).toEqualTypeOf<
-      UseSuspenseInfiniteQueryResult<{ field: string }>
-    >()
-    expectTypeOf(keyFn1SuspenseQuery.data).toEqualTypeOf<
-      InfiniteData<{ field: string }>
-    >()
-    const keyFn1SuspenseQuery_Select = useSuspenseInfiniteQuery({
-      ...infiniteQuery.options1(),
-      select: (data) => ({
-        pages: data.pages.map(({ field }) => field),
-        pageParams: data.pageParams,
-      }),
+    doNotExecute(() => {
+      const keyFn1SuspenseQuery = useSuspenseInfiniteQuery(
+        infiniteQuery.options(),
+      )
+      expectTypeOf(keyFn1SuspenseQuery).toEqualTypeOf<
+        UseSuspenseInfiniteQueryResult<{ field: string }>
+      >()
+      expectTypeOf(keyFn1SuspenseQuery.data).toEqualTypeOf<
+        InfiniteData<{ field: string }>
+      >()
+      const keyFn1SuspenseQuery_Select = useSuspenseInfiniteQuery({
+        ...infiniteQuery.options(),
+        select: (data) => ({
+          pages: data.pages.map(({ field }) => field),
+          pageParams: data.pageParams,
+        }),
+      })
+      expectTypeOf(keyFn1SuspenseQuery_Select).toEqualTypeOf<
+        UseSuspenseInfiniteQueryResult<string>
+      >()
+      expectTypeOf(keyFn1SuspenseQuery_Select.data).toEqualTypeOf<
+        InfiniteData<string>
+      >()
     })
-    expectTypeOf(keyFn1SuspenseQuery_Select).toEqualTypeOf<
-      UseSuspenseInfiniteQueryResult<string>
-    >()
-    expectTypeOf(keyFn1SuspenseQuery_Select.data).toEqualTypeOf<
-      InfiniteData<string>
-    >()
   })
-  it('should be used with useQueryClient', async () => {
-    const queryClient = useQueryClient()
+  it('should be used with useQueryClient', () => {
+    doNotExecute(async () => {
+      const queryClient = useQueryClient()
 
-    queryClient.invalidateQueries(infiniteQuery.options1())
-    queryClient.resetQueries(infiniteQuery.options1())
-    queryClient.removeQueries(infiniteQuery.options1())
-    queryClient.cancelQueries(infiniteQuery.options1())
-    queryClient.prefetchQuery(infiniteQuery.options1())
-    queryClient.refetchQueries(infiniteQuery.options1())
+      queryClient.invalidateQueries(infiniteQuery.options())
+      queryClient.resetQueries(infiniteQuery.options())
+      queryClient.removeQueries(infiniteQuery.options())
+      queryClient.cancelQueries(infiniteQuery.options())
+      queryClient.prefetchQuery(infiniteQuery.options())
+      queryClient.refetchQueries(infiniteQuery.options())
 
-    const query1 = await queryClient.fetchQuery(infiniteQuery.options1())
-    expectTypeOf(query1).toEqualTypeOf<InfiniteData<{ field: string }>>()
+      const query1 = await queryClient.fetchQuery(infiniteQuery.options())
+      expectTypeOf(query1).toEqualTypeOf<InfiniteData<{ field: string }>>()
+    })
   })
 })
