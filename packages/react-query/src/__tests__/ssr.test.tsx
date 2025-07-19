8e43ad53115e340a0b5f78cc9ae3762d34891165
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-07-19 11:02:12 +1000

    Update prettier

--- packages/react-query/src/__tests__/ssr.test.tsx
+++ packages/react-query/src/__tests__/ssr.test.tsx
@@ -152,7 +152,11 @@ describe('Server Side Rendering', () => {
         initialPageParam: 0,
       })
       return (
-        <ul>{query.data?.pages.map((page) => <li key={page}>{page}</li>)}</ul>
+        <ul>
+          {query.data?.pages.map((page) => (
+            <li key={page}>{page}</li>
+          ))}
+        </ul>
       )
     }
 
