729c300d789304c60e73740772f3ec217e099917
gwansikk <gwansik.kim@gwansik.dev>
2025-08-13 00:57:35 +0900

    chore(react-query-devtools, react-query-persist-client): add moduleNameMapper

--- packages/react-query-devtools/jest.config.ts
+++ packages/react-query-devtools/jest.config.ts
@@ -1,5 +1,10 @@
 export default {
   displayName: 'react-query-devtools',
   preset: '../../jest-preset.js',
+  moduleNameMapper: {
+    '^react((\\/.*)?)$': 'react-18$1',
+    '^react-dom((\\/.*)?)$': 'react-dom-18$1',
+    '^@testing-library/react(?:/.*)?$': '@testing-library/react-18',
+  },
   setupFilesAfterEnv: ['./jest.setup.ts'],
 }
