64a0270dd8c7987f28574acb293f55d7ae410037
gwansikk <gwansik.kim@gwansik.dev>
2025-08-13 00:51:13 +0900

    chore(react-query): add moduleNameMapper for React 18 compatibility in Jest config

--- packages/react-query/jest.config.ts
+++ packages/react-query/jest.config.ts
@@ -1,6 +1,11 @@
 export default {
   displayName: 'react-query',
   preset: '../../jest-preset.js',
+  moduleNameMapper: {
+    '^react((\\/.*)?)$': 'react-18$1',
+    '^react-dom((\\/.*)?)$': 'react-dom-18$1',
+    '^@testing-library/react(?:/.*)?$': '@testing-library/react-18',
+  },
   setupFilesAfterEnv: ['./jest.setup.ts'],
   testMatch: ['<rootDir>/src/**/*.test.tsx', '<rootDir>/codemods/**/*.test.js'],
 }
