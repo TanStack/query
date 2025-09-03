948c646e47dcc04b15d00edd556fa090ddd3c7a4
Jake Teton-Landis <just.1.jake@gmail.com>
2025-09-03 13:22:01 -0400

    oops missing import

--- packages/query-core/src/__tests__/timeoutManager.test.tsx
+++ packages/query-core/src/__tests__/timeoutManager.test.tsx
@@ -1,6 +1,7 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import {
   TimeoutManager,
+  defaultTimeoutProvider,
   systemSetTimeoutZero,
   timeoutManager,
 } from '../timeoutManager'
