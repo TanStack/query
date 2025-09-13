7fd609f5d928e4c058944ae36e7f4a16a8275bfd
Min Aung Thu Win <mhemaungthuwin@gmail.com>
2025-09-13 08:23:41 +0630

    fix: restore the tests according svelte 5 adapter branch

--- packages/svelte-query/tests/useMutationState/useMutationState.svelte.test.ts
+++ packages/svelte-query/tests/useMutationState/useMutationState.svelte.test.ts
@@ -1,24 +1,14 @@
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest'
-import { fireEvent, render } from '@testing-library/svelte'
-import { sleep } from '@tanstack/query-test-utils'
+import { describe, expect, test, vi } from 'vitest'
+import { fireEvent, render, waitFor } from '@testing-library/svelte'
 import BaseExample from './BaseExample.svelte'
 
 describe('useMutationState', () => {
-  beforeEach(() => {
-    vi.useFakeTimers()
-  })
-
-  afterEach(() => {
-    vi.useRealTimers()
-  })
-
   test('Run few mutation functions and check from useMutationState', async () => {
-    const successMutationFn = vi.fn(() => sleep(10).then(() => 'data'))
-    const errorMutationFn = vi
-      .fn()
-      .mockImplementation(() =>
-        sleep(20).then(() => Promise.reject(new Error('error'))),
-      )
+    const successMutationFn = vi.fn()
+
+    const errorMutationFn = vi.fn().mockImplementation(() => {
+      throw 'error'
+    })
 
     const rendered = render(BaseExample, {
       props: {
@@ -34,24 +24,28 @@ describe('useMutationState', () => {
       },
     })
 
-    fireEvent.click(rendered.getByText('success'))
-    await vi.advanceTimersByTimeAsync(11)
-    expect(successMutationFn).toHaveBeenCalledTimes(1)
-    expect(rendered.getByText('["success"]')).toBeInTheDocument()
+    fireEvent.click(rendered.getByTestId('success'))
 
-    fireEvent.click(rendered.getByText('error'))
-    await vi.advanceTimersByTimeAsync(21)
-    expect(errorMutationFn).toHaveBeenCalledTimes(1)
-    expect(rendered.getByText('["success","error"]')).toBeInTheDocument()
+    await waitFor(() => {
+      expect(successMutationFn).toHaveBeenCalledTimes(1)
+      expect(rendered.getByTestId('result').innerHTML).toEqual('["success"]')
+    })
+
+    fireEvent.click(rendered.getByTestId('error'))
+
+    await waitFor(() => {
+      expect(errorMutationFn).toHaveBeenCalledTimes(1)
+      expect(rendered.getByTestId('result').innerHTML).toEqual(
+        '["success","error"]',
+      )
+    })
   })
 
   test('Can select specific type of mutation ( i.e: error only )', async () => {
-    const successMutationFn = vi.fn(() => sleep(10).then(() => 'data'))
-    const errorMutationFn = vi
-      .fn()
-      .mockImplementation(() =>
-        sleep(20).then(() => Promise.reject(new Error('error'))),
-      )
+    const successMutationFn = vi.fn()
+    const errorMutationFn = vi.fn().mockImplementation(() => {
+      throw 'error'
+    })
 
     const rendered = render(BaseExample, {
       props: {
@@ -71,24 +65,26 @@ describe('useMutationState', () => {
       },
     })
 
-    fireEvent.click(rendered.getByText('success'))
-    await vi.advanceTimersByTimeAsync(11)
-    expect(successMutationFn).toHaveBeenCalledTimes(1)
-    expect(rendered.getByText('[]')).toBeInTheDocument()
+    fireEvent.click(rendered.getByTestId('success'))
 
-    fireEvent.click(rendered.getByText('error'))
-    await vi.advanceTimersByTimeAsync(21)
-    expect(errorMutationFn).toHaveBeenCalledTimes(1)
-    expect(rendered.getByText('["error"]')).toBeInTheDocument()
+    await waitFor(() => {
+      expect(successMutationFn).toHaveBeenCalledTimes(1)
+      expect(rendered.getByTestId('result').innerHTML).toEqual('[]')
+    })
+
+    fireEvent.click(rendered.getByTestId('error'))
+
+    await waitFor(() => {
+      expect(errorMutationFn).toHaveBeenCalledTimes(1)
+      expect(rendered.getByTestId('result').innerHTML).toEqual('["error"]')
+    })
   })
 
   test('Can select specific mutation using mutation key', async () => {
-    const successMutationFn = vi.fn(() => sleep(10).then(() => 'data'))
-    const errorMutationFn = vi
-      .fn()
-      .mockImplementation(() =>
-        sleep(20).then(() => Promise.reject(new Error('error'))),
-      )
+    const successMutationFn = vi.fn()
+    const errorMutationFn = vi.fn().mockImplementation(() => {
+      throw 'error'
+    })
 
     const rendered = render(BaseExample, {
       props: {
@@ -108,14 +104,18 @@ describe('useMutationState', () => {
       },
     })
 
-    fireEvent.click(rendered.getByText('success'))
-    await vi.advanceTimersByTimeAsync(11)
-    expect(successMutationFn).toHaveBeenCalledTimes(1)
-    expect(rendered.getByText('["success"]')).toBeInTheDocument()
+    fireEvent.click(rendered.getByTestId('success'))
 
-    fireEvent.click(rendered.getByText('error'))
-    await vi.advanceTimersByTimeAsync(21)
-    expect(errorMutationFn).toHaveBeenCalledTimes(1)
-    expect(rendered.getByText('["success"]')).toBeInTheDocument()
+    await waitFor(() => {
+      expect(successMutationFn).toHaveBeenCalledTimes(1)
+      expect(rendered.getByTestId('result').innerHTML).toEqual('["success"]')
+    })
+
+    fireEvent.click(rendered.getByTestId('error'))
+
+    await waitFor(() => {
+      expect(errorMutationFn).toHaveBeenCalledTimes(1)
+      expect(rendered.getByTestId('result').innerHTML).toEqual('["success"]')
+    })
   })
 })
