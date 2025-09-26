8f45f04866e50106e4085d78a255f020d6a5d75e
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-09-26 22:44:38 +1000

    Sync more changes from v5

--- examples/svelte/auto-refetching/svelte.config.js
+++ examples/svelte/auto-refetching/svelte.config.js
@@ -3,10 +3,7 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://github.com/sveltejs/svelte-preprocess
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter(),
   },
--- examples/svelte/playground/svelte.config.js
+++ examples/svelte/playground/svelte.config.js
@@ -4,7 +4,6 @@ import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   preprocess: vitePreprocess(),
-
   kit: {
     adapter: adapter(),
   },
