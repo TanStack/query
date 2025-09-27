62be7d539688a9f9c3c7ea55083635ebd105cd9c
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2025-09-27 23:45:04 +1000

    chore(svelte-query): fix eslint config (#9699)
    * chore(svelte-query): fix eslint config
    
    * Use @typescript-eslint/parser directly

--- packages/svelte-query/eslint.config.js
+++ packages/svelte-query/eslint.config.js
@@ -1,17 +1,19 @@
 // @ts-check
 
+import tsParser from '@typescript-eslint/parser'
 import pluginSvelte from 'eslint-plugin-svelte'
 import rootConfig from './root.eslint.config.js'
 import svelteConfig from './svelte.config.js'
 
 export default [
   ...rootConfig,
-  ...pluginSvelte.configs['flat/recommended'],
+  ...pluginSvelte.configs['recommended'],
   {
-    files: ['**/*.svelte'],
+    files: ['**/*.svelte', '**/*.svelte.ts', '**/*.svelte.js'],
     languageOptions: {
       parserOptions: {
-        parser: '@typescript-eslint/parser',
+        parser: tsParser,
+        extraFileExtensions: ['.svelte'],
         svelteConfig,
       },
     },
@@ -20,7 +22,6 @@ export default [
     rules: {
       'svelte/block-lang': ['error', { script: ['ts'] }],
       'svelte/no-svelte-internal': 'error',
-      'svelte/no-unused-svelte-ignore': 'off',
       'svelte/valid-compile': 'off',
     },
   },
