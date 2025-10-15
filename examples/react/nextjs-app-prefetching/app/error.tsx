8a204c001a49f3826968d1ee356e43be57683e18
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-10-16 00:31:04 +0900

    fix

---
+++ examples/react/nextjs-app-prefetching/app/error.tsx
@@ -0,0 +1,17 @@
+'use client'
+
+export default function Error({
+  error,
+  reset,
+}: {
+  error: Error & { digest?: string }
+  reset: () => void
+}) {
+  return (
+    <div>
+      <h2>Something went wrong!</h2>
+      <p>{error.message}</p>
+      <button onClick={reset}>Try again</button>
+    </div>
+  )
+}
