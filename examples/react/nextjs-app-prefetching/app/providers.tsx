559fc0a4300e2f7cef2549b59054bc9e125b3279
YukiOnishi <58220747+YukiOnishi1129@users.noreply.github.com>
2025-10-15 14:36:54 +0900

    fix

--- examples/react/nextjs-app-prefetching/app/providers.tsx
+++ examples/react/nextjs-app-prefetching/app/providers.tsx
@@ -1,7 +1,7 @@
 'use client'
 import { QueryClientProvider } from '@tanstack/react-query'
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
-import type {ReactNode} from 'react'
+import type { ReactNode } from 'react'
 import { getQueryClient } from '@/app/get-query-client'
 
 
