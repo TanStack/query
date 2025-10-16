300a6d4f9db07bbcd098140e1d9a4af3eebd747f
Serpentariu13 <serpentarium13@mail.ru>
2025-10-16 22:02:23 +0300

    upd(vue-query): Improve queryOptions() type

--- packages/vue-query/src/queryOptions.ts
+++ packages/vue-query/src/queryOptions.ts
@@ -1,7 +1,11 @@
 import type { DataTag, DefaultError, QueryKey } from '@tanstack/query-core'
 import type {
   DefinedInitialQueryOptions,
+  DefinedInitialQueryOptionsBase,
+  DefinedInitialQueryOptionsRef,
   UndefinedInitialQueryOptions,
+  UndefinedInitialQueryOptionsBase,
+  UndefinedInitialQueryOptionsRef,
 } from './useQuery'
 
 export function queryOptions<
@@ -9,23 +13,39 @@ export function queryOptions<
   TError = DefaultError,
   TData = TQueryFnData,
   TQueryKey extends QueryKey = QueryKey,
+  TOptions = DefinedInitialQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
+  TReturnQueryKey = {
+    queryKey: DataTag<TQueryKey, TQueryFnData, TError>
+  },
 >(
-  options: DefinedInitialQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
-): DefinedInitialQueryOptions<TQueryFnData, TError, TData, TQueryKey> & {
-  queryKey: DataTag<TQueryKey, TQueryFnData, TError>
-}
+  options: TOptions,
+): TOptions extends { value: unknown }
+  ? DefinedInitialQueryOptionsRef<TQueryFnData, TError, TData, TQueryKey> &
+  TOptions
+  : DefinedInitialQueryOptionsBase<TQueryFnData, TError, TData, TQueryKey> &
+  TOptions
 
 export function queryOptions<
   TQueryFnData = unknown,
   TError = DefaultError,
   TData = TQueryFnData,
   TQueryKey extends QueryKey = QueryKey,
+  TOptions = UndefinedInitialQueryOptions<
+    TQueryFnData,
+    TError,
+    TData,
+    TQueryKey
+  >
 >(
-  options: UndefinedInitialQueryOptions<TQueryFnData, TError, TData, TQueryKey>,
-): UndefinedInitialQueryOptions<TQueryFnData, TError, TData, TQueryKey> & {
-  queryKey: DataTag<TQueryKey, TQueryFnData, TError>
-}
+  options: TOptions,
+): TOptions extends { value: unknown }
+  ? UndefinedInitialQueryOptionsRef<TQueryFnData, TError, TData, TQueryKey> &
+  TOptions
+  : UndefinedInitialQueryOptionsBase<TQueryFnData, TError, TData, TQueryKey> &
+  TOptions
 
 export function queryOptions(options: unknown) {
   return options
 }
+
+
