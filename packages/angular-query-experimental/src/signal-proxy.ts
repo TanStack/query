c1bc243c2df0a900babeafe11344ba2bf72479d2
Benjamín Vicente <projects@benjavicente.dev>
2025-12-18 20:51:39 -0300

    fix(angular-query): statically split proxy objects to avoid reading input signal too soon

--- packages/angular-query-experimental/src/signal-proxy.ts
+++ packages/angular-query-experimental/src/signal-proxy.ts
@@ -1,32 +1,44 @@
 import { computed, untracked } from '@angular/core'
 import type { Signal } from '@angular/core'
 
-export type MapToSignals<T> = {
-  [K in keyof T]: T[K] extends Function ? T[K] : Signal<T[K]>
+export type MethodKeys<T> = {
+  [K in keyof T]: T[K] extends (...args: any[]) => any ? K : never
+}[keyof T]
+
+export type MapToSignals<T, ExcludeFields extends MethodKeys<T> = never> = {
+  [K in keyof T]: K extends ExcludeFields ? T[K] : Signal<T[K]>
 }
 
 /**
  * Exposes fields of an object passed via an Angular `Signal` as `Computed` signals.
  * Functions on the object are passed through as-is.
  * @param inputSignal - `Signal` that must return an object.
+ * @param excludeFields - Array of function property names that should NOT be converted to signals.
  * @returns A proxy object with the same fields as the input object, but with each field wrapped in a `Computed` signal.
  */
-export function signalProxy<TInput extends Record<string | symbol, any>>(
-  inputSignal: Signal<TInput>,
-) {
-  const internalState = {} as MapToSignals<TInput>
+export function signalProxy<
+  TInput extends Record<string | symbol, any>,
+  const ExcludeFields extends ReadonlyArray<MethodKeys<TInput>> = [],
+>(inputSignal: Signal<TInput>, excludeFields: ExcludeFields) {
+  const internalState = {} as MapToSignals<TInput, ExcludeFields[number]>
+  const excludeFieldsArray = excludeFields as ReadonlyArray<string>
 
-  return new Proxy<MapToSignals<TInput>>(internalState, {
+  return new Proxy<MapToSignals<TInput, ExcludeFields[number]>>(internalState, {
     get(target, prop) {
       // first check if we have it in our internal state and return it
       const computedField = target[prop]
       if (computedField) return computedField
 
-      // then, check if it's a function on the resultState and return it
-      const targetField = untracked(inputSignal)[prop]
-      if (typeof targetField === 'function') return targetField
+      // if it is an expluded function, return it without tracking
+      if (excludeFieldsArray.includes(prop as string)) {
+        const fn = (...args: Parameters<TInput[typeof prop]>) =>
+          untracked(inputSignal)[prop](...args)
+        // @ts-expect-error
+        target[prop] = fn
+        return fn
+      }
 
-      // finally, create a computed field, store it and return it
+      // otherwise, make a computed field
       // @ts-expect-error
       return (target[prop] = computed(() => inputSignal()[prop]))
     },
