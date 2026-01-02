cc7a99ffd62c22bce7217bb2ad50f7f629b6a7f3
Julius <julius0216@outlook.com>
2026-01-02 21:43:02 +0100

    try manual wrap, no difference

--- examples/react/react-start/src/routes/index.tsx
+++ examples/react/react-start/src/routes/index.tsx
@@ -1,12 +1,22 @@
 import { Link, createFileRoute } from '@tanstack/react-router'
 import { useSuspenseQuery } from '@tanstack/react-query'
+import { Suspense } from 'react'
 import { projectsQuery } from '../utils/queries'
 import Spinner from '../components/Spinner'
 
 export const Route = createFileRoute('/')({
-  component: ProjectsComponent,
-  pendingComponent: () => <div>Loading projects...</div>,
+  component: RouteComponent,
+  // pendingComponent: () => <div>Loading projects...</div>,
+  wrapInSuspense: false
 })
+
+function RouteComponent() {
+  return (
+    <Suspense fallback={<div>Loading projects...</div>}>
+      <ProjectsComponent />
+    </Suspense>
+  )
+}
 
 function ProjectsComponent() {
   const { data, isFetching } = useSuspenseQuery(projectsQuery())
@@ -14,7 +24,7 @@ function ProjectsComponent() {
   return (
     <div>
       <h1>TanStack Repositories {isFetching ? <Spinner /> : null}</h1>
-      <Link to='/'>Back</Link>
+      <Link to="/">Back</Link>
       {data.map((project) => (
         <p key={project.full_name}>
           <Link
