2441f5aebae14f0d1450a4af33219ed5477da08b
Jonghyeon Ko <jonghyeon@toss.im>
2026-01-28 20:03:21 +0900

    test(react-query): wrap usePrefetchQuery and usePrefetchInfiniteQuery tests in doNotExecute utility to prevent execution

--- packages/react-query/src/__tests__/usePrefetchInfiniteQuery.types.test.tsx
+++ packages/react-query/src/__tests__/usePrefetchInfiniteQuery.types.test.tsx
@@ -1,40 +1,45 @@
 import { expectTypeOf } from 'expect-type'
 import { usePrefetchInfiniteQuery } from '../usePrefetchInfiniteQuery'
+import { doNotExecute } from './utils'
 
 describe('usePrefetchInfiniteQuery', () => {
   it('should return nothing', () => {
-    const result = usePrefetchInfiniteQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-      getNextPageParam: () => 1,
-    })
+    doNotExecute(() => {
+      const result = usePrefetchInfiniteQuery({
+        queryKey: ['key'],
+        queryFn: () => Promise.resolve(5),
+        getNextPageParam: () => 1,
+      })
 
-    expectTypeOf(result).toEqualTypeOf<void>()
+      expectTypeOf(result).toEqualTypeOf<void>()
+    })
   })
 
   it('should not allow refetchInterval, enabled or throwOnError options', () => {
-    usePrefetchInfiniteQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-      getNextPageParam: () => 1,
-      // @ts-expect-error TS2353
-      refetchInterval: 1000,
-    })
+    doNotExecute(() => {
+      usePrefetchInfiniteQuery({
+        queryKey: ['key'],
+        queryFn: () => Promise.resolve(5),
+        getNextPageParam: () => 1,
+        // @ts-expect-error TS2353
+        refetchInterval: 1000,
+      })
 
-    usePrefetchInfiniteQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-      getNextPageParam: () => 1,
-      // @ts-expect-error TS2353
-      enabled: true,
-    })
+      usePrefetchInfiniteQuery({
+        queryKey: ['key'],
+        queryFn: () => Promise.resolve(5),
+        getNextPageParam: () => 1,
+        // @ts-expect-error TS2353
+        enabled: true,
+      })
 
-    usePrefetchInfiniteQuery({
-      queryKey: ['key'],
-      queryFn: () => Promise.resolve(5),
-      getNextPageParam: () => 1,
-      // @ts-expect-error TS2353
-      throwOnError: true,
+      usePrefetchInfiniteQuery({
+        queryKey: ['key'],
+        queryFn: () => Promise.resolve(5),
+        getNextPageParam: () => 1,
+        // @ts-expect-error TS2353
+        throwOnError: true,
+      })
     })
   })
 })
