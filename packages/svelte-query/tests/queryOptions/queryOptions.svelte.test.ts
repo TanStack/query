47746e66392cf82f3c2e8438ce35ff3d14ee292e
Wonsuk Choi <sukvvon@gmail.com>
2026-02-24 11:36:26 +0900

    test(svelte-query/queryOptions): add runtime test for identity function

---
+++ packages/svelte-query/tests/queryOptions/queryOptions.svelte.test.ts
@@ -0,0 +1,13 @@
+import { describe, expect, it } from 'vitest'
+import { queryOptions } from '../../src/index.js'
+
+describe('queryOptions', () => {
+  it('should return the object received as a parameter without any modification.', () => {
+    const object = {
+      queryKey: ['key'],
+      queryFn: () => Promise.resolve(5),
+    } as const
+
+    expect(queryOptions(object)).toStrictEqual(object)
+  })
+})
