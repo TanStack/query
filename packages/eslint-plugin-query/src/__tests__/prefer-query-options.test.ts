cc7aaf442d44984380c3b67563327ecbfffbe424
Daniel Perez <danielpza@protonmail.com>
2026-02-24 11:29:03 -0800

    fix test

--- packages/eslint-plugin-query/src/__tests__/prefer-query-options.test.ts
+++ packages/eslint-plugin-query/src/__tests__/prefer-query-options.test.ts
@@ -15,19 +15,19 @@ ruleTester.run(rule.name, rule, {
   invalid: [
     {
       code: `useQuery({ queryKey: [] })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-hook' }],
     },
     {
       code: `const users = useQuery({ ...queryOptions, queryKey: [] })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-hook' }],
     },
     {
       code: `const users = useQuery({ queryFn: () => {} })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-hook' }],
     },
     {
       code: `const users = useQuery({ ...queryOptions, queryFn: () => {} })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-hook' }],
     },
   ],
 })
@@ -42,19 +42,19 @@ ruleTester.run(rule.name, rule, {
   invalid: [
     {
       code: `queryClient.invalidateQueries({ queryKey: [] })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-invalidate' }],
     },
     {
       code: `queryClient.invalidateQueries({ ...queryOptions, queryKey: [] })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-invalidate' }],
     },
     {
       code: `queryClient.invalidateQueries({ queryFn: () => {} })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-invalidate' }],
     },
     {
       code: `queryClient.invalidateQueries({ ...queryOptions, queryFn: () => {} })`,
-      errors: [{ messageId: 'no-inline-query' }],
+      errors: [{ messageId: 'no-inline-query-invalidate' }],
     },
   ],
 })
