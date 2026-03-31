d26a9fb0fa157c105dcc905fcda351a7c966848e
Wonsuk Choi <sukvvon@gmail.com>
2026-03-31 15:35:24 +0900

    test(react-query): replace 'import React' with 'import * as React' in 'usePrefetchQuery' and 'usePrefetchInfiniteQuery'

--- packages/react-query/src/__tests__/usePrefetchQuery.test.tsx
+++ packages/react-query/src/__tests__/usePrefetchQuery.test.tsx
@@ -1,5 +1,5 @@
 import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
-import React from 'react'
+import * as React from 'react'
 import { act, fireEvent } from '@testing-library/react'
 import { ErrorBoundary } from 'react-error-boundary'
 import { queryKey, sleep } from '@tanstack/query-test-utils'
