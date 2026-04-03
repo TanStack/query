68473713f008dd9d26f565ed6f810c51960892e1
TkDodo <office@dorfmeister.cc>
2026-04-03 20:07:47 +0200

    bring back jsdocs

--- packages/angular-query-experimental/src/inject-infinite-query.ts
+++ packages/angular-query-experimental/src/inject-infinite-query.ts
@@ -36,6 +36,46 @@ export interface InjectInfiniteQueryOptions {
   injector?: Injector
 }
 
+/**
+ * Injects an infinite query: a declarative dependency on an asynchronous source of data that is tied to a unique key.
+ * Infinite queries can additively "load more" data onto an existing set of data or "infinite scroll"
+ *
+ * **Basic example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   query = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects'],
+ *     queryFn: ({ pageParam }) =>
+ *       this.#http.get<Response>('/api/projects?cursor=' + pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *   }))
+ * }
+ * ```
+ *
+ * Similar to `computed` from Angular, the function passed to `injectInfiniteQuery` will be run in the reactive context.
+ * In the example below, the query will be automatically enabled and executed when the filter signal changes
+ * to a truthy value. When the filter signal changes back to a falsy value, the query will be disabled.
+ *
+ * **Reactive example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   filter = signal('')
+ *
+ *   projectsQuery = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects', this.filter()],
+ *     queryFn: ({ pageParam }) => fetchProjects(this.filter(), pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *     enabled: !!this.filter(),
+ *   }))
+ * }
+ * ```
+ * @param injectInfiniteQueryFn - A function that returns infinite query options.
+ * @param options - Additional configuration.
+ * @returns The infinite query result.
+ * @see https://tanstack.com/query/latest/docs/framework/angular/guides/infinite-queries
+ */
 export function injectInfiniteQuery<
   TQueryFnData,
   TError = DefaultError,
@@ -57,6 +97,46 @@ export function injectInfiniteQuery<
   TPageParam,
   InfiniteQueryMode
 >
+/**
+ * Injects an infinite query: a declarative dependency on an asynchronous source of data that is tied to a unique key.
+ * Infinite queries can additively "load more" data onto an existing set of data or "infinite scroll"
+ *
+ * **Basic example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   query = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects'],
+ *     queryFn: ({ pageParam }) =>
+ *       this.#http.get<Response>('/api/projects?cursor=' + pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *   }))
+ * }
+ * ```
+ *
+ * Similar to `computed` from Angular, the function passed to `injectInfiniteQuery` will be run in the reactive context.
+ * In the example below, the query will be automatically enabled and executed when the filter signal changes
+ * to a truthy value. When the filter signal changes back to a falsy value, the query will be disabled.
+ *
+ * **Reactive example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   filter = signal('')
+ *
+ *   projectsQuery = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects', this.filter()],
+ *     queryFn: ({ pageParam }) => fetchProjects(this.filter(), pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *     enabled: !!this.filter(),
+ *   }))
+ * }
+ * ```
+ * @param injectInfiniteQueryFn - A function that returns infinite query options.
+ * @param options - Additional configuration.
+ * @returns The infinite query result.
+ * @see https://tanstack.com/query/latest/docs/framework/angular/guides/infinite-queries
+ */
 export function injectInfiniteQuery<
   TQueryFnData,
   TError = DefaultError,
@@ -73,6 +153,46 @@ export function injectInfiniteQuery<
   >,
   options?: InjectInfiniteQueryOptions,
 ): DefinedCreateInfiniteQueryResult<TData, TError, TPageParam, undefined>
+/**
+ * Injects an infinite query: a declarative dependency on an asynchronous source of data that is tied to a unique key.
+ * Infinite queries can additively "load more" data onto an existing set of data or "infinite scroll"
+ *
+ * **Basic example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   query = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects'],
+ *     queryFn: ({ pageParam }) =>
+ *       this.#http.get<Response>('/api/projects?cursor=' + pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *   }))
+ * }
+ * ```
+ *
+ * Similar to `computed` from Angular, the function passed to `injectInfiniteQuery` will be run in the reactive context.
+ * In the example below, the query will be automatically enabled and executed when the filter signal changes
+ * to a truthy value. When the filter signal changes back to a falsy value, the query will be disabled.
+ *
+ * **Reactive example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   filter = signal('')
+ *
+ *   projectsQuery = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects', this.filter()],
+ *     queryFn: ({ pageParam }) => fetchProjects(this.filter(), pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *     enabled: !!this.filter(),
+ *   }))
+ * }
+ * ```
+ * @param injectInfiniteQueryFn - A function that returns infinite query options.
+ * @param options - Additional configuration.
+ * @returns The infinite query result.
+ * @see https://tanstack.com/query/latest/docs/framework/angular/guides/infinite-queries
+ */
 export function injectInfiniteQuery<
   TQueryFnData,
   TError = DefaultError,
@@ -89,6 +209,46 @@ export function injectInfiniteQuery<
   >,
   options?: InjectInfiniteQueryOptions,
 ): CreateInfiniteQueryResult<TData, TError, TPageParam, InfiniteQueryMode>
+/**
+ * Injects an infinite query: a declarative dependency on an asynchronous source of data that is tied to a unique key.
+ * Infinite queries can additively "load more" data onto an existing set of data or "infinite scroll"
+ *
+ * **Basic example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   query = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects'],
+ *     queryFn: ({ pageParam }) =>
+ *       this.#http.get<Response>('/api/projects?cursor=' + pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *   }))
+ * }
+ * ```
+ *
+ * Similar to `computed` from Angular, the function passed to `injectInfiniteQuery` will be run in the reactive context.
+ * In the example below, the query will be automatically enabled and executed when the filter signal changes
+ * to a truthy value. When the filter signal changes back to a falsy value, the query will be disabled.
+ *
+ * **Reactive example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   filter = signal('')
+ *
+ *   projectsQuery = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects', this.filter()],
+ *     queryFn: ({ pageParam }) => fetchProjects(this.filter(), pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *     enabled: !!this.filter(),
+ *   }))
+ * }
+ * ```
+ * @param injectInfiniteQueryFn - A function that returns infinite query options.
+ * @param options - Additional configuration.
+ * @returns The infinite query result.
+ * @see https://tanstack.com/query/latest/docs/framework/angular/guides/infinite-queries
+ */
 export function injectInfiniteQuery<
   TQueryFnData,
   TError = DefaultError,
@@ -105,6 +265,46 @@ export function injectInfiniteQuery<
   >,
   options?: InjectInfiniteQueryOptions,
 ): CreateInfiniteQueryResult<TData, TError, TPageParam, undefined>
+/**
+ * Injects an infinite query: a declarative dependency on an asynchronous source of data that is tied to a unique key.
+ * Infinite queries can additively "load more" data onto an existing set of data or "infinite scroll"
+ *
+ * **Basic example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   query = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects'],
+ *     queryFn: ({ pageParam }) =>
+ *       this.#http.get<Response>('/api/projects?cursor=' + pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *   }))
+ * }
+ * ```
+ *
+ * Similar to `computed` from Angular, the function passed to `injectInfiniteQuery` will be run in the reactive context.
+ * In the example below, the query will be automatically enabled and executed when the filter signal changes
+ * to a truthy value. When the filter signal changes back to a falsy value, the query will be disabled.
+ *
+ * **Reactive example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   filter = signal('')
+ *
+ *   projectsQuery = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects', this.filter()],
+ *     queryFn: ({ pageParam }) => fetchProjects(this.filter(), pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *     enabled: !!this.filter(),
+ *   }))
+ * }
+ * ```
+ * @param injectInfiniteQueryFn - A function that returns infinite query options.
+ * @param options - Additional configuration.
+ * @returns The infinite query result.
+ * @see https://tanstack.com/query/latest/docs/framework/angular/guides/infinite-queries
+ */
 export function injectInfiniteQuery<
   TQueryFnData,
   TError = DefaultError,
@@ -121,6 +321,46 @@ export function injectInfiniteQuery<
   >,
   options?: InjectInfiniteQueryOptions,
 ): DefinedCreateInfiniteQueryResult<TData, TError, TPageParam>
+/**
+ * Injects an infinite query: a declarative dependency on an asynchronous source of data that is tied to a unique key.
+ * Infinite queries can additively "load more" data onto an existing set of data or "infinite scroll"
+ *
+ * **Basic example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   query = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects'],
+ *     queryFn: ({ pageParam }) =>
+ *       this.#http.get<Response>('/api/projects?cursor=' + pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *   }))
+ * }
+ * ```
+ *
+ * Similar to `computed` from Angular, the function passed to `injectInfiniteQuery` will be run in the reactive context.
+ * In the example below, the query will be automatically enabled and executed when the filter signal changes
+ * to a truthy value. When the filter signal changes back to a falsy value, the query will be disabled.
+ *
+ * **Reactive example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   filter = signal('')
+ *
+ *   projectsQuery = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects', this.filter()],
+ *     queryFn: ({ pageParam }) => fetchProjects(this.filter(), pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *     enabled: !!this.filter(),
+ *   }))
+ * }
+ * ```
+ * @param injectInfiniteQueryFn - A function that returns infinite query options.
+ * @param options - Additional configuration.
+ * @returns The infinite query result.
+ * @see https://tanstack.com/query/latest/docs/framework/angular/guides/infinite-queries
+ */
 export function injectInfiniteQuery<
   TQueryFnData,
   TError = DefaultError,
@@ -137,6 +377,46 @@ export function injectInfiniteQuery<
   >,
   options?: InjectInfiniteQueryOptions,
 ): CreateInfiniteQueryResult<TData, TError, TPageParam>
+/**
+ * Injects an infinite query: a declarative dependency on an asynchronous source of data that is tied to a unique key.
+ * Infinite queries can additively "load more" data onto an existing set of data or "infinite scroll"
+ *
+ * **Basic example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   query = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects'],
+ *     queryFn: ({ pageParam }) =>
+ *       this.#http.get<Response>('/api/projects?cursor=' + pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *   }))
+ * }
+ * ```
+ *
+ * Similar to `computed` from Angular, the function passed to `injectInfiniteQuery` will be run in the reactive context.
+ * In the example below, the query will be automatically enabled and executed when the filter signal changes
+ * to a truthy value. When the filter signal changes back to a falsy value, the query will be disabled.
+ *
+ * **Reactive example**
+ * ```ts
+ * class ServiceOrComponent {
+ *   filter = signal('')
+ *
+ *   projectsQuery = injectInfiniteQuery(() => ({
+ *     queryKey: ['projects', this.filter()],
+ *     queryFn: ({ pageParam }) => fetchProjects(this.filter(), pageParam),
+ *     initialPageParam: 0,
+ *     getNextPageParam: (lastPage) => lastPage.nextCursor,
+ *     enabled: !!this.filter(),
+ *   }))
+ * }
+ * ```
+ * @param injectInfiniteQueryFn - A function that returns infinite query options.
+ * @param options - Additional configuration.
+ * @returns The infinite query result.
+ * @see https://tanstack.com/query/latest/docs/framework/angular/guides/infinite-queries
+ */
 export function injectInfiniteQuery<
   TQueryFnData,
   TError = DefaultError,
