8b86951550b9d293b807c9259a1b7494754e1c15
Wonsuk Choi <sukvvon@gmail.com>
2026-04-06 22:33:08 +0900

    Revert "test({react,preact}-query/infiniteQueryOptions): replace type annotation with 'as const'"
    This reverts commit 2bb0029621aa32333259ab0db7d6653929fcdb28.

--- packages/preact-query/src/__tests__/infiniteQueryOptions.test.tsx
+++ packages/preact-query/src/__tests__/infiniteQueryOptions.test.tsx
@@ -1,14 +1,16 @@
 import { describe, expect, it } from 'vitest'
+
 import { infiniteQueryOptions } from '../infiniteQueryOptions'
+import type { UseInfiniteQueryOptions } from '../types'
 
 describe('infiniteQueryOptions', () => {
   it('should return the object received as a parameter without any modification.', () => {
-    const object = {
+    const object: UseInfiniteQueryOptions = {
       queryKey: ['key'],
       queryFn: () => Promise.resolve(5),
       getNextPageParam: () => null,
       initialPageParam: null,
-    } as const
+    }
 
     expect(infiniteQueryOptions(object)).toBe(object)
   })
