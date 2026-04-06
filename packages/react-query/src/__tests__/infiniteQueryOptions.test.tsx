2bb0029621aa32333259ab0db7d6653929fcdb28
Wonsuk Choi <sukvvon@gmail.com>
2026-04-06 21:58:15 +0900

    test({react,preact}-query/infiniteQueryOptions): replace type annotation with 'as const'

--- packages/react-query/src/__tests__/infiniteQueryOptions.test.tsx
+++ packages/react-query/src/__tests__/infiniteQueryOptions.test.tsx
@@ -1,16 +1,14 @@
 import { describe, expect, it } from 'vitest'
-
 import { infiniteQueryOptions } from '../infiniteQueryOptions'
-import type { UseInfiniteQueryOptions } from '../types'
 
 describe('infiniteQueryOptions', () => {
   it('should return the object received as a parameter without any modification.', () => {
-    const object: UseInfiniteQueryOptions = {
+    const object = {
       queryKey: ['key'],
       queryFn: () => Promise.resolve(5),
       getNextPageParam: () => null,
       initialPageParam: null,
-    }
+    } as const
 
     expect(infiniteQueryOptions(object)).toBe(object)
   })
