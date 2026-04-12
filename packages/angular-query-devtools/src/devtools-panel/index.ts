dd9078244aab087d58a97cee9111fd23383516d1
Benjamín Vicente <projects@benjavicente.dev>
2026-04-11 23:20:23 -0400

    feat(angular-query-devtools): add standalone Angular devtools package

---
+++ packages/angular-query-devtools/src/devtools-panel/index.ts
@@ -0,0 +1,8 @@
+export type {
+  InjectDevtoolsPanel,
+  DevtoolsPanelOptions,
+  InjectDevtoolsPanelOptions,
+  DevtoolsPanelRef,
+} from './types'
+
+export { injectDevtoolsPanel } from './inject-devtools-panel'
