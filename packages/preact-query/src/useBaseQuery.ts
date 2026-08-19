8aeaa35fcd4845077e1e7a52eee0624fec4a1681
TkDodo <office@dorfmeister.cc>
2026-08-19 20:25:59 +0200

    ref: remove unused experimental_beforeQuery and experimental_afterQuery

--- packages/preact-query/src/useBaseQuery.ts
+++ packages/preact-query/src/useBaseQuery.ts
@@ -54,10 +54,6 @@ export function useBaseQuery<
   const client = useQueryClient(queryClient)
   const defaultedOptions = client.defaultQueryOptions(options)
 
-  ;(client.getDefaultOptions().queries as any)?._experimental_beforeQuery?.(
-    defaultedOptions,
-  )
-
   if (process.env.NODE_ENV !== 'production') {
     if (!defaultedOptions.queryFn) {
       console.error(
@@ -139,11 +135,6 @@ export function useBaseQuery<
   ) {
     throw result.error
   }
-
-  ;(client.getDefaultOptions().queries as any)?._experimental_afterQuery?.(
-    defaultedOptions,
-    result,
-  )
 
   if (
     defaultedOptions.experimental_prefetchInRender &&
