0adf659a65ff1f6e0841bbdf3985ec479efd6aeb
Lucas Leung <kayauleung2024@gmail.com>
2026-08-20 20:30:37 +0800

    put svelte config into vite config

--- examples/svelte/basic/vite.config.ts
+++ examples/svelte/basic/vite.config.ts
@@ -1,6 +1,14 @@
+import adapter from '@sveltejs/adapter-auto'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [sveltekit()],
+  plugins: [
+    sveltekit({
+      preprocess: vitePreprocess(),
+      adapter: adapter(),
+      compilerOptions: { runes: true },
+    }),
+  ],
 })
