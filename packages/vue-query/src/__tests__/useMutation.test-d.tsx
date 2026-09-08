3874bdc9af89f1f53d90c9cdfd629dade7256bc3
Wonsuk Choi <sukvvon@gmail.com>
2026-09-08 20:40:49 +0900

    test(vue-query/useMutation): drop redundant type annotation from 'mutationFn' params in context test

--- packages/vue-query/src/__tests__/useMutation.test-d.tsx
+++ packages/vue-query/src/__tests__/useMutation.test-d.tsx
@@ -104,7 +104,7 @@ describe('useMutation', () => {
 
   it('should type context as the last argument for mutationFn and every hook-level callback', () => {
     useMutation({
-      mutationFn: (_vars: string, context) => {
+      mutationFn: (_vars, context) => {
         expectTypeOf(context).toEqualTypeOf<MutationFunctionContext>()
         expectTypeOf(context.client).toEqualTypeOf<QueryClient>()
         return Promise.resolve('data')
