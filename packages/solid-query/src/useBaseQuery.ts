6ebffacbe7451ab330574dcf0ec6a139546548e3
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2026-09-26 06:18:00 +1000

    fix(solid-query): useBaseQuery SSR cleanup (#11628)
    * fix(solid-query): useBaseQuery SSR cleanup
    
    * Add changeset

--- packages/solid-query/src/useBaseQuery.ts
+++ packages/solid-query/src/useBaseQuery.ts
@@ -340,7 +340,7 @@ export function useBaseQuery<
   )
 
   onCleanup(() => {
-    if (isServer && queryResource.loading) {
+    if (isServer && queryResource.state === 'pending') {
       unsubscribeQueued = true
       return
     }
