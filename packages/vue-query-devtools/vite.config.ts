c7ef12184b0e957f06048652061ec2215e67b395
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2026-09-26 10:33:47 +1000

    Fix angular, vue-devtools

--- packages/vue-query-devtools/vite.config.ts
+++ packages/vue-query-devtools/vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
-import vue from '@vitejs/plugin-vue'
+import vue from 'unplugin-vue/vite'
 import { tanstackViteConfig } from '@tanstack/vite-config'
 
 import packageJson from './package.json'
